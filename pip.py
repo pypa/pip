46800cd1d4ad0322594eb7bdf8dd3365814e3a4d
Jannis Leidel <jannis@leidel.info>
2009-09-29 01:08:26 +0200

    Fixed #31 -- Correctly remove read-only attributes of build files in rmtree_errorhandler.

--- pip.py
+++ pip.py
@@ -17,6 +17,7 @@ import re
 import shutil
 import fnmatch
 import operator
+import stat
 try:
     from hashlib import md5
 except ImportError:
@@ -87,6 +88,22 @@ def rmtree_errorhandler(func, path, exc_info):
         func(path)
     else:
         raise typ, val, tb
+
+def rmtree_errorhandler(func, path, exc_info):
+    """On Windows, the files in .svn are read-only, so when rmtree() tries to
+    remove them, an exception is thrown.  We catch that here, remove the
+    read-only attribute, and hopefully continue without problems."""
+    exctype, value = exc_info[:2]
+    # lookin for a windows error
+    if exctype is not WindowsError or 'Access is denied' not in str(value):
+        raise
+    # file type should currently be read only
+    if ((os.stat(path).st_mode & stat.S_IREAD) != stat.S_IREAD):
+        raise
+    # convert to read/write
+    os.chmod(path, stat.S_IWRITE)
+    # use the original function to repeat the operation
+    func(path)
 
 class VcsSupport(object):
     _registry = {}
