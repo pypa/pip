8b9c3c5abc0785d814d264dc3cf5dab0f41f67a5
Jannis Leidel <jannis@leidel.info>
2009-11-21 22:25:18 +0100

    Updates to the documentation stylesheets and theme options

--- docs/conf.py
+++ docs/conf.py
@@ -146,7 +146,7 @@ html_last_updated_fmt = '%b %d, %Y'
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
-#html_use_smartypants = True
+html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
 #html_sidebars = {}
@@ -156,10 +156,10 @@ html_last_updated_fmt = '%b %d, %Y'
 #html_additional_pages = {}
 
 # If false, no module index is generated.
-html_use_modindex = True
+html_use_modindex = False
 
 # If false, no index is generated.
-html_use_index = True
+html_use_index = False
 
 # If true, the index is split into individual pages for each letter.
 #html_split_index = False
