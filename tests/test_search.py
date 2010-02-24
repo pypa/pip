17db3cce9a2a88fecc3feed6f366604811615a76
Jakub Vysoky <jakub@borka.cz>
2010-02-24 11:24:55 +0100

    respect merged changes in txt files

--- tests/test_search.py
+++ tests/test_search.py
@@ -2,7 +2,7 @@
 import os, sys
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
-from pip.commands.search import compare_versions, highest_version, translate_hits
+from pip.commands.search import compare_versions, highest_version, transform_hits
 from test_pip import run_pip, reset_env
 
 
@@ -18,14 +18,14 @@ def test_1():
 
 def test_2():
     '''
-    Test hit translation (pypi xmlrpc to custom list)::
+    Test transformation of data structures (pypi xmlrpc to custom list)::
     '''
     pypi_hits = [{'_pypi_ordering': 100, 'name': 'foo', 'summary': 'foo summary', 'version': '1.0'},
             {'_pypi_ordering': 200, 'name': 'foo', 'summary': 'foo summary v2', 'version': '2.0'},
             {'_pypi_ordering': 50, 'name': 'bar', 'summary': 'bar summary', 'version': '1.0'}]
     expected = [{'score': 200, 'versions': ['1.0', '2.0'], 'name': 'foo', 'summary': 'foo summary v2'},
             {'score': 50, 'versions': ['1.0'], 'name': 'bar', 'summary': 'bar summary'}]
-    assert expected == translate_hits(pypi_hits)
+    assert expected == transform_hits(pypi_hits)
 
 def test_3():
     '''
