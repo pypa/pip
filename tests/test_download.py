9ed09e69dad740e1ed12db46b129e98c3db7671a
Francesco <f.guerrieri@gmail.com>
2010-04-13 01:40:56 +0200

    removed lib_py, use env instead. download test pass

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,23 +1,24 @@
 from os import makedirs
 from os.path import join
 import textwrap
-from test_pip import here, reset_env, run_pip, pyversion, lib_py, get_env, diff_states, write_file
+from test_pip import here, reset_env, run_pip, pyversion,  diff_states, write_file
+from path import Path
 
 def test_download_if_requested():
     """
-    It should download and not install if requested.
+    It should download (in the scratch path) and not install if requested.
     
     """
-    reset_env()
+    env = reset_env()
     result = run_pip('install', 'INITools==0.1', '-d', '.', expect_error=True)
     assert Path('scratch')/ 'INITools-0.1.tar.gz' in result.files_created
     assert env.site_packages/ 'initools' not in result.files_created
 
 def test_single_download_from_requirements_file():
     """
-    It should support download from PyPi from a requirements file"
+    It should support download (in the scratch path) from PyPi from a requirements file
     """
-    reset_env()
+    env = reset_env()
     write_file('test-req.txt', textwrap.dedent("""
         INITools==0.1
         """))
