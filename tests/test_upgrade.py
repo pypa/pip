bfc40f08de27302608cfc0f8af22231a627694ce
Francesco <f.guerrieri@gmail.com>
2010-04-15 23:54:36 +0200

    All test_upgrade test pass

--- tests/test_upgrade.py
+++ tests/test_upgrade.py
@@ -1,7 +1,7 @@
 
 from os.path import join
 import textwrap
-from test_pip import here, reset_env, run_pip, pyversion, lib_py, get_env, diff_states, write_file
+from test_pip import here, reset_env, run_pip, get_env, diff_states, write_file
 
 def test_no_upgrade_unless_requested():
     """
@@ -38,34 +38,35 @@ def test_uninstall_before_upgrade():
     Automatic uninstall-before-upgrade.
 
     """
-    reset_env()
+    env = reset_env()
     result = run_pip('install', 'INITools==0.2', expect_error=True)
-    assert join(lib_py + 'site-packages', 'initools') in result.files_created, sorted(result.files_created.keys())
+    assert env.site_packages/ 'initools' in result.files_created, sorted(result.files_created.keys())
     result2 = run_pip('install', 'INITools==0.3', expect_error=True)
     assert result2.files_created, 'upgrade to INITools 0.3 failed'
     result3 = run_pip('uninstall', 'initools', '-y', expect_error=True)
-    assert diff_states(result.files_before, result3.files_after, ignore=['build']).values() == [{}, {}, {}]
+    assert diff_states(result.files_before, result3.files_after, ignore=['env/build', 'cache']).values() == [{}, {}, {}]
 
 def test_upgrade_from_reqs_file():
     """
     Upgrade from a requirements file.
 
     """
-    reset_env()
+    env = reset_env()
     write_file('test-req.txt', textwrap.dedent("""\
         PyLogo<0.4
         # and something else to test out:
         INITools==0.3
         """))
-    result = run_pip('install', '-r', 'test-req.txt')
+    result = run_pip('install', '-r', env.scratch_path/ 'test-req.txt')
     write_file('test-req.txt', textwrap.dedent("""\
         PyLogo
         # and something else to test out:
         INITools
         """))
-    result2 = run_pip('install', '--upgrade', '-r', 'test-req.txt')
-    result3 = run_pip('uninstall', '-r', 'test-req.txt', '-y')
-    assert diff_states(result.files_before, result3.files_after, ignore=['build', 'test-req.txt']).values() == [{}, {}, {}]
+    result2 = run_pip('install', '--upgrade', '-r', env.scratch_path/ 'test-req.txt')
+    result3 = run_pip('uninstall', '-r', env.scratch_path/ 'test-req.txt', '-y')
+    res = diff_states(result.files_before, result3.files_after, ignore=['env/build', 'cache', 'scratch/test-req.txt']).values() 
+    assert res == [{}, {}, {}], res
 
 def test_uninstall_rollback():
     """
@@ -73,13 +74,12 @@ def test_uninstall_rollback():
     crafted to fail on install).
 
     """
-    reset_env()
-    env = get_env()
+    env = reset_env()
     find_links = 'file://' + join(here, 'packages')
     result = run_pip('install', '-f', find_links, '--no-index', 'broken==0.1')
-    assert (join(lib_py, 'site-packages', 'broken.py') in result.files_created), result.files_created.keys()
+    assert env.site_packages / 'broken.py' in result.files_created, result.files_created.keys()
     result2 = run_pip('install', '-f', find_links, '--no-index', 'broken==0.2broken', expect_error=True)
     assert result2.returncode == 1, str(result2)
-    env.run(join(env.bin_dir, 'python'), '-c', "import broken; print broken.VERSION").stdout
+    env.run( 'python', '-c', "import broken; print broken.VERSION").stdout
     '0.1\n'
-    assert diff_states(result.files_after, result2.files_after, ignore=['build', 'pip-log.txt']).values() == [{}, {}, {}], diff_states(result.files_after, result2.files_after, ignore=['build', 'pip-log.txt'])
+    assert diff_states(result.files_after, result2.files_after, ignore=[env.relative_env_path/'build', 'pip-log.txt']).values() == [{}, {}, {}]
