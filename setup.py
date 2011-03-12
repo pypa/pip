2d25114d28280dff55b021dd0803364d378697df
Jannis Leidel <jannis@leidel.info>
2011-03-12 15:10:55 -0500

    Updated news and bumped version to 0.8.3.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import sys
 import os
 from setuptools import setup
 
-version = "0.8.2"
+version = "0.8.3"
 
 doc_dir = os.path.join(os.path.dirname(__file__), 'docs')
 index_filename = os.path.join(doc_dir, 'index.txt')
