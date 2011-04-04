ac38fb6f2aa6d2710ccb5bf8096dffd5e67ccd7e
Jannis Leidel <jannis@leidel.info>
2011-04-04 10:47:17 +0200

    Bumped version to 1.0.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup
 
 # If you change this version, change it also in docs/conf.py
-version = "0.9.0.pre1"
+version = "1.0"
 
 doc_dir = os.path.join(os.path.dirname(__file__), "docs")
 index_filename = os.path.join(doc_dir, "index.txt")
@@ -23,11 +23,10 @@ f.close()
 
 setup(name="pip",
       version=version,
-      description="pip installs packages.  Python packages.  An easy_install replacement",
+      description="pip installs packages. Python packages. An easy_install replacement",
       long_description=long_description,
       classifiers=[
-        'Development Status :: 4 - Beta',
-        #'Development Status :: 5 - Production/Stable',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Topic :: Software Development :: Build Tools',
