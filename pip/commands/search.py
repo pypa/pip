0922f8135240210b270cf2c2665561f85feb3a8a
Paul Nasrat <pnasrat@googlemail.com>
2011-04-16 21:57:22 +0100

    Work around failing test due to distutils.version issue
    In py3k the following fails, handle this and use string comparison in worst
    case.
    
    Tested on 2.6 and 3.2
    
    >> from distutils.version import LooseVersion as LV
    >>> v1, v2, v3 = (LV('1.1'), LV('1.1.2'), LV('1.1a1'))
    >>> v1 > v2
    False
    >>> v1 > v3
    False
    >>> v2 > v3
    Traceback (most recent call last):
      File "<stdin>", line 1, in <module>
      File "distutils/version.py", line 70, in __gt__
        c = self._cmp(other)
      File "distutils/version.py", line 343, in _cmp
        if self.version < other.version:
    TypeError: unorderable types: int() < str()

--- pip/commands/search.py
+++ pip/commands/search.py
@@ -106,7 +106,14 @@ def compare_versions(version1, version2):
         return cmp(StrictVersion(version1), StrictVersion(version2))
     # in case of abnormal version number, fall back to LooseVersion
     except ValueError:
+        pass
+    try:
         return cmp(LooseVersion(version1), LooseVersion(version2))
+    except TypeError:
+    # certain LooseVersion comparions raise due to unorderable types,
+    # fallback to string comparison
+        return cmp([str(v) for v in LooseVersion(version1).version],
+                   [str(v) for v in LooseVersion(version2).version])
 
 
 def highest_version(versions):
