1ecda0db167c68994cb2c0ec4fcd5523d36f0fe6
Erik Bray <embray@stsci.edu>
2011-08-24 14:07:16 -0400

    Fix --upgrade to leave already up-to-date packages alone.  Add --force-reinstall to force reinstallation even of up-to-date packages, as was the prior behavior.

--- pip/index.py
+++ pip/index.py
@@ -15,7 +15,7 @@ import zlib
 from pip.log import logger
 from pip.util import Inf
 from pip.util import normalize_name, splitext
-from pip.exceptions import DistributionNotFound
+from pip.exceptions import DistributionNotFound, BestVersionAlreadyInstalled
 from pip.backwardcompat import (WindowsError, BytesIO,
                                 Queue, httplib, urlparse,
                                 URLError, HTTPError, u,
@@ -172,6 +172,7 @@ class PackageFinder(object):
             if applicable_versions[0][1] is Inf:
                 logger.info('Existing installed version (%s) is most up-to-date and satisfies requirement'
                             % req.satisfied_by.version)
+                raise BestVersionAlreadyInstalled
             else:
                 logger.info('Existing installed version (%s) satisfies requirement (most up-to-date version is %s)'
                             % (req.satisfied_by.version, applicable_versions[0][1]))
@@ -184,7 +185,7 @@ class PackageFinder(object):
             # We have an existing version, and its the best version
             logger.info('Installed version (%s) is most up-to-date (past versions: %s)'
                         % (req.satisfied_by.version, ', '.join([version for link, version in applicable_versions[1:]]) or 'none'))
-            return None
+            raise BestVersionAlreadyInstalled
         if len(applicable_versions) > 1:
             logger.info('Using version %s (newest of versions: %s)' %
                         (applicable_versions[0][1], ', '.join([version for link, version in applicable_versions])))
