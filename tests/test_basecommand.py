e00fd71f95dee66b2d54c408b63ac508b71b1533
Olivier Girardot <ssaboum@gmail.com>
2011-12-10 12:45:36 +0100

    remove defaults from my plateform + formatting and proper method definition

--- tests/test_basecommand.py
+++ tests/test_basecommand.py
@@ -7,11 +7,28 @@ def test_timeout_properly_passed_on():
     """
     class TestCommand(Command):
         name = "test-cmd"
+        def run(self, options, args):
+            pass
     cmd = TestCommand()    
     complete_args = ['install', '--timeout', '1', 'traits']
     args = ['--timeout', '1', 'traits']
     import optparse
-    initial_options = {'log': None, 'timeout': 15, 'venv': None, 'no_input': False, 'help': None, 'venv_base': '/Users/ogirardot/Documents/Python/virtualenv', 'site_packages': None, 'quiet': 0, 'default_vcs': '', 'require_venv': False, 'proxy': '', 'respect_venv': 1, 'skip_requirements_regex': '', 'log_file': '/Users/ogirardot/.pip/pip.log', 'log_explicit_levels': False, 'verbose': 0}
+    initial_options = {'log': None, 
+        'timeout': 15, 
+        'venv': None, 
+        'no_input': False, 
+        'help': None, 
+        'venv_base': None, 
+        'site_packages': None, 
+        'quiet': 0, 
+        'default_vcs': '', 
+        'require_venv': False, 
+        'proxy': '', 
+        'respect_venv': 1, 
+        'skip_requirements_regex': '', 
+        'log_file': None, 
+        'log_explicit_levels': False, 
+        'verbose': 0}
     cmd.main(complete_args, args, optparse.Values(initial_options))
     import socket
     assert socket.getdefaulttimeout() == 1
