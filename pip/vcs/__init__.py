c0ccd4dca2dd4f6c2f55490054157225624d103f
another_thomas <none@none>
2012-03-13 12:28:19 -0700

    fixes issue #427, improved error message

--- pip/vcs/__init__.py
+++ pip/vcs/__init__.py
@@ -115,7 +115,10 @@ class VersionControl(object):
         Returns the correct repository URL and revision by parsing the given
         repository URL
         """
-        assert '+' in self.url, "Sorry, '{}' is a malformed url".format(self.url)
+
+	error_message= "Sorry, '{}' is a malformed url. In requirements files, the \
+format is <vcs>+<protocol>://<url>, e.g. svn+http://myrepo/svn/MyApp#egg=MyApp"
+        assert '+' in self.url, error_message.format(self.url)
         url = self.url.split('+', 1)[1]
         scheme, netloc, path, query, frag = urlparse.urlsplit(url)
         rev = None
