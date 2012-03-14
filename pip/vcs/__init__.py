4b6872dfbbd8a88a7d88fbb7a71c06b2a21d4f54
Carl Meyer <carl@oddbird.net>
2012-03-13 23:55:17 -0700

    No .format() formatting yet.

--- pip/vcs/__init__.py
+++ pip/vcs/__init__.py
@@ -115,9 +115,11 @@ class VersionControl(object):
         Returns the correct repository URL and revision by parsing the given
         repository URL
         """
-        error_message= "Sorry, '{}' is a malformed url. In requirements files, the \
-format is <vcs>+<protocol>://<url>, e.g. svn+http://myrepo/svn/MyApp#egg=MyApp"
-        assert '+' in self.url, error_message.format(self.url)
+        error_message= (
+           "Sorry, '%s' is a malformed VCS url. "
+           "Ihe format is <vcs>+<protocol>://<url>, "
+           "e.g. svn+http://myrepo/svn/MyApp#egg=MyApp")
+        assert '+' in self.url, error_message % self.url
         url = self.url.split('+', 1)[1]
         scheme, netloc, path, query, frag = urlparse.urlsplit(url)
         rev = None
