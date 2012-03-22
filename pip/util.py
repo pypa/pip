eb8a19ab6fa5f8e62f2c1e6c53f2165885e62c39
jstasiak <kuba.stasiak@gmail.com>
2012-03-22 14:09:34 +1100

    Fix unit decimal prefix (KB->kB)

--- pip/util.py
+++ pip/util.py
@@ -149,9 +149,9 @@ def format_size(bytes):
     if bytes > 1000*1000:
         return '%.1fMB' % (bytes/1000.0/1000)
     elif bytes > 10*1000:
-        return '%iKB' % (bytes/1000)
+        return '%ikB' % (bytes/1000)
     elif bytes > 1000:
-        return '%.1fKB' % (bytes/1000.0)
+        return '%.1fkB' % (bytes/1000.0)
     else:
         return '%ibytes' % bytes
 
