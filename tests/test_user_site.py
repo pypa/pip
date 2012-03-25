3d59a7db226e742b82939ba73486f9a62d7745c0
Marcus Smith <qwcode@gmail.com>
2012-03-24 23:22:18 -0700

    remove abbreviations from test suite

--- tests/test_user_site.py
+++ tests/test_user_site.py
@@ -1,44 +1,6 @@
 """
 tests specific to "--user" option
 
-abbreviations:
- sp = system python
- vp = virtualenv python
- vps = virtualenv python with --system_site_packages
- ssp = system site-packages 
- usp = user site packages
- vsp = virtualenv site packages
-
-summary of test coverage:
- 
-  "action : precondition
-     env -> result"
-
-  1. install --user : python<2.6 
-     vps -> error
-
-  2. install --user -e : setuptools
-     vps -> error
-
-  3. install --user -e : distribute
-     vps -> works     
-
-  4. install --user : anything
-     vp -> error (vp's don't know about usersite)
-
-  5. install --user : conflict in vsp
-     vps -> ignores conflict, installs to usp
-
-  6. install --user : conflict in usp
-     vps -> uninstalls from usp, installs to usp
-
-  7. install --user : conflict in vsp & usp
-     vps -> uninstalls from usp, installs to usp
-
-  8. uninstall: install editable local dir in usp
-     vps -> uninstall from usp
-
-
 """
 import sys
 from os.path import abspath, join, curdir, isdir, isfile
@@ -47,8 +9,7 @@ from tests.local_repos import local_checkout
 from tests.test_pip import here, reset_env, run_pip, pyversion
 
 
-
-def Test1_install_user_in_old_python_fails():
+def Test_install_user_in_old_python_fails():
     """
     user install on older Python<2.6 fails with message
     """
@@ -71,7 +32,7 @@ class Tests_UserSite:
             raise SkipTest()
 
 
-    def Test2_install_user_editable_with_setuptools_fails(self):
+    def Test_install_user_editable_with_setuptools_fails(self):
         """
         "--user" and "-e" with setuptools fails with message
         """
@@ -86,7 +47,7 @@ class Tests_UserSite:
         assert '--user --editable not supported with setuptools, use distribute' in result.stdout
 
 
-    def Test3_install_user_editable_with_distribute(self):
+    def Test_install_user_editable_with_distribute(self):
         """
         "--user" and "-e" with distribute works
         """
@@ -98,7 +59,7 @@ class Tests_UserSite:
 
 
 
-    def Test4_install_user_vp_fails(self):
+    def Test_install_user_venv_nositepkgs_fails(self):
         """
         user install in virtualenv (with no system packages) fails with message
         """
@@ -108,9 +69,9 @@ class Tests_UserSite:
         assert 'You are in virtualenv where the user site is not visible, will not continue.' in result.stdout
 
 
-    def Test5_install_user_conflict_in_vsp(self):
+    def Test_install_user_conflict_in_venv(self):
         """
-        user install with conflict in vsp; ignores vsp and installs
+        user install with conflict in venv site-pkgs; ignores venv site-pkgs and installs
         """
 
         env = reset_env(system_site_packages=True)
@@ -130,9 +91,9 @@ class Tests_UserSite:
         assert isdir(initools_folder)
 
 
-    def Test6_install_user_conflict_in_usp(self):
+    def Test_install_user_conflict_in_usersite(self):
         """
-        user install with conflict in usp; updates usp
+        user install with conflict in user site-pkgs; updates user site-pkgs
         """
 
         env = reset_env(system_site_packages=True)
@@ -147,9 +108,9 @@ class Tests_UserSite:
         assert initools_folder in result2.files_updated, str(result2)
 
 
-    def Test7_install_user_conflict_in_vsp_usp(self):
+    def Test_install_user_conflict_in_venv_usersite(self):
         """
-        user install with conflict in vsp & usp; ignores vsp and updates usp
+        user install with conflict in venv site-pkgs and user site-pkgs; ignores venv site-pkgs and updates user site-pkgs
         """
 
         env = reset_env(system_site_packages=True)
@@ -164,18 +125,28 @@ class Tests_UserSite:
         #files_updated due to uninstall not currently supporting removal from user_site in virtualenvs
         assert initools_folder in result3.files_updated, str(result3) 
 
-        #vsp still has 0.2 (can't just look in result1; have to check)
+        #venv still has 0.2 (can't just look in result1; have to check)
         egg_info_folder = env.root_path / env.site_packages / 'INITools-0.2-py%s.egg-info' % pyversion
         initools_folder = env.root_path / env.site_packages / 'initools'
         assert isdir(egg_info_folder)
         assert isdir(initools_folder)
 
 
-    def Test8_uninstall_editable_from_usp_in_vsp(self):
+    def Test_uninstall_from_usersite(self):
         """
-        uninstall editable local dir user install (in vsp)
+        uninstall from usersite
         """
+        env = reset_env(system_site_packages=True)
+        result1 = run_pip('install', '--user', 'INITools==0.3')
+        result2 = run_pip('uninstall', '-y', 'INITools')
+        result3 = run_pip('uninstall', '-y', 'INITools', expect_error=True)
+        assert 'Cannot uninstall requirement INITools, not installed' in result3.stdout
 
+
+    def Test_uninstall_editable_from_usersite(self):
+        """
+        uninstall editable local dir user install
+        """
         env = reset_env(use_distribute=True, system_site_packages=True)
         to_install = abspath(join(here, 'packages', 'FSPkg'))
         result1 = run_pip('install', '--user', '-e', to_install, expect_error=False)
@@ -187,5 +158,4 @@ class Tests_UserSite:
 
         result3 = run_pip('uninstall', '-y', 'FSPkg', expect_error=True)
         assert 'Cannot uninstall requirement FSPkg, not installed' in result3.stdout
-        
-        
+
