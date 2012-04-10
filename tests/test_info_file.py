efaf1066d2e279d34d19c4b9634484e2a690ea4f
Erik Simmler <tgecho@gmail.com>
2012-04-10 10:36:12 -0400

    Cleaned up info_file tests based on discussion in https://github.com/pypa/pip/pull/453

--- tests/test_info_file.py
+++ tests/test_info_file.py
@@ -2,49 +2,40 @@ import os
 
 from pip.backwardcompat import ConfigParser
 from pip.download import path_to_url2
-from tests.test_pip import here, reset_env, run_pip
+from tests.test_pip import here, reset_env, run_pip, pyversion
+
 from tests.path import Path
 
 
 def test_index():
     """
-    Test that the info.ini is written and works from an index (PyPI).
+    Test that the pip.ini is written and works from an index (PyPI).
     """
     env = reset_env()
-    run_pip('install', '-i http://pypi.python.org/simple/', 'INITools')
+    run_pip('install', '-i http://pypi.python.org/simple/', 'INITools==0.3.1')
 
-    egg_info_dir = None
-    for x in os.listdir(os.path.join(os.path.dirname(env.venv_path), env.site_packages)):
-        if x.startswith("INITools-") and x.endswith(".egg-info"):
-            egg_info_dir = os.path.join(os.path.dirname(env.venv_path), env.site_packages, x)
-            break
-    assert egg_info_dir is not None
+    egg_info_dir = env.base_path / env.site_packages / 'INITools-0.3.1-py%s.egg-info' % pyversion
 
-    infofp = open(os.path.join(os.path.dirname(env.venv_path), env.site_packages, egg_info_dir, "info.ini"))
+    infofp = open(os.path.join(egg_info_dir, "pip.ini"))
     info = ConfigParser.RawConfigParser()
     info.readfp(infofp)
     infofp.close()
 
     assert info.has_section("download")
     assert info.get("download", "url").startswith("http://pypi.python.org/packages/source/I/INITools/INITools")
-    assert info.get("download", "requirement") == "INITools"
+    assert info.get("download", "requirement") == "INITools==0.3.1"
 
 
 def test_tarball():
     """
-    Test that the info.ini is written and works from an tarball.
+    Test that the pip.ini is written and works from an tarball.
     """
     env = reset_env()
     run_pip('install', 'http://pypi.python.org/packages/source/I/INITools/INITools-0.3.1.tar.gz')
 
-    egg_info_dir = None
-    for x in os.listdir(os.path.join(os.path.dirname(env.venv_path), env.site_packages)):
-        if x.startswith("INITools-") and x.endswith(".egg-info"):
-            egg_info_dir = os.path.join(os.path.dirname(env.venv_path), env.site_packages, x)
-            break
-    assert egg_info_dir is not None
+    egg_info_dir = env.base_path / env.site_packages / 'INITools-0.3.1-py%s.egg-info' % pyversion
 
-    infofp = open(os.path.join(egg_info_dir, "info.ini"))
+    infofp = open(os.path.join(egg_info_dir, "pip.ini"))
     info = ConfigParser.RawConfigParser()
     info.readfp(infofp)
     infofp.close()
@@ -56,24 +47,19 @@ def test_tarball():
 
 def test_editable():
     """
-    Test that the info.ini is written and works from an editable.
+    Test that the pip.ini is written and works from an editable.
     """
     env = reset_env()
     fspkg = path_to_url2(Path(here) / 'packages' / 'FSPkg')
     run_pip('install', '-e', fspkg)
 
-    egg_info_dir = None
-    for x in os.listdir(Path(here) / 'packages' / 'FSPkg'):
-        if x.startswith("FSPkg") and x.endswith(".egg-info"):
-            egg_info_dir = os.path.join(Path(here) / 'packages' / 'FSPkg', x)
-            break
-    assert egg_info_dir is not None
+    egg_info_dir = Path(here) / 'packages' / 'FSPkg' / 'FSPkg.egg-info'
 
-    infofp = open(os.path.join(egg_info_dir, "info.ini"))
+    infofp = open(os.path.join(egg_info_dir, "pip.ini"))
     info = ConfigParser.RawConfigParser()
     info.readfp(infofp)
     infofp.close()
 
     assert info.has_section("download")
-    assert info.get("download", "url") == "file:///Users/dstufft/projects/pip/tests/packages/FSPkg"
-    assert info.get("download", "requirement") == "--editable=file:///Users/dstufft/projects/pip/tests/packages/FSPkg"
+    assert info.get("download", "url") == fspkg
+    assert info.get("download", "requirement") == "--editable=" + fspkg
