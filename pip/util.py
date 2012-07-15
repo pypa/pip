f6ccc0b657502ea3546c9615188f8dc548adb8c1
Daniel Holth <dholth@fastmail.fm>
2012-07-15 08:01:03 -0400

    install from wheel files

--- pip/util.py
+++ pip/util.py
@@ -417,7 +417,8 @@ def unzip_file(filename, location, flatten=True):
     try:
         zip = zipfile.ZipFile(zipfp)
         leading = has_leading_dir(zip.namelist()) and flatten
-        for name in zip.namelist():
+        for info in zip.infolist():
+            name = info.filename
             data = zip.read(name)
             fn = name
             if leading:
@@ -436,6 +437,7 @@ def unzip_file(filename, location, flatten=True):
                     fp.write(data)
                 finally:
                     fp.close()
+                    os.chmod(fn, info.external_attr >> 16)                
     finally:
         zipfp.close()
 
@@ -521,6 +523,7 @@ def cache_download(target_file, temp_location, content_type):
 
 
 def unpack_file(filename, location, content_type, link):
+    filename = os.path.realpath(filename)
     if (content_type == 'application/zip'
         or filename.endswith('.zip')
         or filename.endswith('.pybundle')
