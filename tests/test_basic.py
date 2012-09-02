6545c845f00d9064ccd0121edb2667872dc55fef
Daniel Holth <dholth@fastmail.fm>
2012-09-02 07:41:58 -0400

    modified wheel tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -329,15 +329,29 @@ def test_install_as_egg():
 
 def test_install_from_wheel():
     """
-    Test installing from a wheel file.
+    Test installing from a wheel.
     """
-    from pip.backwardcompat import get_path
-
     env = reset_env(use_distribute=True)
     find_links = 'file://'+abspath(join(here, 'packages'))
-    run_pip('install', 'markerlib', expect_error=False)
     # winds up empty...
     result = run_pip('install', 'simple.dist', '--no-index', '--find-links='+find_links, expect_error=False)
+    # Could be dist-packages on Debian
+    dist_info_folder = env.site_packages/'simple.dist-0.1.dist-info'
+    assert dist_info_folder in result.files_created, (dist_info_folder,
+                                                      result.files_created,
+                                                      result.stdout)
+    
+    
+def test_install_from_wheel_file():
+    """
+    Test installing directly from a wheel file.
+    """
+    env = reset_env(use_distribute=True)
+    package = abspath(join(here, 
+                           'packages', 
+                           'simple.dist-0.1-py2.py3-none-any.whl'))
+    # winds up empty...
+    result = run_pip('install', package, '--no-index', expect_error=False)
     # Could be dist-packages on Debian
     dist_info_folder = env.site_packages/'simple.dist-0.1.dist-info'
     assert dist_info_folder in result.files_created, (dist_info_folder,
