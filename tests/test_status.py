776fcbce9f23e799cd3101ddfa0bb966898d7064
Rafael Caricio <rafael.jacinto@gmail.com>
2012-09-07 17:28:18 -0400

    Test search for more than one distribution.

--- tests/test_status.py
+++ tests/test_status.py
@@ -4,6 +4,7 @@ from pip import __version__
 from pip.commands.status import search_packages_info
 from tests.test_pip import reset_env, run_pip
 
+
 def test_status():
     """
     Test end to end test for status command.
@@ -20,6 +21,17 @@ def test_status():
     assert 'Location: %s' % dist.location, lines[3]
     assert 'Files:' == lines[4]
     assert 'Cannot locate installed-files.txt' == lines[5]
+
+
+def test_missing_argument():
+    """
+    Test status command with no arguments.
+
+    """
+    dist = pkg_resources.get_distribution('pip')
+    reset_env()
+    result = run_pip('status')
+    assert 'ERROR: Missing required argument (status query).' in result.stdout
 
 
 def test_find_package_not_found():
@@ -39,3 +51,12 @@ def test_search_any_case():
     result = list(search_packages_info(['PIP']))
     assert len(result) == 1
     assert 'pip' == result[0]['name']
+
+
+def test_more_than_one_package():
+    """
+    Search for more than one package.
+
+    """
+    result = list(search_packages_info(['Pip', 'Nose', 'Virtualenv']))
+    assert len(result) == 3
