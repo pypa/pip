8647c68adc24d9dc21f7e3274a59e522efa85185
Rafael Caricio <rafael.jacinto@gmail.com>
2012-10-01 19:52:13 -0300

    Change tests to use mock instead of pytz.

--- tests/test_list.py
+++ tests/test_list.py
@@ -9,10 +9,10 @@ def test_list_command():
 
     """
     reset_env()
-    run_pip('install', 'INITools==0.2', 'pytz==2011k')
+    run_pip('install', 'INITools==0.2', 'mock==0.7.0')
     result = run_pip('list')
     assert 'initools (0.2)' in result.stdout
-    assert 'pytz (2011k)' in result.stdout
+    assert 'mock (0.7.0)' in result.stdout
 
 
 def test_local_flag():
@@ -21,9 +21,9 @@ def test_local_flag():
 
     """
     reset_env()
-    run_pip('install', 'pytz==2011k')
+    run_pip('install', 'mock==0.7.0')
     result = run_pip('list', '--local')
-    assert 'pytz (2011k)' in result.stdout
+    assert 'mock (0.7.0)' in result.stdout
 
 
 def test_uptodate_flag():
@@ -48,16 +48,16 @@ def test_outdated_flag():
     """
     env = reset_env()
     total_re = re.compile('LATEST: +([0-9.\w]+)')
-    write_file('initools-req.txt', textwrap.dedent("""\
+    write_file('req.txt', textwrap.dedent("""\
         INITools==0.2
         # and something else to test out:
-        pytz==2011k
+        mock==0.7.0
         """))
-    run_pip('install', '-r', env.scratch_path/'initools-req.txt')
-    result = run_pip('search', 'pytz')
-    pytz_ver = total_re.search(str(result)).group(1)
+    run_pip('install', '-r', env.scratch_path/'req.txt')
+    result = run_pip('search', 'mock')
+    mock_ver = total_re.search(str(result)).group(1)
     result = run_pip('search', 'INITools')
     initools_ver = total_re.search(str(result)).group(1)
     result = run_pip('list', '--outdated', expect_stderr=True)
     assert 'initools (CURRENT: 0.2 LATEST: %s)' % initools_ver in result.stdout
-    assert 'pytz (CURRENT: 2011k LATEST: %s)' % pytz_ver in result.stdout
+    assert 'mock (CURRENT: 0.7.0 LATEST: %s)' % mock_ver in result.stdout
