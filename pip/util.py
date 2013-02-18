2cbc7fadcdbfca5610cdcfedcee48d4d7cd7aaa1
Marcus Smith <qwcode@gmail.com>
2013-02-18 00:30:05 -0800

    improve ssl exception text and docs

--- pip/util.py
+++ pip/util.py
@@ -671,13 +671,21 @@ def call_subprocess(cmd, show_stdout=True,
 def raise_no_ssl_exception():
     """Raise when there's no ssl and not using '--no-ssl'"""
     raise PipError(textwrap.dedent("""
-            #############################################################
-            ##  You don't have an importable ssl module.               ##
-            ##  We can not provide ssl certified downloads.            ##
-            ##  Do one of 2 things:                                    ##
-            ##   1) Install this: https://pypi.python.org/pypi/ssl/    ##
-            ##      (It provides ssl support for older Pythons )       ##
-            ##   2) Use the --insecure option to allow insecurity      ##
-            #############################################################
+            ###################################################################
+            ##  You don't have an importable ssl module. You are most        ##
+            ##  likely using Python 2.5, which did not include ssl           ##
+            ##  support by default. In this state, we can not provide        ##
+            ##  ssl certified downloads from PyPI.                           ##
+            ##                                                               ##
+            ##  You can do one of 2 things:                                  ##
+            ##   1) Install this: https://pypi.python.org/pypi/ssl/          ##
+            ##      (It provides ssl support for older Pythons )             ##
+            ##   2) Use the --insecure option to allow this insecurity       ##
+            ##                                                               ##
+            ##  For more details, go to the  "SSL Certificate Verification"  ##
+            ##  section located here:                                        ##
+            ##     http://www.pip-installer.org/en/latest/logic.html         ##
+            ##                                                               ##
+            ###################################################################
             """))
 
