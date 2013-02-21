fa1b292f32b600a415b0b3f67550a7b0089de333
Marcus Smith <qwcode@gmail.com>
2013-02-20 19:09:05 -0800

    1.3rc1

--- pip/__init__.py
+++ pip/__init__.py
@@ -14,7 +14,7 @@ from pip.commands import commands, get_similar_commands, get_summaries
 
 
 # The version as used in the setup.py and the docs conf.py
-__version__ = "1.2.1.post1"
+__version__ = "1.3rc1"
 
 
 def autocomplete():
