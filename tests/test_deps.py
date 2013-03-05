48ccb60192d4242cb2d88fd1ad36caadd4da25bf
Enrico <rico.bl@gmail.com>
2013-03-05 11:55:32 -0300

    Improved `deps` command tests to run faster.

--- tests/test_deps.py
+++ tests/test_deps.py
@@ -1,24 +1,33 @@
 # -*- coding: utf-8 -*-
 
 import os
-from tests.test_pip import reset_env, run_pip, here, mkdir
+from unittest import TestCase
+from tests.test_pip import reset_env, run_pip, here
 
 
 def _run_deps(*args, **kwargs):
-    reset_env()
     kwargs['expect_stderr'] = True
     find_links = 'file://' + os.path.join(here, 'packages/deps')
     return run_pip('deps', '--no-index', '-f', find_links, *args, **kwargs)
 
-def test_deps_command_returns_info_for_a_single_package():
-    result = _run_deps('dependency')
-    assert 'dependency==1.0' in result.stdout
+
+class TestDepsCommandWithASinglePackage(TestCase):
+
+    @classmethod
+    def setupClass(cls):
+        reset_env()
+        cls.result = _run_deps('dependency')
+
+    def test_returns_version_info(self):
+        self.assertTrue('dependency==1.0' in self.result.stdout)
+
+    def test_redirects_downloading_messages_to_stderr(self):
+        self.assertTrue('Downloading/unpacking' in self.result.stderr)
+
 
 def test_deps_command_returns_info_for_a_package_with_dependencies():
+    reset_env()
     result = _run_deps('dependant')
     assert 'dependant==1.0' in result.stdout
     assert 'dependency==1.0' in result.stdout
 
-def test_deps_command_redirects_downloading_messages_to_stderr():
-    result = _run_deps('dependency')
-    assert 'Downloading/unpacking' in result.stderr
