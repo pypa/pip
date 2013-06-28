6e32b21b0439131e6192e9d78b1c3d4159034560
Marcus Smith <qwcode@gmail.com>
2013-06-27 23:10:36 -0700

    correct wheel extension: '.whl'

--- pip/util.py
+++ pip/util.py
@@ -591,7 +591,7 @@ def unpack_file(filename, location, content_type, link):
         or filename.endswith('.pybundle')
         or filename.endswith('.whl')
         or zipfile.is_zipfile(filename)):
-        unzip_file(filename, location, flatten=not filename.endswith(('.pybundle', 'whl')))
+        unzip_file(filename, location, flatten=not filename.endswith(('.pybundle', '.whl')))
     elif (content_type == 'application/x-gzip'
           or tarfile.is_tarfile(filename)
           or splitext(filename)[1].lower() in ('.tar', '.tar.gz', '.tar.bz2', '.tgz', '.tbz')):
