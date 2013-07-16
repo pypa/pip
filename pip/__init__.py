70a45c3d074391c0ccd470b2a929ffbd7145782e
Marcus Smith <qwcode@gmail.com>
2013-07-15 21:11:21 -0700

    1.4rc5

--- pip/__init__.py
+++ pip/__init__.py
@@ -14,7 +14,7 @@ from pip.commands import commands, get_similar_commands, get_summaries
 
 
 # The version as used in the setup.py and the docs conf.py
-__version__ = "1.4rc4"
+__version__ = "1.4rc5"
 
 def autocomplete():
     """Command and option completion for the main option parser (and options)
