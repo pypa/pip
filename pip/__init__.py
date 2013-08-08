77f959a3ce9cc506efbf3a17290d387d0a6624f5
Donald Stufft <donald@stufft.io>
2013-08-07 20:42:56 -0400

    Bump version to 1.4.1

--- pip/__init__.py
+++ pip/__init__.py
@@ -14,7 +14,7 @@ from pip.commands import commands, get_similar_commands, get_summaries
 
 
 # The version as used in the setup.py and the docs conf.py
-__version__ = "1.4"
+__version__ = "1.4.1"
 
 def autocomplete():
     """Command and option completion for the main option parser (and options)
