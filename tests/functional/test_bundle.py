71ea32baaf0b6e854db939e059ff1d41c6530eef
Donald Stufft <donald@stufft.io>
2013-08-18 08:47:49 -0400

    Rewrite the bundle functional tests to be better isolated

--- tests/functional/test_bundle.py
+++ tests/functional/test_bundle.py
@@ -1,68 +1,86 @@
-import zipfile
+import os.path
 import textwrap
-from os.path import abspath, exists, join
+import zipfile
+
 from pip.download import path_to_url2
-from tests.lib import tests_data, reset_env, run_pip, write_file
-from tests.lib.path import Path
+from tests.lib import tests_data
 from tests.lib.local_repos import local_checkout
 
 
-def test_create_bundle():
+def test_create_bundle(tmpdir, script):
     """
     Test making a bundle.  We'll grab one package from the filesystem
     (the FSPkg dummy package), one from vcs (initools) and one from an
     index (pip itself).
-
     """
-    env = reset_env()
-    fspkg = path_to_url2(Path(tests_data)/'packages'/'FSPkg')
-    run_pip('install', '-e', fspkg)
-    pkg_lines = textwrap.dedent('''\
+    fspkg = path_to_url2(os.path.join(tests_data, "packages", "FSPkg"))
+    ctx = (
+        fspkg,
+        local_checkout("svn+http://svn.colorstudy.com/INITools/trunk"),
+    )
+    bundle_req_path = str(tmpdir.join("bundle-req.txt"))
+    bundle_path = str(tmpdir.join("test.pybundle"))
+
+    script.pip("install", "-e", fspkg)
+
+    with open(bundle_req_path, "w") as bundle_req:
+        bundle_req.write(textwrap.dedent("""
             -e %s
             -e %s#egg=initools-dev
-            pip''' % (fspkg, local_checkout('svn+http://svn.colorstudy.com/INITools/trunk')))
-    write_file('bundle-req.txt', pkg_lines)
-    # Create a bundle in env.scratch_path/ test.pybundle
-    result = run_pip('bundle', '-r', env.scratch_path/ 'bundle-req.txt', env.scratch_path/ 'test.pybundle')
-    bundle = result.files_after.get(join('scratch', 'test.pybundle'), None)
-    assert bundle is not None
+            pip
+        """ % ctx))
 
-    files = zipfile.ZipFile(bundle.full).namelist()
-    assert 'src/FSPkg/' in files
-    assert 'src/initools/' in files
-    assert 'build/pip/' in files
+    # Create a bundle
+    script.pip("bundle", "-r", bundle_req_path, bundle_path)
+
+    # Ensure the bundle exists
+    assert os.path.exists(bundle_path)
+
+    # Ensure the bundle has the files we expect
+    bundle = zipfile.ZipFile(bundle_path)
+
+    assert "src/FSPkg/" in bundle.namelist()
+    assert "src/initools/" in bundle.namelist()
+    assert "build/pip/" in bundle.namelist()
 
 
-def test_cleanup_after_create_bundle():
+def test_cleanup_after_create_bundle(tmpdir, script):
     """
-    Test clean up after making a bundle. Make sure (build|src)-bundle/ dirs are removed but not src/.
-
+    Test clean up after making a bundle. Make sure (build|src)-bundle/ dirs are
+    removed but not src/.
     """
-    env = reset_env()
-    # Install an editable to create a src/ dir.
-    args = ['install']
-    args.extend(['-e',
-                 '%s#egg=pip-test-package' %
-                    local_checkout('git+http://github.com/pypa/pip-test-package.git')])
-    run_pip(*args)
-    build = env.venv_path/"build"
-    src = env.venv_path/"src"
-    assert not exists(build), "build/ dir still exists: %s" % build
-    assert exists(src), "expected src/ dir doesn't exist: %s" % src
+    # Install an editable to create a src/ dir
+    script.pip(
+        "install", "-e",
+        (local_checkout("git+http://github.com/pypa/pip-test-package.git") +
+            "#egg=pip-test-package"),
+    )
 
-    # Make the bundle.
-    fspkg = 'file://%s/FSPkg' %join(tests_data, 'packages')
-    pkg_lines = textwrap.dedent('''\
+    build_dir = script.virtualenv.join("build")
+    src_dir = script.virtualenv.join("src")
+
+    assert not build_dir.check(), "build/ dir still exists: %s" % build_dir
+    assert src_dir.check(), "expected src/ dir doesn't exist: %s" % src_dir
+
+    # Make the bundle
+    fspkg = path_to_url2(os.path.join(tests_data, "packages", "FSPkg"))
+    req_path = str(tmpdir.join("bundle-req.txt"))
+    bundle_path = str(tmpdir.join("test.pybundle"))
+    ctx = (
+        fspkg,
+        local_checkout("svn+http://svn.colorstudy.com/INITools/trunk"),
+    )
+
+    with open(req_path, "w") as req:
+        req.write(textwrap.dedent("""
             -e %s
             -e %s#egg=initools-dev
-            pip''' % (fspkg, local_checkout('svn+http://svn.colorstudy.com/INITools/trunk')))
-    write_file('bundle-req.txt', pkg_lines)
-    run_pip('bundle', '-r', 'bundle-req.txt', 'test.pybundle')
-    build_bundle = env.scratch_path/"build-bundle"
-    src_bundle = env.scratch_path/"src-bundle"
-    assert not exists(build_bundle), "build-bundle/ dir still exists: %s" % build_bundle
-    assert not exists(src_bundle), "src-bundle/ dir still exists: %s" % src_bundle
-    env.assert_no_temp()
+            pip
+        """ % ctx))
+
+    script.pip("bundle", "-r", req_path, bundle_path)
+
+    assert not os.listdir(script.cwd)
 
     # Make sure previously created src/ from editable still exists
-    assert exists(src), "expected src dir doesn't exist: %s" % src
+    assert src_dir.check(), "expected src/ dir doesn't exist: %s" % src_dir
