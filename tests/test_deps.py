695b1e5df0ffa498f7a323232eabeca0a9553c1d
Enrico <rico.bl@gmail.com>
2013-08-18 12:45:52 -0300

    Added support for URL requirements on `deps` command.

--- tests/test_deps.py
+++ tests/test_deps.py
@@ -6,10 +6,13 @@ from tests.test_pip import reset_env, run_pip, here
 from pip.basecommand import ERROR, SUCCESS
 
 
+def _run_local_deps(*args, **kwargs):
+    find_links = 'file://' + os.path.join(here, 'packages/deps')
+    return _run_deps('-f', find_links, *args, **kwargs)
+
 def _run_deps(*args, **kwargs):
     kwargs['expect_stderr'] = True
-    find_links = 'file://' + os.path.join(here, 'packages/deps')
-    return run_pip('deps', '--no-index', '-f', find_links, *args, **kwargs)
+    return run_pip('deps', '--no-index', *args, **kwargs)
 
 
 class TestDepsCommandWithASinglePackage(TestCase):
@@ -17,7 +20,7 @@ class TestDepsCommandWithASinglePackage(TestCase):
     @classmethod
     def setupClass(cls):
         reset_env()
-        cls.result = _run_deps('dependency')
+        cls.result = _run_local_deps('dependency')
 
     def test_exits_with_success(self):
         self.assertEqual(self.result.returncode, SUCCESS)
@@ -37,7 +40,7 @@ class TestDepsCommandWithDependencies(TestCase):
     @classmethod
     def setupClass(cls):
         reset_env()
-        cls.result = _run_deps('dependant')
+        cls.result = _run_local_deps('dependant')
 
     def test_returns_version_info(self):
         assert 'dependant==1.0' in self.result.stdout
@@ -52,9 +55,23 @@ class TestDepsCommandWithNonExistentPackage(TestCase):
     @classmethod
     def setupClass(cls):
         reset_env()
-        cls.result = _run_deps('non-existent', expect_error=True)
+        cls.result = _run_local_deps('non-existent', expect_error=True)
 
     def test_exits_with_error(self):
         self.assertEqual(self.result.returncode, ERROR)
+
+
+class TestDepsWithURL(TestCase):
+
+    @classmethod
+    def setupClass(cls):
+        reset_env()
+        path = os.path.join(here, 'packages/deps/dependency-1.0.tar.gz')
+        cls.url = 'file://' + path
+        cls.result = _run_deps(cls.url)
+
+    def test_returns_dependency_url(self):
+        assert self.url in self.result.stdout
+
     def test_exits_with_success(self):
-        self.assertEqual(self.result.returncode, ERROR)
+        self.assertEqual(self.result.returncode, SUCCESS)
