86f5ebb4cae3965dc7862e3d6748e8c321dc937a
Donald Stufft <donald@stufft.io>
2013-08-24 05:14:06 -0400

    Work around a Python 3.2 bug with pytest and pytest-xdist

--- tests/conftest.py
+++ tests/conftest.py
@@ -73,3 +73,5 @@ def script(tmpdir, virtualenv):
 @pytest.fixture
 def data(tmpdir):
     return TestData.copy(tmpdir.join("data"))
+
+# This is here to work around a bug with pytest, pytest-xdist, and Python 3.2
