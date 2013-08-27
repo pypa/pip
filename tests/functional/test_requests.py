20c52fdaf8f0eaefd9d857b37d89e7b429cc3013
Donald Stufft <donald@stufft.io>
2013-08-27 10:52:55 -0400

    Update requests test for the new funcargs

--- tests/functional/test_requests.py
+++ tests/functional/test_requests.py
@@ -1,9 +1,5 @@
-from tests.lib import run_pip, reset_env
-
-
-def test_timeout():
-    reset_env()
-    result = run_pip("--timeout", "0.01", "install", "-vvv", "INITools",
+def test_timeout(script):
+    result = script.pip("--timeout", "0.01", "install", "-vvv", "INITools",
         expect_error=True,
     )
     assert "Could not fetch URL https://pypi.python.org/simple/INITools/: timed out" in result.stdout
