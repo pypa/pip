56c8674fde784d9f492f46affdcf90e25f109492
Matthew Iversen <teh.ivo@gmail.com>
2014-01-27 05:45:57 +1100

    Use latin encoding to store filestrings

--- contrib/packager/template.py
+++ contrib/packager/template.py
@@ -3,7 +3,6 @@
 sources = """
 @SOURCES@"""
 
-import codecs
 import os
 import sys
 import base64
@@ -11,35 +10,40 @@ import bz2
 import tempfile
 import shutil
 
-def unpack(sources):
-    temp_dir = tempfile.mkdtemp('-scratchdir', 'unpacker-')
+
+def unpack(sources, into_dir):
     for package, content in sources.items():
         filepath = package.split("/")
         dirpath = os.sep.join(filepath[:-1])
-        packagedir = os.path.join(temp_dir, dirpath)
+        packagedir = os.path.join(into_dir, dirpath)
         if not os.path.isdir(packagedir):
             os.makedirs(packagedir)
         mod = open(os.path.join(packagedir, filepath[-1]), 'wb')
         try:
-            mod.write(base64.b64decode(content))
+            if sys.version_info >= (3, 0):
+                content = content.encode('latin1')
+            mod.write(content)
         finally:
             mod.close()
-    return temp_dir
 
 
 if __name__ == "__main__":
     if sys.version_info >= (3, 0):
         exec("def do_exec(co, loc): exec(co, loc)\n")
         import pickle
-        sources = sources.encode("ascii") # ensure bytes
-        sources = pickle.loads(bz2.decompress(base64.decodebytes(sources)))
+        sources = sources.encode("ascii")  # ensure bytes
+        up = lambda s: pickle.loads(s, encoding='latin1')
+        b64decode = base64.decodebytes
     else:
         import cPickle as pickle
         exec("def do_exec(co, loc): exec co in loc\n")
-        sources = pickle.loads(bz2.decompress(base64.decodestring(sources)))
+        up = lambda s: pickle.loads(s)
+        b64decode = base64.decodestring
+    sources = up(bz2.decompress(b64decode(sources)))
 
     try:
-        temp_dir = unpack(sources)
+        temp_dir = tempfile.mkdtemp('-scratchdir', 'unpacker-')
+        unpack(sources, temp_dir)
         sys.path.insert(0, temp_dir)
 
         entry = """@ENTRY@"""
