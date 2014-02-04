e430b58cebff963b457f06a5f6bcb047e7eeb36f
dongweiming <ciici1234@hotmail.com>
2014-02-03 21:14:43 -0500

    Bugfix: when not find a version that satisfies the requirement. show all versions but duplicate

--- pip/index.py
+++ pip/index.py
@@ -348,10 +348,11 @@ class PackageFinder(object):
                 '(from versions: %s)' %
                 (
                     req,
-                    ', '.join([
-                        version
-                        for parsed_version, link, version in all_versions
-                    ])
+                    ', '.join(
+                        sorted(list(set([
+                            version
+                            for parsed_version, link, version in all_versions
+                        ]))))
                 )
             )
 
