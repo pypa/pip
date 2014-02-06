777953777e63d3111ff70c15559a541c2127751e
Matthew Iversen <teh.ivo@gmail.com>
2014-02-07 03:41:05 +1100

    Make main even simpler

--- pip/__main__.py
+++ pip/__main__.py
@@ -1,16 +1,4 @@
-import os
-import sys
-
-
-def run_main():
-    # Put the pip relative to this file first on sys.path so that it would be
-    # run with first priority.
-    base = os.path.dirname(
-        os.path.dirname(os.path.abspath(__file__))
-    )
-    sys.path.insert(0, base)
+if __name__ == '__main__':
+    import sys
     import pip
     sys.exit(pip.main())
-
-if __name__ == '__main__':
-    run_main()
