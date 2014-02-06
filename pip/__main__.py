2e8f1917c81f4b18b53e4a9eb449cd6dac297796
Matthew Iversen <teh.ivo@gmail.com>
2014-02-06 23:50:01 +1100

    Simplify __main__ logic
    runner.py seemed to exist for the sole function of containing
    a simple function that was called by __main__.py.
    
    I've simply removed that file with its function moved into
    __main__.py itself; no functionality should be expected to change.

--- pip/__main__.py
+++ pip/__main__.py
@@ -1,7 +1,16 @@
+import os
 import sys
-from .runner import run
+
+
+def run_main():
+    # Put the pip relative to this file first on sys.path so that it would be
+    # run with first priority.
+    base = os.path.dirname(
+        os.path.dirname(os.path.abspath(__file__))
+    )
+    sys.path.insert(0, base)
+    import pip
+    sys.exit(pip.main())
 
 if __name__ == '__main__':
-    exit = run()
-    if exit:
-        sys.exit(exit)
+    run_main()
