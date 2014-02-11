387058675ec2f68cafa386a78ca7f0e82a624a42
Marc Abramowitz <marc@marc-abramowitz.com>
2014-02-11 07:11:24 -0800

    Check self.ignore_installed before doing requirement.check_if_exists

--- pip/req/req_set.py
+++ pip/req/req_set.py
@@ -625,7 +625,8 @@ class RequirementSet(object):
                         # distribute wasn't installed, so nothing to do
                         pass
 
-                requirement.check_if_exists()
+                if not self.ignore_installed:
+                    requirement.check_if_exists()
 
                 if requirement.conflicts_with:
                     logger.notify('Found existing installation: %s'
