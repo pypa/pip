0bbfaf6f06e47c4c550a9cf0a11d13b86558fa40
Matthew Iversen <teh.ivo@gmail.com>
2014-02-19 18:44:44 +1100

    Expand docstring for check_compatibility

--- pip/wheel.py
+++ pip/wheel.py
@@ -418,9 +418,16 @@ def wheel_version(source_dir):
 
 def check_compatibility(version, name):
     """
-    Raises errors or warns if called with an incompatible wheel version
+    Raises errors or warns if called with an incompatible Wheel-Version.
 
+    Pip should refuse to install a Wheel-Version that's a major series
+    ahead of what it's compatible with (e.g 2.0 > 1.1); and warn when
+    installing a version only minor version ahead (e.g 1.2 > 1.1).
+
+    version: a 2-tuple representing a Wheel-Version (Major, Minor)
     name: name of wheel or package to raise exception about
+
+    :raises UnsupportedWheel: when an incompatible Wheel-Version is given
     """
     if not version:
         raise UnsupportedWheel(
