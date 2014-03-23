be4da97b40637c7d34f18857691f12801d79394f
Jannis Leidel <jannis@leidel.info>
2014-03-23 15:22:57 +0100

    Use the deadsnakes PPA for a not so dead snake (3.4).

--- .travis/py34.sh
+++ .travis/py34.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
-
-# Get the Source Code
-cd ..
-hg clone http://hg.python.org/cpython
-
-# Build Python
-cd cpython
-./configure
-make -j8
-sudo make install
+sudo add-apt-repository ppa:fkrull/deadsnakes
+sudo apt-get update
+sudo apt-get install python3.4
