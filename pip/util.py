f355623a70074b7da4506a301f6ca2c2f2e7c89e
RobberPhex <robberphex@gmail.com>
2014-04-23 02:28:37 +0800

    rmtree_errorhandler will delete read-only file

--- pip/util.py
+++ pip/util.py
@@ -52,19 +52,15 @@ def rmtree_errorhandler(func, path, exc_info):
     """On Windows, the files in .svn are read-only, so when rmtree() tries to
     remove them, an exception is thrown.  We catch that here, remove the
     read-only attribute, and hopefully continue without problems."""
-    exctype, value = exc_info[:2]
-    if not ((exctype is WindowsError and value.args[0] == 5) or  # others
-            (exctype is OSError and value.args[0] == 13) or  # python2.4
-            (exctype is PermissionError and value.args[3] == 5)  # python3.3
-            ):
+    # if file type currently read only
+    if os.stat(path).st_mode & stat.S_IREAD:
+        # convert to read/write
+        os.chmod(path, stat.S_IWRITE)
+        # use the original function to repeat the operation
+        func(path)
+        return
+    else:
         raise
-    # file type should currently be read only
-    if ((os.stat(path).st_mode & stat.S_IREAD) != stat.S_IREAD):
-        raise
-    # convert to read/write
-    os.chmod(path, stat.S_IWRITE)
-    # use the original function to repeat the operation
-    func(path)
 
 
 def display_path(path):
