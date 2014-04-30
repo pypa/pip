4928f983f33b5b991815d9e549c7be2df2ab1c54
Alex Gaynor <alex.gaynor@gmail.com>
2014-04-29 22:23:19 -0700

    The PyPy on travis is also a recent PyPy

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,14 +3,6 @@
 set -e
 set -x
 
-case $TOXENV in
-    pypy)
-        sudo add-apt-repository -y ppa:pypy/ppa
-        sudo apt-get -y update
-        sudo apt-get -y install pypy
-        ;;
-esac
-
 git config --global user.email "python-virtualenv@googlegroups.com"
 git config --global user.name "Pip"
 
