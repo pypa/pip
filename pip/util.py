48266149fbd189045f1254ba1e5b3a67a68e570e
Alex Gaynor <alex.gaynor@gmail.com>
2014-05-02 09:10:34 -0700

    Random change to trigger tests
    But for real, oxford comma best comma.

--- pip/util.py
+++ pip/util.py
@@ -757,7 +757,7 @@ def read_text_file(filename):
     """Return the contents of *filename*.
 
     Try to decode the file contents with utf-8, the preferred system encoding
-    (e.g., cp1252 on some Windows machines) and latin1, in that order. Decoding
+    (e.g., cp1252 on some Windows machines), and latin1, in that order. Decoding
     a byte string with latin1 will never raise an error. In the worst case, the
     returned string will contain some garbage characters.
 
