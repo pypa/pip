93098d0c6781dd7019dfa43d9030f14b28f1bf2d
Donald Stufft <donald@stufft.io>
2014-05-03 00:03:23 -0400

    Bump version

--- pip/__init__.py
+++ pip/__init__.py
@@ -19,7 +19,7 @@ import pip.cmdoptions
 cmdoptions = pip.cmdoptions
 
 # The version as used in the setup.py and the docs conf.py
-__version__ = "1.5.4"
+__version__ = "1.5.5"
 
 
 def autocomplete():
