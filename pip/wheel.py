4950c7685c0f6f368d46bd4ccf42807d023ef286
INADA Naoki <songofacandy@gmail.com>
2014-06-07 12:20:42 +0900

    Fix flake8 error.
    Github's online editor is bad...

--- pip/wheel.py
+++ pip/wheel.py
@@ -241,7 +241,7 @@ def move_wheel_files(name, req, wheeldir, user=False, home=None, root=None,
             clobber(source, dest, False, fixer=fixer, filter=filter)
 
     maker = ScriptMaker(None, scheme['scripts'])
-    
+
     # Ensure old scripts are overwritten.
     # See https://github.com/pypa/pip/issues/1800
     maker.clobber = True
