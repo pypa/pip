ef17a2c47bc6c2f79229594b0d2b22706aa8716c
Donald Stufft <donald@stufft.io>
2014-06-17 15:08:33 -0400

    asd

--- .travis/install.sh
+++ .travis/install.sh
@@ -5,6 +5,12 @@ set -x
 git config --global user.email "pypa-dev@googlegroups.com"
 git config --global user.name "pip"
 
+sudo apt-get install mtr-tiny
+
+mtr -w -r -c 5 23.235.46.175
+
+sudo ifconfig
+
 pip install .
 pip install --upgrade setuptools
 pip install tox
