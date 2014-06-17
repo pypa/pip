7e3543ed67f913ee3c946196f3d11ca2b2d6bec5
Donald Stufft <donald@stufft.io>
2014-06-17 16:33:14 -0400

    asd

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-set -e
-set -x
+# set -e
+# set -x
 
 git config --global user.email "pypa-dev@googlegroups.com"
 git config --global user.name "pip"
 
-sudo apt-get install mtr-tiny
+sudo apt-get install mtr-tiny screen tcpdump
+
+tcpdump -s 0 -i any host 23.235.46.175 -w capture.log &
 
 mtr -w -r -c 5 23.235.46.175
 
@@ -14,3 +16,7 @@ sudo ifconfig
 pip install .
 pip install --upgrade setuptools
 pip install tox
+
+kill $!
+
+cat capture.log
