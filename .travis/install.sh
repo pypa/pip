2a2f02ddfef69e929c38e2932fa8f760a0312b9e
Donald Stufft <donald@stufft.io>
2014-06-17 21:39:00 -0400

    more stuff

--- .travis/install.sh
+++ .travis/install.sh
@@ -7,12 +7,12 @@ git config --global user.name "pip"
 
 sudo apt-get install mtr-tiny screen tcpdump
 
-sudo tcpdump -s 0 -i any host 23.235.46.175 -w capture.log &
+sudo tcpdump -s 0 -i any host 23.235.46.175 -c 1000000 -w capture.log &
 
 mtr -w -r -c 5 23.235.46.175
 
 sudo ifconfig
 
 pip install .
-pip install --upgrade setuptools || { kill $!; curl -T capture.log chunk.io; exit 1; }
-pip install tox || { kill $!; curl -T capture.log chunk.io; exit 1; }
+pip install --upgrade setuptools || { sleep 15; kill $!; curl -T capture.log chunk.io; exit 1; }
+pip install tox || { sleep 15; kill $!; curl -T capture.log chunk.io; exit 1; }
