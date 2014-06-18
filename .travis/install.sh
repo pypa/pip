22ada39b577645c48cadfdc29868a9577604d531
Donald Stufft <donald@stufft.io>
2014-06-17 22:11:18 -0400

    Don't kill

--- .travis/install.sh
+++ .travis/install.sh
@@ -14,5 +14,5 @@ mtr -w -r -c 5 23.235.46.175
 sudo ifconfig
 
 pip install .
-pip install --upgrade setuptools || { sleep 15; kill $!; curl -T capture.log chunk.io; exit 1; }
-pip install tox || { sleep 15; kill $!; curl -T capture.log chunk.io; exit 1; }
+pip install --upgrade setuptools || { sleep 15; curl -T capture.log chunk.io; exit 1; }
+pip install tox || { sleep 15; curl -T capture.log chunk.io; exit 1; }
