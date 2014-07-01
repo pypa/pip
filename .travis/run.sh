3cd6032b5c02c33ff95c80edd39c93973a24a735
Donald Stufft <donald@stufft.io>
2014-06-30 23:17:27 -0400

    Use diff-cover to show the change in coverage

--- .travis/run.sh
+++ .travis/run.sh
@@ -7,7 +7,7 @@ set -x
 export LC_CTYPE=en_US.UTF-8
 
 # Run the unit tests
-tox -- -m unit
+tox -- -m unit --cov-report coverage.xml
 
 # Run our integration tests, typically with pytest-xdist to speed things up
 # except on Python 3.2 where it doesn't work quite right.
@@ -19,3 +19,12 @@ case $TOXENV in
         tox -- -m integration -n 8
         ;;
 esac
+
+
+# If this is a pull request then run our diff-cover to get the difference in
+# coverage that this PR introduces
+if [[ $TRAVIS_PULL_REQUEST != "false" ]]
+then
+    git fetch origin $TRAVIS_BRANCH:refs/remotes/origin/$TRAVIS_BRANCH
+    diff-cover --compare-branch=origin/$TRAVIS_BRANCH coverage.xml
+fi
