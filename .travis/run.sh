a6b59e8a558c4551729be5823fbb4c9cfb3d97f6
Donald Stufft <donald@stufft.io>
2014-06-30 23:31:27 -0400

    Remove some noise in the output

--- .travis/run.sh
+++ .travis/run.sh
@@ -7,7 +7,7 @@ set -x
 export LC_CTYPE=en_US.UTF-8
 
 # Run the unit tests
-tox -- -m unit --cov-report coverage.xml
+tox -- -m unit --cov pip/ --cov-report coverage.xml
 
 # Run our integration tests, typically with pytest-xdist to speed things up
 # except on Python 3.2 where it doesn't work quite right.
@@ -21,10 +21,14 @@ case $TOXENV in
 esac
 
 
-# If this is a pull request then run our diff-cover to get the difference in
-# coverage that this PR introduces
 if [[ $TRAVIS_PULL_REQUEST != "false" ]]
 then
+    # If this is a pull request then run our diff-cover to get the difference
+    # in coverage that this PR introduces
     git fetch origin $TRAVIS_BRANCH:refs/remotes/origin/$TRAVIS_BRANCH
     diff-cover --compare-branch=origin/$TRAVIS_BRANCH coverage.xml
+else
+    # If this is not a PR, but is being run against a branch, then just report
+    # the coverage results for the entire code base.
+    coverage report -m
 fi
