0dedf2b6f5adefcc29d3d295318a7ebc916fc822
Antonio Alvarado Hernandez <tnotstar@gmail.com>
2014-07-04 00:25:21 +0200

    Passing headers per-request in PipXmlrpcTransport
    * Changed `PipXmlrpcTransport` to pass headers at request scope
    * Added summary line to the changelog

--- pip/download.py
+++ pip/download.py
@@ -706,14 +706,15 @@ class PipXmlrpcTransport(xmlrpc_client.Transport):
         xmlrpc_client.Transport.__init__(self, use_datetime)
         index_parts = urlparse.urlparse(index_url)
         self._scheme = index_parts.scheme
-        session.headers.update({'Content-Type': 'text/xml'})
         self._session = session
 
     def request(self, host, handler, request_body, verbose=False):
         parts = (self._scheme, host, handler, None, None, None)
         url = urlparse.urlunparse(parts)
         try:
-            response = self._session.post(url, data=request_body, stream=True)
+            headers = {'Content-Type': 'text/xml'}
+            response = self._session.post(url, data=request_body,
+                                          headers=headers, stream=True)
             response.raise_for_status()
             self.verbose = verbose
             return self.parse_response(response.raw)
