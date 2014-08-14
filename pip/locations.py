9a1a69fbaab883a9b0d55a9bdfcacff5095de792
Krzysztof Żelechowski <krzysztof.zelechowski@syncron.com>
2014-08-14 14:18:48 +0200

    getattr needs a default argument

--- pip/locations.py
+++ pip/locations.py
@@ -103,7 +103,7 @@ def virtualenv_no_global():
 
 def __get_username():
     """ Returns the effective username of the current process. """
-    if sys.platform == 'win32' or getattr (os, '_name') and os._name == 'nt':
+    if sys.platform == 'win32' or getattr (os, '_name', str ()) == 'nt':
         return getpass.getuser()
     import pwd
     return pwd.getpwuid(os.geteuid()).pw_name
