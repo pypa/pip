ffb2ddee4f8044ae2988b95f2ee91d4aa7e9f52f
Kevin Burke <kev@inburke.com>
2014-09-10 14:13:35 -0700

    Dont fallback to sitewide imports

--- pip/_vendor/cachecontrol/compat.py
+++ pip/_vendor/cachecontrol/compat.py
@@ -12,12 +12,5 @@ except ImportError:
 
 # Handle the case where the requests has been patched to not have urllib3
 # bundled as part of it's source.
-try:
-    from pip._vendor.requests.packages.urllib3.response import HTTPResponse
-except ImportError:
-    from urllib3.response import HTTPResponse
-
-try:
-    from pip._vendor.requests.packages.urllib3.util import is_fp_closed
-except ImportError:
-    from urllib3.util import is_fp_closed
+from pip._vendor.requests.packages.urllib3.response import HTTPResponse
+from pip._vendor.requests.packages.urllib3.util import is_fp_closed
