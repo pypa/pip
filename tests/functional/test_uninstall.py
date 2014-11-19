5f0692b40a715619a1c9b021f3e15d36cb09d1f0
Donald Stufft <donald@stufft.io>
2014-11-19 17:05:14 -0500

    Fix for latest setuptools

--- tests/functional/test_uninstall.py
+++ tests/functional/test_uninstall.py
@@ -283,7 +283,7 @@ def test_uninstall_as_egg(script, data):
     to_install = data.packages.join("FSPkg")
     result = script.pip('install', to_install, '--egg', expect_error=False)
     fspkg_folder = script.site_packages / 'fspkg'
-    egg_folder = script.site_packages / 'FSPkg-0.1dev-py%s.egg' % pyversion
+    egg_folder = script.site_packages / 'FSPkg-0.1.dev0-py%s.egg' % pyversion
     assert fspkg_folder not in result.files_created, str(result.stdout)
     assert egg_folder in result.files_created, str(result)
 
