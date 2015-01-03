eccc0e60ff103e3112a6fecc949c54bebeae3953
Donald Stufft <donald@stufft.io>
2015-01-03 02:32:44 -0500

    Skip the ownership check if we don't have os.geteuid()

--- pip/utils/filesystem.py
+++ pip/utils/filesystem.py
@@ -1,9 +1,15 @@
+import os
 import os.path
 
 from pip.compat import get_path_uid
 
 
 def check_path_owner(path, uid):
+    # If we don't have a way to check the effective uid of this process, then
+    # we'll just assume that we own the directory.
+    if not hasattr(os, "geteuid"):
+        return True
+
     previous = None
     while path != previous:
         if os.path.lexists(path):
