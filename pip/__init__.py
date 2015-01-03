499eee2e35e65c9a5f2a11e9f090bb48e198e2f6
Donald Stufft <donald@stufft.io>
2015-01-03 00:52:26 -0500

    Bump version for 6.0.4 development

--- pip/__init__.py
+++ pip/__init__.py
@@ -30,7 +30,7 @@ import pip.cmdoptions
 cmdoptions = pip.cmdoptions
 
 # The version as used in the setup.py and the docs conf.py
-__version__ = "6.0.3"
+__version__ = "6.0.4.dev0"
 
 
 logger = logging.getLogger(__name__)
