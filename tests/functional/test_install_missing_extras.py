8a094361e7a1ce80ed13de56ae4f119f1df0e563
derwolfe <chriswwolfe@gmail.com>
2015-02-24 22:08:38 -0600

    fix formatting in tests, comments

--- tests/functional/test_install_missing_extras.py
+++ tests/functional/test_install_missing_extras.py
@@ -2,12 +2,13 @@
 temporarily locate test here to make it fail faster
 """
 
+
 def test_non_existant_extra_warns_user_no_wheel(script, data):
     """
     A warning is logged telling the user that the extra option they requested
     does not exist in the project they are wishing to install.
 
-    This is meant to exercise the code that is meant for non-wheel installs.
+    This exercises source installs.
     """
     result = script.pip(
         'install', '--no-use-wheel', '--no-index',
@@ -19,10 +20,13 @@ def test_non_existant_extra_warns_user_no_wheel(script, data):
         in result.stdout
     )
 
+
 def test_non_existant_extra_warns_user_with_wheel(script, data):
     """
     A warning is logged telling the user that the extra option they requested
-    does not exist in the project they are wishing to install
+    does not exist in the project they are wishing to install.
+
+    This exercises wheel installs.
     """
     result = script.pip(
         'install', '--use-wheel', '--no-index',
@@ -34,7 +38,8 @@ def test_non_existant_extra_warns_user_with_wheel(script, data):
         in result.stdout
     )
 
-def test_non_existant_options_logged_as_single_list_per_dependency(script, data):
+
+def test_non_existant_options_listed_in_order(script, data):
     """
     Warn the user for each extra that doesn't exist.
     """
@@ -43,11 +48,8 @@ def test_non_existant_options_logged_as_single_list_per_dependency(script, data)
         '--find-links=' + data.find_links,
         'simplewheel[nonexistant, nope]', expect_stderr=True,
     )
-    assert (
-        "simplewheel 2.0 does not provide the extra 'nonexistant'"
-        in result.stdout
+    msg = (
+        "  simplewheel 2.0 does not provide the extra 'nonexistant'\n"
+        "  simplewheel 2.0 does not provide the extra 'nope'"
     )
-    assert (
-        "simplewheel 2.0 does not provide the extra 'nope'"
-        in result.stdout
-    )
+    assert msg in result.stdout
