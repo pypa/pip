b5d84a0dbad869b9f79368010f96adc5f91a0053
Thomas Kluyver <takowl@gmail.com>
2015-03-20 16:18:43 -0700

    Fix monkeypatching of is_local()

--- tests/unit/test_req_uninstall.py
+++ tests/unit/test_req_uninstall.py
@@ -15,7 +15,7 @@ def mock_is_local(path):
 
 class TestUninstallPathSet(object):
     def test_add(self, tmpdir, monkeypatch):
-        monkeypatch.setattr(pip.utils, 'is_local', mock_is_local)
+        monkeypatch.setattr(pip.req.req_uninstall, 'is_local', mock_is_local)
         file_extant = os.path.join(tmpdir, 'foo')
         file_nonexistant = os.path.join(tmpdir, 'nonexistant')
         with open(file_extant, 'w'):
@@ -31,7 +31,7 @@ class TestUninstallPathSet(object):
 
     @pytest.mark.skipif("sys.platform == 'win32'")
     def test_add_symlink(self, tmpdir, monkeypatch):
-        monkeypatch.setattr(pip.utils, 'is_local', mock_is_local)
+        monkeypatch.setattr(pip.req.req_uninstall, 'is_local', mock_is_local)
         f = os.path.join(tmpdir, 'foo')
         with open(f, 'w'):
             pass
