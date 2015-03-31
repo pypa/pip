d0516691491e3f6c005fd81624a95189050de9a8
Marcus Smith <qwcode@gmail.com>
2015-03-30 21:11:32 -0700

    create new test module for req_file.py and stub out the classes

---
+++ tests/unit/test_req_file.py
@@ -0,0 +1,39 @@
+from pip.req.req_file import (parse_requirement_options, parse_content,
+                              parse_line, join_lines, ignore_comments,
+                              partition_line, REQUIREMENT_EDITABLE, REQUIREMENT,
+                              FLAG, OPTION)
+
+
+class TestIgnoreComments(object):
+    """tests for `ignore_comment`"""
+    pass
+
+
+class TestPartitionLine(object):
+    """tests for `partition_line`"""
+    pass
+
+
+class TestJoinLines(object):
+    """tests for `join_lines`"""
+    pass
+
+
+class TestParseRequirementOptions(object):
+    """tests for `parse_requirement_options`"""
+    pass
+
+
+class TestParseLine(object):
+    """tests for `parse_line`"""
+    pass
+
+
+class TestParseContent(object):
+    """tests for `parse_content`"""
+    pass
+
+
+class TestParseRequirements(object):
+    """tests for `parse_requirements`"""
+    pass
