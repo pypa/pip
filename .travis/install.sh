717cfc6ac05fbd83adc639a7bbd5111fdfa792ec
Donald Stufft <donald@stufft.io>
2015-06-09 18:31:12 -0400

    Don't run diff-cover anymore

--- .travis/install.sh
+++ .travis/install.sh
@@ -6,7 +6,7 @@ git config --global user.email "pypa-dev@googlegroups.com"
 git config --global user.name "pip"
 
 pip install --upgrade setuptools
-pip install coverage diff_cover tox
+pip install --upgrade tox
 
 # If we're running under Python 3.5, then we need to actually go and install
 # Python 3.5.
