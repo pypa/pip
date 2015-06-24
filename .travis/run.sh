2912910b44e7f1123fc014be3c377fa8ee08c44d
Donald Stufft <donald@stufft.io>
2015-06-23 20:37:21 -0400

    Do a (slightly) less insane thing to handle debundling
    Previously we attempt to do some crazy things with import hooks
    in order to attempt to automatically alias normally installed
    dependencies as our vendored dependencies. This turned out to be
    fairly fragile, so instead we'll manually patch sys.modules to
    trigger the aliasing.
    
    As part of this, we also drop support for the
    PIP_NO_VENDOR_FOR_DOWNSTREAM environment variable because it was
    never fully supported and now that we have wheel caching, actually
    using it could possibly trigger a bad wheel to be cached. The new
    mechanism requires some light patching by downstream to opt into
    the mechanism, so they can also easily remove all of the files
    in pip/_vendor/ except for __init__.py.

--- .travis/run.sh
+++ .travis/run.sh
@@ -19,15 +19,24 @@ if [[ $VENDOR = "no" ]]; then
         .tox/$TOXENV/bin/pip install -r pip/_vendor/vendor.txt --no-deps
     fi
 
-    # Actually install pip without the bundled dependencies
-    PIP_NO_VENDOR_FOR_DOWNSTREAM=1 .tox/$TOXENV/bin/pip install .
-
     # Install our dependencies if we're installing from wheels
     if [[ $WHEELS = "yes" ]]; then
         mkdir -p /tmp/wheels
         pip wheel --wheel-dir /tmp/wheels --no-deps -r pip/_vendor/vendor.txt
         cp /tmp/wheels/* `echo .tox/$TOXENV/lib/python*/site-packages/pip/_vendor/`
     fi
+
+    # Remove the vendored dependencies from within the installed pip inside of
+    # our installed copy of pip.
+    find .tox/$TOXENV/lib/python*/site-packages/pip/_vendor -d \
+        -not -regex '.*/pip/_vendor/__init__\.py$' \
+        -not -regex '.*/pip/_vendor$' \
+        -exec rm -rf {} \;
+
+    # Patch our installed pip/_vendor/__init__.py so that it knows to look for
+    # the vendored dependencies instead of only looking for the vendored.
+    sed -i 's/DEBUNDLED = False/DEBUNDLED = True/' \
+        .tox/$TOXENV/lib/python*/site-packages/pip/_vendor/__init__.py
 
     # Test to make sure that we successfully installed without vendoring
     if [ -f .tox/$TOXENV/lib/python*/site-packages/pip/_vendor/six.py ]; then
--- setup.py
+++ setup.py
@@ -7,12 +7,6 @@ from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
 
-# We support this primarily for downstream distributions. This is NOT intended
-# to be used by end users and any actual use of thus is not considered a
-# supported configuration.
-NO_VENDOR = ("PIP_NO_VENDOR_FOR_DOWNSTREAM" in os.environ)
-
-
 here = os.path.abspath(os.path.dirname(__file__))
 
 
@@ -49,24 +43,6 @@ long_description = read('README.rst')
 
 tests_require = ['pytest', 'virtualenv>=1.10', 'scripttest>=1.3', 'mock']
 
-find_excludes = ["contrib", "docs", "tests*", "tasks"]
-
-py_modules = []
-
-package_data = {}
-
-
-if NO_VENDOR:
-    find_excludes += ["pip._vendor", "pip._vendor.*"]
-    py_modules += ["pip._vendor.__init__"]
-else:
-    package_data.update({
-        "pip._vendor.certifi": ["*.pem"],
-        "pip._vendor.requests": ["*.pem"],
-        "pip._vendor.distlib._backport": ["sysconfig.cfg"],
-        "pip._vendor.distlib": ["t32.exe", "t64.exe", "w32.exe", "w64.exe"],
-    })
-
 
 setup(
     name="pip",
@@ -92,9 +68,13 @@ setup(
     author_email='python-virtualenv@groups.google.com',
     url='https://pip.pypa.io/',
     license='MIT',
-    py_modules=py_modules,
-    packages=find_packages(exclude=find_excludes),
-    package_data=package_data,
+    packages=find_packages(exclude=["contrib", "docs", "tests*", "tasks"]),
+    package_data={
+        "pip._vendor.certifi": ["*.pem"],
+        "pip._vendor.requests": ["*.pem"],
+        "pip._vendor.distlib._backport": ["sysconfig.cfg"],
+        "pip._vendor.distlib": ["t32.exe", "t64.exe", "w32.exe", "w64.exe"],
+    },
     entry_points={
         "console_scripts": [
             "pip=pip:main",
@@ -109,18 +89,3 @@ setup(
     },
     cmdclass={'test': PyTest},
 )
-
-
-if NO_VENDOR:
-    print("""
-###########################################################################
-## IMPORTANT IMPORTANT IMPORTANT IMPORTANT IMPORTANT IMPORTANT IMPORTANT ##
-###########################################################################
-## You are installing pip without the bundled dependencies.              ##
-##                                                                       ##
-## This is not an officially supported option for end users and should   ##
-## only be used by downstream redistributors such as the various Linux   ##
-## distributions. This method of installation is not as well tested by   ##
-## the pip team.                                                         ##
-###########################################################################
-""")
