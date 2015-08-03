b290e13cfa398177d6e517822f4a7e748990c8f8
Xavier Fernandez <xav.fernandez@gmail.com>
2015-08-03 23:07:07 +0200

    Allow --pre option in requirement files
    closes #1273

--- pip/req/req_file.py
+++ pip/req/req_file.py
@@ -43,6 +43,7 @@ SUPPORTED_OPTIONS = [
     cmdoptions.always_unzip,
     cmdoptions.no_binary,
     cmdoptions.only_binary,
+    cmdoptions.pre,
     cmdoptions.process_dependency_links,
     cmdoptions.trusted_host,
 ]
@@ -220,6 +221,8 @@ def process_line(line, filename, line_number, finder=None, comes_from=None,
             if os.path.exists(relative_to_reqs_file):
                 value = relative_to_reqs_file
             finder.find_links.append(value)
+        if opts.pre:
+            finder.allow_all_prereleases = True
         if opts.process_dependency_links:
             finder.process_dependency_links = True
         if opts.trusted_hosts:
