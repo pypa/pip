7b44092f062faf8a338aa1dc98b3e10cf71fa79d
Xavier Fernandez <xavier.fernandez@polyconseil.fr>
2015-09-11 17:44:30 +0200

    Remove unused user_log_dir

--- pip/utils/appdirs.py
+++ pip/utils/appdirs.py
@@ -97,38 +97,6 @@ def user_data_dir(appname, roaming=False):
     return path
 
 
-def user_log_dir(appname):
-    """
-    Return full path to the user-specific log dir for this application.
-
-        "appname" is the name of application.
-            If None, just the system directory is returned.
-
-    Typical user cache directories are:
-        Mac OS X:   ~/Library/Logs/<AppName>
-        Unix:       ~/.cache/<AppName>/log  # or under $XDG_CACHE_HOME if
-                    defined
-        Win XP:     C:\Documents and Settings\<username>\Local Settings\ ...
-                    ...Application Data\<AppName>\Logs
-        Vista:      C:\\Users\<username>\AppData\Local\<AppName>\Logs
-
-    On Windows the only suggestion in the MSDN docs is that local settings
-    go in the `CSIDL_LOCAL_APPDATA` directory. (Note: I'm interested in
-    examples of what some windows apps use for a logs dir.)
-
-    OPINION: This function appends "Logs" to the `CSIDL_LOCAL_APPDATA`
-    value for Windows and appends "log" to the user cache dir for Unix.
-    """
-    if WINDOWS:
-        path = os.path.join(user_data_dir(appname), "Logs")
-    elif sys.platform == "darwin":
-        path = os.path.join(os.path.expanduser('~/Library/Logs'), appname)
-    else:
-        path = os.path.join(user_cache_dir(appname), "log")
-
-    return path
-
-
 def user_config_dir(appname, roaming=True):
     """Return full path to the user-specific config dir for this application.
 
