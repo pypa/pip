59b44ad9867f64ab1cb78032a0c8105becab9c9e
Xavier Fernandez <xavier.fernandez@polyconseil.fr>
2015-12-06 22:04:23 +0100

    add pip list

--- .travis/install.sh
+++ .travis/install.sh
@@ -8,5 +8,7 @@ git config --global user.name "pip"
 pip install --upgrade setuptools
 pip install --upgrade tox
 
+pip list
+
 # Make sure wheel is not installed for system_site_packages virtualenvs
 pip uninstall --yes wheel || true
