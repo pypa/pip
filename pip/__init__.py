7b71ae3b7428927ad0995de4cc5c0d1a0d570caf
Donald Stufft <donald@stufft.io>
2016-01-21 14:44:30 -0500

    Bump for development

--- pip/__init__.py
+++ pip/__init__.py
@@ -30,7 +30,7 @@ import pip.cmdoptions
 cmdoptions = pip.cmdoptions
 
 # The version as used in the setup.py and the docs conf.py
-__version__ = "8.0.1"
+__version__ = "8.1.0.dev0"
 
 
 logger = logging.getLogger(__name__)
