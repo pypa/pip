a9154f9150b062dbe9cfc8fd729c85b7c54e2cbd
Francesco Montesano <franz.bergesund@gmail.com>
2016-02-01 10:02:49 +0100

    Add ``pretend`` to list of test_requires

--- setup.py
+++ setup.py
@@ -41,7 +41,8 @@ def find_version(*file_paths):
 
 long_description = read('README.rst')
 
-tests_require = ['pytest', 'virtualenv>=1.10', 'scripttest>=1.3', 'mock']
+tests_require = ['pytest', 'virtualenv>=1.10', 'scripttest>=1.3', 'mock',
+                 'pretend']
 
 
 setup(
