af675e77972afd42147056a76b98ea0aaad1042a
Nathaniel J. Smith <njs@pobox.com>
2016-02-01 12:36:49 -0800

    Make install --quiet really quiet
    Commit 5bb989993 added some code for hiding/showing the cursor when
    running on the terminal, but accidentally made it so that it always
    printed the invisible control codes, even when not on a tty or when
    --quiet was specified. This turns out to be surprisingly bad (e.g. it
    breaks the official docker python builds). So, let's not do that.
    
    Fixes gh-3418

--- pip/utils/ui.py
+++ pip/utils/ui.py
@@ -219,6 +219,11 @@ def hidden_cursor(file):
     # even via colorama. So don't even try.
     if WINDOWS:
         yield
+    # We don't want to clutter the output with control characters if we're
+    # writing to a file, or if the user is running with --quiet.
+    # See https://github.com/pypa/pip/issues/3418
+    elif not file.isatty() or logger.getEffectiveLevel() > logging.INFO:
+        yield
     else:
         file.write(HIDE_CURSOR)
         try:
