85df03455e507a977f8ecd1cade6db1b419c55f8
Nathaniel J. Smith <njs@pobox.com>
2016-03-29 21:48:05 -0700

    Add a test for handling of totally malformed glibc version strings

--- tests/unit/test_pep425tags.py
+++ tests/unit/test_pep425tags.py
@@ -1,4 +1,5 @@
 import sys
+import warnings
 from mock import patch
 from pip import pep425tags
 
@@ -178,3 +179,16 @@ class TestManylinux1Tags(object):
             assert not pep425tags.check_glibc_version(two_twenty, 2, 21)
             assert not pep425tags.check_glibc_version(two_twenty, 3, 15)
             assert not pep425tags.check_glibc_version(two_twenty, 1, 15)
+
+        # For strings that we just can't parse at all, we should warn and
+        # return false
+        for bad_string in ["asdf", "", "foo.bar"]:
+            with warnings.catch_warnings(record=True) as ws:
+                warnings.filterwarnings("always")
+                assert not pep425tags.check_glibc_version(bad_string, 2, 5)
+                for w in ws:
+                    if "Expected glibc version with" in str(w.message):
+                        break
+                else:
+                    # Didn't find the warning we were expecting
+                    assert False
