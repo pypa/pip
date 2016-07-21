d7676b2b2f91a7b99ecc773cfb5f412e42e3dca7
Xavier Fernandez <xavier.fernandez@polyconseil.fr>
2016-07-21 10:40:15 +0200

    Specify the supported python version via python_requires

--- setup.py
+++ setup.py
@@ -85,6 +85,7 @@ setup(
     },
     tests_require=tests_require,
     zip_safe=False,
+    python_requires='>=2.6,!=3.0.*,!=3.1.*,!=3.2.*',
     extras_require={
         'testing': tests_require,
     },
