d4583be6fc99b87590c5753b70eaf1e0ed36b3ff
Francesco Montesano <franz.bergesund@gmail.com>
2016-08-10 10:02:59 +0200

    test entry points with colon in name

--- tests/unit/test_wheel.py
+++ tests/unit/test_wheel.py
@@ -11,15 +11,21 @@ from pip.exceptions import InvalidWheelFilename, UnsupportedWheel
 from pip.utils import unpack_file
 
 
-def test_get_entrypoints(tmpdir):
+@pytest.mark.parametrize("console_scripts",
+                         ["pip = pip.main:pip", "pip:pip = pip.main:pip"])
+def test_get_entrypoints(tmpdir, console_scripts):
     with open(str(tmpdir.join("entry_points.txt")), "w") as fp:
         fp.write("""
             [console_scripts]
-            pip = pip.main:pip
-        """)
+            {}
+            [section]
+            common:one = module:func
+            common:two = module:other_func
+        """.format(console_scripts))
+        fp.write(console_scripts + '\n')
 
     assert wheel.get_entrypoints(str(tmpdir.join("entry_points.txt"))) == (
-        {"pip": "pip.main:pip"},
+        dict([console_scripts.split(' = ')]),
         {},
     )
 
