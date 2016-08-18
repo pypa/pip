8afd9d2c1afcf4d6e8a861d53636be1811c1a997
stepshal <nessento@openmailbox.org>
2016-08-18 20:53:52 +0700

    Remove whitespace around parameter = sign.

--- tests/data/src/sample/setup.py
+++ tests/data/src/sample/setup.py
@@ -77,7 +77,7 @@ setup(
 
     # List run-time dependencies here.  These will be installed by pip when your
     # project is installed.
-    install_requires = ['peppercorn'],
+    install_requires=['peppercorn'],
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
