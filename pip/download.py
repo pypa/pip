58c7fa95e29f77bb551d5a2537185268a7faea46
Christian Ullrich <chris@chrullrich.net>
2016-09-01 13:33:40 +0200

    Try and make pep8 happy.

--- pip/download.py
+++ pip/download.py
@@ -38,10 +38,6 @@ from pip.utils.ui import DownloadProgressBar, DownloadProgressSpinner
 from pip.locations import write_delete_marker_file
 from pip.vcs import vcs
 
-# Fails at import time.
-if sys.platform.startswith("linux"):
-    from pip._vendor import distro
-
 from pip._vendor import requests, six
 from pip._vendor.requests.adapters import BaseAdapter, HTTPAdapter
 from pip._vendor.requests.auth import AuthBase, HTTPBasicAuth
@@ -52,6 +48,10 @@ from pip._vendor.cachecontrol import CacheControlAdapter
 from pip._vendor.cachecontrol.caches import FileCache
 from pip._vendor.lockfile import LockError
 from pip._vendor.six.moves import xmlrpc_client
+
+# Fails at import time.
+if sys.platform.startswith("linux"):
+    from pip._vendor import distro
 
 
 __all__ = ['get_file_content',
