8091b7e162eb53ba21af9d03d3ef1de8dfb64a0e
Jason R. Coombs <jaraco@jaraco.com>
2016-09-18 19:30:54 -0400

    Ensure help commands match (indicated by tests)

--- pip/commands/run.py
+++ pip/commands/run.py
@@ -10,11 +10,13 @@ from pip._vendor import rwt
 class RunCommand(Command):
     """Show help for commands"""
     name = 'run'
-    usage = """
-      %prog <command>"""
+    usage = rwt.commands.help_doc
     summary = 'Run Python with dependencies loaded.'
 
     def main(self, args):
+        if ['--help'] == args:
+            return super(RunCommand, self).main(args)
+
         sys.argv[1:] = args
         rwt.run()
 
