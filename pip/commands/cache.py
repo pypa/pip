1027e4c5a86eda9dbe66041132570606efaa99ba
Tim D. Smith <git@tim-smith.us>
2016-09-19 19:58:03 -0700

    Implement `pip cache rm`

--- pip/commands/cache.py
+++ pip/commands/cache.py
@@ -2,9 +2,11 @@ from __future__ import absolute_import
 
 import logging
 import os
+from os.path import realpath
 import textwrap
 
 from pip.basecommand import Command
+from pip.exceptions import CommandError
 from pip.status_codes import SUCCESS, ERROR
 from pip.utils import format_size
 from pip.utils.filesystem import tree_statistics, find_files
@@ -19,8 +21,10 @@ class CacheCommand(Command):
 
     Subcommands:
         info: Show information about the caches.
-        list (wheel cache only): List wheels stored in the cache."""
-    actions = ["info", "list"]
+        list (wheel cache only): List wheels stored in the cache.
+        rm <filename> (wheel cache only): Remove one or more wheels from the cache.
+    """  # noqa
+    actions = ["info", "list", "rm"]
     name = "cache"
     usage = """
       %%prog [options] %s""" % "|".join(actions)
@@ -85,3 +89,39 @@ class CacheCommand(Command):
                   find_files(cache_location, "*.whl")]
         wheels.sort()
         logger.info("\n".join(wheels))
+
+    def action_rm(self, options, args):
+        if options.type != "wheel":
+            raise CommandError(
+                "pip cache rm only operates on the wheel cache.")
+        if len(args) == 0:
+            raise CommandError(
+                "Must specify the filename of (a) wheel(s) to remove.")
+        cache_location = self.get_cache_location(options.cache_dir, "wheel")
+        cache_realpath = realpath(cache_location)
+        value = SUCCESS
+        for target in args:
+            matches = find_files(cache_location, target)
+            if not matches:
+                logger.warning("No match found for %s" % target)
+                value = ERROR
+                continue
+            for match in matches:
+                # protect against symlink attacks
+                if (os.path.commonprefix([cache_realpath, realpath(match)]) !=
+                        cache_realpath):
+                    logger.warning(
+                        "Did not remove %s because it is not a child of the "
+                        "wheel cache." % realpath(match))
+                    value = ERROR
+                    continue
+                try:
+                    os.unlink(match)
+                except OSError as e:
+                    logger.warning(
+                        "Could not remove %s; %s" % (match, e))
+                    value = ERROR
+                else:
+                    logger.info("Removed %s" % match)
+
+        return value
