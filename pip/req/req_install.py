cc3e98c6501fd2f118f80371387bcbc9af0c0d10
Xavier Fernandez <xav.fernandez@gmail.com>
2016-11-02 16:04:47 +0100

    Deprecate --default-vcs option (#4053)
    Close issue #4052

--- pip/req/req_install.py
+++ pip/req/req_install.py
@@ -1168,6 +1168,11 @@ def parse_editable(editable_req, default_vcs=None):
 
     if '+' not in url:
         if default_vcs:
+            warnings.warn(
+                "--default-vcs has been deprecated and will be removed in "
+                "the future.",
+                RemovedInPip10Warning,
+            )
             url = default_vcs + '+' + url
         else:
             raise InstallationError(
