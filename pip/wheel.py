f9354a4f7bcd8c26941a6b6662631d2f55481f9a
Xavier Fernandez <xavier.fernandez@polyconseil.fr>
2016-11-11 00:41:48 +0100

    Move uninstall file selection logic to req_uninstall.py

--- pip/wheel.py
+++ pip/wheel.py
@@ -6,7 +6,6 @@ from __future__ import absolute_import
 import compileall
 import csv
 import errno
-import functools
 import hashlib
 import logging
 import os
@@ -527,40 +526,6 @@ if __name__ == '__main__':
             for f in installed:
                 writer.writerow((installed[f], '', ''))
     shutil.move(temp_record, record)
-
-
-def _unique(fn):
-    @functools.wraps(fn)
-    def unique(*args, **kw):
-        seen = set()
-        for item in fn(*args, **kw):
-            if item not in seen:
-                seen.add(item)
-                yield item
-    return unique
-
-
-# TODO: this goes somewhere besides the wheel module
-@_unique
-def uninstallation_paths(dist):
-    """
-    Yield all the uninstallation paths for dist based on RECORD-without-.pyc
-
-    Yield paths to all the files in RECORD. For each .py file in RECORD, add
-    the .pyc in the same directory.
-
-    UninstallPathSet.add() takes care of the __pycache__ .pyc.
-    """
-    from pip.utils import FakeFile  # circular import
-    r = csv.reader(FakeFile(dist.get_metadata_lines('RECORD')))
-    for row in r:
-        path = os.path.join(dist.location, row[0])
-        yield path
-        if path.endswith('.py'):
-            dn, fn = os.path.split(path)
-            base = fn[:-3]
-            path = os.path.join(dn, base + '.pyc')
-            yield path
 
 
 def wheel_version(source_dir):
