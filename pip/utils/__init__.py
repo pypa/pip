9a7768f3b4b9ed5fe35b5cb8a448653bd97b2e72
Curtis Doty <Curtis@GreenKey.net>
2016-11-20 16:23:27 -0800

    fix rollback of failed upgrade

--- pip/utils/__init__.py
+++ pip/utils/__init__.py
@@ -264,6 +264,8 @@ def renames(old, new):
     if head and tail and not os.path.exists(head):
         os.makedirs(head)
 
+    if os.path.isdir(new):
+        shutil.rmtree(new)
     shutil.move(old, new)
 
     head, tail = os.path.split(old)
