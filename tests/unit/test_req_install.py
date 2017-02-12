2a5dc163f2a6ea08d4ce855879530ccd74fbbf23
Atsushi Odagiri <aodagx@gmail.com>
2017-02-12 19:26:55 +0900

    added ut for InstallRequirement

--- tests/unit/test_req_install.py
+++ tests/unit/test_req_install.py
@@ -2,8 +2,9 @@ import os
 import tempfile
 
 import pytest
-
+import mock
 from pip.req.req_install import InstallRequirement
+from pip._vendor.packaging.requirements import Requirement
 
 
 class TestInstallRequirementBuildDirectory(object):
@@ -26,3 +27,33 @@ class TestInstallRequirementBuildDirectory(object):
             assert os.path.dirname(tmp_build_dir) == os.path.dirname(tmp_dir)
         os.rmdir(tmp_dir)
         assert not os.path.exists(tmp_dir)
+
+    @pytest.mark.parametrize(
+        "installed_requires,confirm_answer",
+        [
+            ((), None),
+            (("dummy",), 'y'),
+            pytest.mark.xfail((("dummy",), 'n')),
+        ],
+    )
+    def test_confirm_dependencies(self, installed_requires, confirm_answer):
+
+        with mock.patch('pip.req.req_install.ask') as mock_ask:
+            mock_ask.return_value = confirm_answer
+            class req(Requirement):
+                def __init__(self, key):
+                    self.key = key
+
+            class installed(object):
+                def __init__(self, requires):
+                    self._requires = [req(r) for r in requires]
+
+                def requires(self):
+                    return self._requires
+
+            comes_from = None
+            requirement = InstallRequirement(Requirement("dummy"), None)
+            installed_packages = [installed(installed_requires)]
+            assert requirement.confirm_dependencies(installed_packages)
+            if confirm_answer:
+                mock_ask.assert_called_with('Proceed (y/n)? ', ('y', 'n'))
