0c5a28bd5b160817db632b4868666f07bc07760d
Cooper Lees <me@cooperlees.com>
2017-02-26 20:19:06 -0800

    - Change the pip outdated check to use PackageFinder to find current   PyPI versions -- Replaces the always phone back to pypi.python.org JSON call -- This allows us specify a local mirror to be use to query for updated version and respects other set options -- Update tests -- Fix setup.py to include freezegun for tests

--- pip/models/index.py
+++ pip/models/index.py
@@ -7,7 +7,6 @@ class Index(object):
         self.netloc = urllib_parse.urlsplit(url).netloc
         self.simple_url = self.url_to_path('simple')
         self.pypi_url = self.url_to_path('pypi')
-        self.pip_json_url = self.url_to_path('pypi/pip/json')
 
     def url_to_path(self, path):
         return urllib_parse.urljoin(self.url, path)
