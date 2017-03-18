28d4954a50d1b67b261638e5f707f6b82f317abd
Donald Stufft <donald@stufft.io>
2017-03-18 13:38:50 -0400

    We only support Pythons with argparse on it

--- pip/compat.py
+++ pip/compat.py
@@ -105,9 +105,7 @@ def expanduser(path):
 # dist.location (py27:`sysconfig.get_paths()['stdlib']`,
 # py26:sysconfig.get_config_vars('LIBDEST')), but fear platform variation may
 # make this ineffective, so hard-coding
-stdlib_pkgs = ('python', 'wsgiref')
-if sys.version_info >= (2, 7):
-    stdlib_pkgs += ('argparse',)
+stdlib_pkgs = {"python", "Wsgiref", "argparse"}
 
 
 # windows detection, covers cpython and ironpython
