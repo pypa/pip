8a680eb11875e8d4717881cc063b522fd67736d8
Pradyun S. Gedam <pradyunsg@gmail.com>
2017-05-13 15:46:32 +0530

    :x: Add non-working test for patched configuration file

--- tests/lib/configuration_helpers.py
+++ tests/lib/configuration_helpers.py
@@ -1,8 +1,59 @@
 """Helpers for tests that check configuration
 """
 
+import os
+import textwrap
+import tempfile
 import functools
+import contextlib
+from mock import patch
+
 import pip.configuration
+from pip.utils import ensure_dir
+
+
+class ConfigurationFileIOMixin(object):
+
+    def setup(self):
+        self._patches_to_clear = []
+
+    def teardown(self):
+        for patch_obj in self._patches_to_clear:
+            patch_obj.stop()
+
+    @contextlib.contextmanager
+    def patched_file(self, kind, contents):
+        # Create a temporary file
+        _, path = tempfile.mkstemp(
+            prefix="pip_", suffix="_" + kind + "config.ini", text=True
+        )
+
+        contents = textwrap.dedent(contents)
+        ensure_dir(os.path.dirname(path))
+        with open(path, "w") as f:
+            f.write(contents)
+
+        # Convert kind to attribute
+        kind_to_attribute_mapping = {
+            "venv": "pip.configuration.venv_config_file",
+            "user": "pip.configuration.new_config_file",
+            "global": "pip.configuration.site_config_files",
+        }
+
+        # Patch the attribute
+        # FIXME: This won't work. There's subprocesses!
+        patch_to_make = patch(kind_to_attribute_mapping[kind], path)
+        patch_to_make.start()
+        self._patches_to_clear.append(patch_to_make)
+
+        yield
+
+        os.remove(path)
+
+    @staticmethod
+    def get_file_contents(path):
+        with open(path) as f:
+            return f.read()
 
 
 class ConfigurationPatchingMixin(object):
