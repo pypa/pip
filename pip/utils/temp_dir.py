d389e96249396ec28158a362dc00a73047276f7f
Pradyun S. Gedam <pradyunsg@gmail.com>
2017-06-01 17:18:16 +0530

    :pencil: Add basic documentation

--- pip/utils/temp_dir.py
+++ pip/utils/temp_dir.py
@@ -12,6 +12,27 @@ logger = logging.getLogger(__name__)
 
 class TempDirectory(object):
     """Helper class that owns and cleans up a temporary directory.
+
+    This class can be used as a context manager or as an OO representation of a
+    temporary directory.
+
+    Attributes:
+        path
+            Location to the created temporary directory or None
+        delete
+            Whether the directory should be deleted when exiting
+            (when used as a contextmanager)
+
+    Methods:
+        create()
+            Creates a temporary directory and stores its path in the path
+            attribute.
+        cleanup()
+            Deletes the temporary directory and sets path attribute to None
+
+    When used as a context manager, a temporary directory is created on
+    entering the context and, if the delete attribute is True, on exiting the
+    context the created directory is deleted.
     """
 
     def __init__(self, path=None, delete=None, kind="temp"):
@@ -38,6 +59,8 @@ class TempDirectory(object):
             self.cleanup()
 
     def create(self):
+        """Create a temporary directory and store it's path in self.path
+        """
         if self.path is not None:
             logger.debug(
                 "Skipped creation of temporary directory: {}".format(self.path)
@@ -53,6 +76,8 @@ class TempDirectory(object):
         logger.debug("Created temporary directory: {}".format(self.path))
 
     def cleanup(self):
+        """Remove the temporary directory created and reset state
+        """
         if self.path is not None and os.path.exists(self.path):
             rmtree(self.path)
         self.path = None
