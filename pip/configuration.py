daf754ae258db63658f13c5cf03301b1159d37f0
Pradyun S. Gedam <pradyunsg@gmail.com>
2017-06-13 21:40:35 +0530

    Protect get_file_to_edit from not having a load_only

--- pip/configuration.py
+++ pip/configuration.py
@@ -99,6 +99,9 @@ class Configuration(object):
     def get_file_to_edit(self):
         """Returns the file with highest priority in configuration
         """
+        assert self.load_only is not None, \
+               "Need to be specified a file to be editing"
+
         try:
             return self._get_parser_to_modify()[0]
         except IndexError:
