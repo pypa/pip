841f9cb5a0c3c561d8b1c58ae6312a32a1d9541a
Chris Jerdonek <chris.jerdonek@gmail.com>
2017-08-04 11:16:10 -0700

    Shorten if line in _set_req_to_reinstall().

--- pip/resolve.py
+++ pip/resolve.py
@@ -120,8 +120,7 @@ class Resolver(object):
         """
         # Don't uninstall the conflict if doing a user install and the
         # conflict is not a user install.
-        if not (self.use_user_site and
-                not dist_in_usersite(req.satisfied_by)):
+        if not self.use_user_site or dist_in_usersite(req.satisfied_by):
             req.conflicts_with = req.satisfied_by
         req.satisfied_by = None
 
