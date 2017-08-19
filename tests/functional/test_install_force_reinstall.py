f344f53ff9899442502b64bec4db46772f023add
Chris Jerdonek <chris.jerdonek@gmail.com>
2017-08-19 13:53:38 -0700

    Address review comments.

--- tests/functional/test_install_force_reinstall.py
+++ tests/functional/test_install_force_reinstall.py
@@ -1,18 +1,34 @@
 import pytest
 
-from tests.lib import assert_all_changes, pyversion_tuple
+from tests.lib import assert_all_changes
 
 
 def check_installed_version(script, package, expected):
-    # Python 3.3 emits a "Python 3.3 support has been deprecated" warning.
-    expect_stderr = (pyversion_tuple[:2] == (3, 3))
-    result = script.run('pip', 'show', package, expect_stderr=expect_stderr)
+    result = script.pip('show', package)
     lines = result.stdout.splitlines()
+    version = None
     for line in lines:
         if line.startswith('Version: '):
             version = line.split()[-1]
             break
     assert version == expected, 'version {} != {}'.format(version, expected)
+
+
+def check_force_reinstall(script, specifier, expected):
+    """
+    Args:
+      specifier: the requirement specifier to force-reinstall.
+      expected: the expected version after force-reinstalling.
+    """
+    result = script.pip('install', 'INITools==0.2')
+    check_installed_version(script, 'initools', '0.2')
+
+    result2 = script.pip('install', '--force-reinstall', specifier)
+    assert result2.files_updated, 'force-reinstall failed'
+    check_installed_version(script, 'initools', expected)
+
+    result3 = script.pip('uninstall', 'initools', '-y', expect_error=True)
+    assert_all_changes(result, result3, [script.venv / 'build', 'cache'])
 
 
 @pytest.mark.network
@@ -21,13 +37,7 @@ def test_force_reinstall_with_no_version_specifier(script):
     Check --force-reinstall when there is no version specifier and the
     installed version is not the newest version.
     """
-    result = script.pip('install', 'INITools==0.2')
-    check_installed_version(script, 'initools', '0.2')
-    result2 = script.pip('install', '--force-reinstall', 'INITools')
-    assert result2.files_updated, 'force-reinstall failed'
-    check_installed_version(script, 'initools', '0.3.1')
-    result3 = script.pip('uninstall', 'initools', '-y', expect_error=True)
-    assert_all_changes(result, result3, [script.venv / 'build', 'cache'])
+    check_force_reinstall(script, 'INITools', '0.3.1')
 
 
 @pytest.mark.network
@@ -36,10 +46,4 @@ def test_force_reinstall_with_same_version_specifier(script):
     Check --force-reinstall when the version specifier equals the installed
     version and the installed version is not the newest version.
     """
-    result = script.pip('install', 'INITools==0.2')
-    check_installed_version(script, 'initools', '0.2')
-    result2 = script.pip('install', '--force-reinstall', 'INITools==0.2')
-    assert result2.files_updated, 'force-reinstall failed'
-    check_installed_version(script, 'initools', '0.2')
-    result3 = script.pip('uninstall', 'initools', '-y', expect_error=True)
-    assert_all_changes(result, result3, [script.venv / 'build', 'cache'])
+    check_force_reinstall(script, 'INITools==0.2', '0.2')
