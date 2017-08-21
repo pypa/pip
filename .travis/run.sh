9ec2c1487bbc20f63c7a4e57305b2b8c0f384bc6
Pradyun S. Gedam <pradyunsg@gmail.com>
2017-08-21 20:08:54 +0530

    Try running with 4 workers

--- .travis/run.sh
+++ .travis/run.sh
@@ -44,7 +44,7 @@ if [[ $TOXENV == py* ]]; then
     # Run unit tests
     tox -- -m unit
     # Run integration tests
-    tox -- -m integration -n 8 --duration=5
+    tox -- -m integration -n 4 --duration=5
 else
     # Run once
     tox
