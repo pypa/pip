edcc6de4a092b47b3ef8aface289a20806823fb6
xoviat <xoviat@users.noreply.github.com>
2017-09-08 10:58:57 -0500

    tests: user: use common_wheels fixture

--- tests/functional/test_install_user.py
+++ tests/functional/test_install_user.py
@@ -69,12 +69,13 @@ class Tests_UserSite:
         result.assert_installed('INITools', use_user_site=True)
 
     @pytest.mark.network
-    def test_install_curdir_usersite(self, script, virtualenv, data):
+    def test_install_curdir_usersite(self, script, virtualenv, data,
+                                     common_wheels):
         """
         Test installing current directory ('.') into usersite
         """
         virtualenv.system_site_packages = True
-        script.pip("install", "wheel")
+        script.pip('install', 'wheel', '--no-index', '-f', common_wheels)
         run_from = data.packages.join("FSPkg")
         result = script.pip(
             'install', '-vvv', '--user', curdir,
