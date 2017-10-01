6de819b9d3fcee98edc75dbfc4e5c82fe362aa9d
Dustin Ingram <di@users.noreply.github.com>
2017-10-01 14:09:18 -0500

    Use get_prog in completion scripts for executable name

--- tests/functional/test_completion.py
+++ tests/functional/test_completion.py
@@ -47,7 +47,9 @@ def test_completion_for_fish(script):
     fish_completion = """\
 function __fish_complete_pip
     set -lx COMP_WORDS (commandline -o) ""
-    set -lx COMP_CWORD (math (contains -i -- (commandline -t) $COMP_WORDS)-1)
+    set -lx COMP_CWORD ( \\
+        math (contains -i -- (commandline -t) $COMP_WORDS)-1 \\
+    )
     set -lx PIP_AUTO_COMPLETE 1
     string split \\  -- (eval $COMP_WORDS[1])
 end
