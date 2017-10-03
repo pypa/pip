6ad82abb4d589381de3180d510a4dec4f014fe84
Benoit Pierre <benoit.pierre@gmail.com>
2017-10-03 22:35:31 +0200

    tests: fix `isolate` fixture on Windows

--- tests/conftest.py
+++ tests/conftest.py
@@ -77,8 +77,6 @@ def isolate(tmpdir):
     We use an autouse function scoped fixture because we want to ensure that
     every test has it's own isolated home directory.
     """
-    # TODO: Ensure Windows will respect $HOME, including for the cache
-    #       directory
 
     # TODO: Figure out how to isolate from *system* level configuration files
     #       as well as user level configuration files.
@@ -91,21 +89,36 @@ def isolate(tmpdir):
     fake_root = os.path.join(str(tmpdir), "fake-root")
     os.makedirs(fake_root)
 
-    # Set our home directory to our temporary directory, this should force all
-    # of our relative configuration files to be read from here instead of the
-    # user's actual $HOME directory.
-    os.environ["HOME"] = home_dir
-
-    # Isolate ourselves from XDG directories
-    os.environ["XDG_DATA_HOME"] = os.path.join(home_dir, ".local", "share")
-    os.environ["XDG_CONFIG_HOME"] = os.path.join(home_dir, ".config")
-    os.environ["XDG_CACHE_HOME"] = os.path.join(home_dir, ".cache")
-    os.environ["XDG_RUNTIME_DIR"] = os.path.join(home_dir, ".runtime")
-    os.environ["XDG_DATA_DIRS"] = ":".join([
-        os.path.join(fake_root, "usr", "local", "share"),
-        os.path.join(fake_root, "usr", "share"),
-    ])
-    os.environ["XDG_CONFIG_DIRS"] = os.path.join(fake_root, "etc", "xdg")
+    if sys.platform == 'win32':
+        # Note: this will only take effect in subprocesses...
+        home_drive, home_path = os.path.splitdrive(home_dir)
+        os.environ.update({
+            'USERPROFILE': home_dir,
+            'HOMEDRIVE': home_drive,
+            'HOMEPATH': home_path,
+        })
+        for env_var, sub_path in (
+            ('APPDATA', 'AppData/Roaming'),
+            ('LOCALAPPDATA', 'AppData/Local'),
+        ):
+            path = os.path.join(home_dir, *sub_path.split('/'))
+            os.environ[env_var] = path
+            os.makedirs(path)
+    else:
+        # Set our home directory to our temporary directory, this should force
+        # all of our relative configuration files to be read from here instead
+        # of the user's actual $HOME directory.
+        os.environ["HOME"] = home_dir
+        # Isolate ourselves from XDG directories
+        os.environ["XDG_DATA_HOME"] = os.path.join(home_dir, ".local", "share")
+        os.environ["XDG_CONFIG_HOME"] = os.path.join(home_dir, ".config")
+        os.environ["XDG_CACHE_HOME"] = os.path.join(home_dir, ".cache")
+        os.environ["XDG_RUNTIME_DIR"] = os.path.join(home_dir, ".runtime")
+        os.environ["XDG_DATA_DIRS"] = ":".join([
+            os.path.join(fake_root, "usr", "local", "share"),
+            os.path.join(fake_root, "usr", "share"),
+        ])
+        os.environ["XDG_CONFIG_DIRS"] = os.path.join(fake_root, "etc", "xdg")
 
     # Configure git, because without an author name/email git will complain
     # and cause test failures.
@@ -116,6 +129,7 @@ def isolate(tmpdir):
     # We want to disable the version check from running in the tests
     os.environ["PIP_DISABLE_PIP_VERSION_CHECK"] = "true"
 
+    # FIXME: Windows...
     os.makedirs(os.path.join(home_dir, ".config", "git"))
     with open(os.path.join(home_dir, ".config", "git", "config"), "wb") as fp:
         fp.write(
