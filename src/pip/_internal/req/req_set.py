b96618bedc07970d5cf9816ca062d28279a6389d
Pradyun S. Gedam <pradyunsg@gmail.com>
2017-10-04 18:20:13 +0530

    Revert unrelated change

--- src/pip/_internal/req/req_set.py
+++ src/pip/_internal/req/req_set.py
@@ -16,6 +16,9 @@ class RequirementSet(object):
                  require_hashes=False, target_dir=None, use_user_site=False,
                  pycompile=True):
         """Create a RequirementSet.
+
+        :param wheel_cache: The pip wheel cache, for passing to
+            InstallRequirement.
         """
 
         self.requirements = OrderedDict()
