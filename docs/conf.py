a12a0c5387958cd3f20d702f0b4d62a922ba79bb
Pradyun S. Gedam <pradyunsg@gmail.com>
2017-10-04 12:07:35 +0530

    Use standard names

--- docs/conf.py
+++ docs/conf.py
@@ -83,7 +83,7 @@ today_fmt = '%B %d, %Y'
 
 # List of directories, relative to source directory, that shouldn't be searched
 # for source files.
-exclude_patterns = ['_build/', 'man/']
+exclude_patterns = ['build/', 'man/']
 
 # The reST default role (used for this markup: `text`) to use for all documents
 # default_role = None
