90105d62743a357789db3f0b8bff10d51f898c3a
Pradyun S. Gedam <pradyunsg@gmail.com>
2017-10-04 12:08:12 +0530

    Disable smart quotes
    Preserves --

--- docs/conf.py
+++ docs/conf.py
@@ -155,7 +155,7 @@ html_last_updated_fmt = '%b %d, %Y'
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
-# html_use_smartypants = False
+smart_quotes = False
 
 # Custom sidebar templates, maps document names to template names.
 html_sidebars = {
