f04937e5d1b4426ee1f014c205c6edb1f846c885
xoviat <xoviat@users.noreply.github.com>
2017-10-14 16:04:46 -0500

    FIX: backend: print system PATH

--- src/pip/_internal/backend.py
+++ src/pip/_internal/backend.py
@@ -2,6 +2,7 @@ import logging
 import os
 import sys
 import importlib
+import textwrap
 
 from sysconfig import get_paths
 
@@ -104,5 +105,17 @@ class BuildBackendCaller(BuildBackendBase):
         """Handles aribrary function invocations on the build backend."""
         os.chdir(self.cwd)
         os.environ.update(self.env)
+        logger.debug(textwrap.dedent("""
+            Subprocess runner data:
+                Current Directory: {}
+                System Backend: {}
+                System Path: {}
+                System Environment: {}
+            """).format(
+            self.cwd,
+            self.backend_name,
+            sys.path,
+            os.environ))
+
         mod = importlib.import_module(self.backend_name)
         return getattr(mod, name)(*args, **kw)
