d05159cd5e44627b35ddc0b42b967db0072dfde5
Jon Dufresne <jon.dufresne@gmail.com>
2017-10-17 22:03:07 -0700

    Fix ResourceWarning generated by setup.py when warnings are enabled
    Appears as:
    
      ResourceWarning: unclosed file ...
    
    Always explicitly close open files. Use context manager to make this
    easier.

--- setup.py
+++ setup.py
@@ -12,7 +12,8 @@ here = os.path.abspath(os.path.dirname(__file__))
 def read(*parts):
     # intentionally *not* adding an encoding option to open, See:
     #   https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
-    return codecs.open(os.path.join(here, *parts), 'r').read()
+    with codecs.open(os.path.join(here, *parts), 'r') as fp:
+        return fp.read()
 
 
 def find_version(*file_paths):
