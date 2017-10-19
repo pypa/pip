8f5e27a4a9cf8e2a3c65ccc034be37cf3d5471b7
xoviat <xoviat@users.noreply.github.com>
2017-10-18 22:31:51 -0500

    FIX: req_install: show directories to tests

--- src/pip/_internal/req/req_install.py
+++ src/pip/_internal/req/req_install.py
@@ -496,6 +496,8 @@ class InstallRequirement(object):
                 self.build_backend.prepare_metadata_for_build_wheel(
                     metadata_directory)
             except:
+                for d in os.listdir(metadata_directory):
+                    print(d)
                 self.build_environment.cleanup()
 
         if not self.req:
