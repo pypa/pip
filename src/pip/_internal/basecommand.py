5a8b11cbe4b5e143e3ef6ebacfabe831b620f93b
Pradyun Gedam <pradyunsg@gmail.com>
2017-11-20 17:18:13 +0530

    Improve --no-color and test for it (#4866)
    * Improve the --no-color test
    * Let the logger decide to not color things
    * misc: Move handler name outside dict

--- src/pip/_internal/basecommand.py
+++ src/pip/_internal/basecommand.py
@@ -132,10 +132,8 @@ class Command(object):
         if options.log:
             root_level = "DEBUG"
 
-        if options.no_color:
-            logger_class = "logging.StreamHandler"
-        else:
-            logger_class = "pip._internal.utils.logging.ColorizedStreamHandler"
+        logger_class = "pip._internal.utils.logging.ColorizedStreamHandler"
+        handler_class = "pip._internal.utils.logging.BetterRotatingFileHandler"
 
         logging.config.dictConfig({
             "version": 1,
@@ -156,6 +154,7 @@ class Command(object):
                 "console": {
                     "level": level,
                     "class": logger_class,
+                    "no_color": options.no_color,
                     "stream": self.log_streams[0],
                     "filters": ["exclude_warnings"],
                     "formatter": "indent",
@@ -163,14 +162,13 @@ class Command(object):
                 "console_errors": {
                     "level": "WARNING",
                     "class": logger_class,
+                    "no_color": options.no_color,
                     "stream": self.log_streams[1],
                     "formatter": "indent",
                 },
                 "user_log": {
                     "level": "DEBUG",
-                    "class":
-                        ("pip._internal.utils.logging"
-                         ".BetterRotatingFileHandler"),
+                    "class": handler_class,
                     "filename": options.log or "/dev/null",
                     "delay": True,
                     "formatter": "indent",
