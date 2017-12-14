8922c34d43ff6666a8bd7a73ee7e74fc7197ad13
Giftlin Rajaiah <giftlin.rgn@gmail.com>
2017-12-15 00:46:04 +0530

    Update subversion.py

--- src/pip/_internal/vcs/subversion.py
+++ src/pip/_internal/vcs/subversion.py
@@ -130,7 +130,7 @@ class Subversion(VersionControl):
             dirurl, localrev = self._get_svn_url_rev(base)
 
             if base == location:
-                base_url = dirurl + '/'   # save the root url
+                base = dirurl + '/'   # save the root url
             elif not dirurl or not dirurl.startswith(base):
                 dirs[:] = []
                 continue    # not part of the same svn tree, skip it
