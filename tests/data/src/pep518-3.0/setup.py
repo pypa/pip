b2144259f0b3bea6d138ffa47ade39f9a27901ad
xoviat <xoviat@users.noreply.github.com>
2018-01-27 11:27:19 -0600

    Add regression test

--- tests/data/src/pep518-3.0/setup.py
+++ tests/data/src/pep518-3.0/setup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 from setuptools import find_packages, setup
 
+import simple  # Test gh-4647 regression
+
 setup(name='pep518',
       version='3.0',
       packages=find_packages()
