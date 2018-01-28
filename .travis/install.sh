2f50d11060289d7c70eefa8a3486427c299360d9
barneygale <barney.gale@gmail.com>
2018-01-28 15:05:23 +0000

    Modify CI config to install Helix Core client and server.

--- .travis/install.sh
+++ .travis/install.sh
@@ -7,3 +7,10 @@ git config --global user.name "pip"
 
 pip install --upgrade setuptools
 pip install --upgrade tox
+
+wget -N -P "$HOME/.p4/bin" \
+    "http://ftp.perforce.com/perforce/r17.2/bin.linux26x86_64/p4" \
+    "http://ftp.perforce.com/perforce/r17.2/bin.linux26x86_64/p4d"
+chmod +x \
+    "$HOME/.p4/bin/p4" \
+    "$HOME/.p4/bin/p4d"
