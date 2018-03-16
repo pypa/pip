063f5477d3960128395465a4ef888110c3fa11b1
Donald Stufft <donald@stufft.io>
2018-03-16 18:47:23 -0400

    Bump version

--- pip/__init__.py
+++ pip/__init__.py
@@ -58,7 +58,7 @@ import pip.cmdoptions
 cmdoptions = pip.cmdoptions
 
 # The version as used in the setup.py and the docs conf.py
-__version__ = "9.0.1"
+__version__ = "9.0.2.dev0"
 
 
 logger = logging.getLogger(__name__)
