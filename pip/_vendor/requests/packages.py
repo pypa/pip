aee9eda0bccea8ffbc6e98dfddf5f88edf85792d
Donald Stufft <donald@stufft.io>
2018-03-21 20:27:08 -0400

    Fix #5081

--- pip/_vendor/requests/packages.py
+++ pip/_vendor/requests/packages.py
@@ -4,11 +4,13 @@ import sys
 # I don't like it either. Just look the other way. :)
 
 for package in ('urllib3', 'idna', 'chardet'):
-    locals()[package] = __import__("pip._vendor." + package)
+    vendored_package = "pip._vendor." + package
+    locals()[package] = __import__(vendored_package)
     # This traversal is apparently necessary such that the identities are
     # preserved (requests.packages.urllib3.* is urllib3.*)
     for mod in list(sys.modules):
-        if mod == package or mod.startswith(package + '.'):
-            sys.modules['pip._vendor.requests.packages.' + mod] = sys.modules["pip._vendor." + mod]
+        if mod == vendored_package or mod.startswith(vendored_package + '.'):
+            unprefixed_mod = mod[len("pip._vendor."):]
+            sys.modules['pip._vendor.requests.packages.' + unprefixed_mod] = sys.modules[mod]
 
 # Kinda cool, though, right?
