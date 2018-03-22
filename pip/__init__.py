ece1c6b189755cef94242fbd1a2fd185e557ec68
Donald Stufft <donald@stufft.io>
2018-03-21 20:35:07 -0400

    Bump for release

--- pip/__init__.py
+++ pip/__init__.py
@@ -58,7 +58,7 @@ import pip.cmdoptions
 cmdoptions = pip.cmdoptions
 
 # The version as used in the setup.py and the docs conf.py
-__version__ = "9.0.2"
+__version__ = "9.0.3"
 
 
 logger = logging.getLogger(__name__)
