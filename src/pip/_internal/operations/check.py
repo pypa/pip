ee2e81ca1f29fc2e141d24570abeeb128c22d4ee
Pradyun Gedam <pradyunsg@gmail.com>
2018-04-04 13:03:07 +0530

    Use all available distributions
    Same as b5f56efde570c77cf443fa431da8ce49516ab0aa

--- src/pip/_internal/operations/check.py
+++ src/pip/_internal/operations/check.py
@@ -30,6 +30,9 @@ def create_package_set_from_installed(**kwargs):
     # type: (**Any) -> PackageSet
     """Converts a list of distributions into a PackageSet.
     """
+    # Default to using all packages installed on the system
+    if kwargs == {}:
+        kwargs = {"local_only": False, "skip": ()}
     retval = {}
     for dist in get_installed_distributions(**kwargs):
         name = canonicalize_name(dist.project_name)
