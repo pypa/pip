9d26025c4349e6c09b7b3f78554d38bb5abb88dd
Daniel Black <dyspop@gmail.com>
2018-06-14 21:12:25 -0400

    make error output a little more specific.

--- src/pip/_internal/req/req_install.py
+++ src/pip/_internal/req/req_install.py
@@ -560,10 +560,10 @@ class InstallRequirement(object):
 
             if not filenames:
                 raise InstallationError(
-                    'No files/directories in %s (from %s)' % (base, filename)
+                    'Could not find files/directories in %s (from %s)' % (base, filename)
                 )
             assert filenames, \
-                "No files/directories in %s (from %s)" % (base, filename)
+                "Could not find files/directories in %s (from %s)" % (base, filename)
 
             # if we have more than one match, we pick the toplevel one.  This
             # can easily be the case if there is a dist folder which contains
