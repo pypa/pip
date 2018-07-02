ca5cca342637247f58c005711798cfd7e6adf6a5
Oliver Jeeves <oliver.jeeves@ocado.com>
2018-07-02 13:14:28 +0100

    Fix error trying to generate scripts warning when PATH not set
    fixes #5558

--- src/pip/_internal/wheel.py
+++ src/pip/_internal/wheel.py
@@ -163,7 +163,7 @@ def message_about_scripts_not_on_PATH(scripts):
     # We don't want to warn for directories that are on PATH.
     not_warn_dirs = [
         os.path.normcase(i).rstrip(os.sep) for i in
-        os.environ["PATH"].split(os.pathsep)
+        os.environ.get("PATH", "").split(os.pathsep)
     ]
     # If an executable sits with sys.executable, we don't warn for it.
     #     This covers the case of venv invocations without activating the venv.
