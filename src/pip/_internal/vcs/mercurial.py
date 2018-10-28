3f98ee42509679a5bfd53899c9b6127d6f52e44d
Adam Tse <adam.tse@me.com>
2018-10-28 11:44:03 +0000

    Explicit support and tests for `hg+file` scheme for `pip install`.

--- src/pip/_internal/vcs/mercurial.py
+++ src/pip/_internal/vcs/mercurial.py
@@ -22,7 +22,9 @@ class Mercurial(VersionControl):
     name = 'hg'
     dirname = '.hg'
     repo_name = 'clone'
-    schemes = ('hg', 'hg+http', 'hg+https', 'hg+ssh', 'hg+static-http')
+    schemes = (
+        'hg', 'hg+http', 'hg+https', 'hg+ssh', 'hg+static-http', 'hg+file'
+    )
 
     @staticmethod
     def get_base_rev_args(rev):
--- tests/functional/test_install.py
+++ tests/functional/test_install.py
@@ -334,11 +334,13 @@ def test_install_editable_uninstalls_existing_from_path(script, data):
 
 @need_mercurial
 def test_basic_install_editable_from_hg(script, tmpdir):
-    """Test cloning from Mercurial."""
+    """Test cloning and hg+file install from Mercurial."""
     pkg_path = _create_test_package(script, name='testpackage', vcs='hg')
     args = ['install', '-e', 'hg+%s#egg=testpackage' % path_to_url(pkg_path)]
     result = script.pip(*args)
     result.assert_installed('testpackage', with_files=['.hg'])
+    assert path_to_url(pkg_path).startswith("file://")
+
 
 
 @need_mercurial
