f60c57df4fda335e114a56dd85d4d17b2055567e
Pradyun Gedam <pradyunsg@gmail.com>
2018-11-08 16:35:32 +0530

    Fix an incorrect type annotation

--- src/pip/_internal/vcs/__init__.py
+++ src/pip/_internal/vcs/__init__.py
@@ -17,7 +17,6 @@ from pip._internal.utils.typing import MYPY_CHECK_RUNNING
 
 if MYPY_CHECK_RUNNING:
     from typing import Dict, Optional, Tuple  # noqa: F401
-    from pip._internal.cli.base_command import Command  # noqa: F401
 
 __all__ = ['vcs']
 
@@ -87,7 +86,7 @@ class RevOptions(object):
 
 
 class VcsSupport(object):
-    _registry = {}  # type: Dict[str, Command]
+    _registry = {}  # type: Dict[str, VersionControl]
     schemes = ['ssh', 'git', 'hg', 'bzr', 'sftp', 'svn']
 
     def __init__(self):
