b2e9d32d1dafdd32f1be25e65b743ebd41d767ff
Xavier Fernandez <xavier.fernandez@polyconseil.fr>
2019-02-06 23:10:08 +0100

    add deprecation test

--- tests/functional/test_message_ids.py
+++ tests/functional/test_message_ids.py
@@ -1,3 +1,5 @@
+import pytest
+
 def test_show_message_ids(script):
     result = script.pip('show', 'pip', '--show-message-ids')
     assert 'message_id=default' in result.stdout
@@ -6,3 +8,12 @@ def test_show_message_ids(script):
 def test_ignore_messages(script):
     result = script.pip('show', 'pip', '--ignore-messages', 'default')
     assert 'Name: pip' not in result.stdout
+
+
+def test_hide_deprecation(script, deprecated_python):
+    if not deprecated_python:
+        pytest.skip("Python version isn't deprecated")
+    result = script.pip('show', 'pip')
+    assert 'DEPRECATION' in result.stderr
+    result = script.pip('show', 'pip', '--ignore-messages', 'deprecation')
+    assert 'DEPRECATION' not in result.stderr
