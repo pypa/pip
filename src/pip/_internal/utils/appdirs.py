79a70289d248057d8cee855dfa0a96a1d4820b7c
Steve Dower <steve.dower@microsoft.com>
2019-02-15 11:24:39 -0800

    Fixes #5060: Configuration files may now also be stored under `sys.prefix`

--- src/pip/_internal/utils/appdirs.py
+++ src/pip/_internal/utils/appdirs.py
@@ -190,6 +190,10 @@ def site_config_dirs(appname):
         # always look in /etc directly as well
         pathlist.append('/etc')
 
+    # allow configuration in sys.prefix without any appname
+    # see <https://github.com/pypa/pip/issues/5060>
+    pathlist.append(sys.prefix)
+
     return pathlist
 
 
