3816a747e1c3eaf20458be3f59d170df4bd1b51a
Steve Dower <steve.dower@microsoft.com>
2019-05-08 10:58:25 -0400

    Upwrap import

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -29,8 +29,7 @@ from pip._internal.utils.misc import (
     call_subprocess, egg_link_path, ensure_dir, format_command_args,
     get_installed_distributions, get_prog, normalize_path, redact_netloc,
     redact_password_from_url, remove_auth_from_url, rmtree,
-    split_auth_from_netloc, split_auth_netloc_from_url, untar_file,
-    unzip_file,
+    split_auth_from_netloc, split_auth_netloc_from_url, untar_file, unzip_file,
 )
 from pip._internal.utils.packaging import check_dist_requires_python
 from pip._internal.utils.temp_dir import AdjacentTempDirectory, TempDirectory
