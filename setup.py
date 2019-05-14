c4c7b8eb945382dc18f8303043c96e0e068d88a0
Bastian Venthur <bastian.venthur@flixbus.com>
2019-05-14 10:49:28 +0200

    Moved everything but entry_points and package_data into setup.cfg

--- setup.py
+++ setup.py
@@ -1,69 +1,9 @@
-import codecs
-import os
-import re
 import sys
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
-here = os.path.abspath(os.path.dirname(__file__))
-
-
-def read(*parts):
-    # intentionally *not* adding an encoding option to open, See:
-    #   https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
-    with codecs.open(os.path.join(here, *parts), 'r') as fp:
-        return fp.read()
-
-
-def find_version(*file_paths):
-    version_file = read(*file_paths)
-    version_match = re.search(
-        r"^__version__ = ['\"]([^'\"]*)['\"]",
-        version_file,
-        re.M,
-    )
-    if version_match:
-        return version_match.group(1)
-
-    raise RuntimeError("Unable to find version string.")
-
-
-long_description = read('README.rst')
 
 setup(
-    name="pip",
-    version=find_version("src", "pip", "__init__.py"),
-    description="The PyPA recommended tool for installing Python packages.",
-    long_description=long_description,
-
-    license='MIT',
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Topic :: Software Development :: Build Tools",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
-    ],
-    url='https://pip.pypa.io/',
-    keywords='distutils easy_install egg setuptools wheel virtualenv',
-
-    author='The pip developers',
-    author_email='pypa-dev@groups.google.com',
-
-    package_dir={"": "src"},
-    packages=find_packages(
-        where="src",
-        exclude=["contrib", "docs", "tests*", "tasks"],
-    ),
     package_data={
         "pip._vendor.certifi": ["*.pem"],
         "pip._vendor.requests": ["*.pem"],
@@ -77,7 +17,4 @@ setup(
             "pip%s.%s=pip._internal:main" % sys.version_info[:2],
         ],
     },
-
-    zip_safe=False,
-    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
 )
