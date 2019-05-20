3cab748c6ed088561f4a3161fb5c5fdda37153a7
johnthagen <johnthagen@gmail.com>
2019-05-20 12:45:19 -0400

    Set use_interactive=False to avoid having to mock

--- tests/functional/test_install_vcs_svn.py
+++ tests/functional/test_install_vcs_svn.py
@@ -4,33 +4,27 @@ from mock import patch
 from pip._internal.vcs.subversion import Subversion
 
 
-@patch('pip._internal.vcs.subversion.Subversion.get_remote_call_options')
 @patch('pip._internal.vcs.call_subprocess')
 @pytest.mark.network
-def test_obtain_should_recognize_auth_info_url(
-        call_subprocess_mock, get_remote_call_options_mock, script):
-    get_remote_call_options_mock.return_value = []
+def test_obtain_should_recognize_auth_info_url(call_subprocess_mock, script):
     url = 'svn+http://username:password@svn.example.com/'
-    svn = Subversion()
+    svn = Subversion(use_interactive=False)
     svn.obtain(script.scratch_path / 'test', url=url)
     assert call_subprocess_mock.call_args[0][0] == [
-        svn.name, 'checkout', '-q', '--username', 'username', '--password',
-        'password', 'http://svn.example.com/',
+        svn.name, 'checkout', '-q', '--non-interactive', '--username',
+        'username', '--password', 'password', 'http://svn.example.com/',
         script.scratch_path / 'test',
     ]
 
 
-@patch('pip._internal.vcs.subversion.Subversion.get_remote_call_options')
 @patch('pip._internal.vcs.call_subprocess')
 @pytest.mark.network
-def test_export_should_recognize_auth_info_url(
-        call_subprocess_mock, get_remote_call_options_mock, script):
-    get_remote_call_options_mock.return_value = []
+def test_export_should_recognize_auth_info_url(call_subprocess_mock, script):
     url = 'svn+http://username:password@svn.example.com/'
-    svn = Subversion()
+    svn = Subversion(use_interactive=False)
     svn.export(script.scratch_path / 'test', url=url)
     assert call_subprocess_mock.call_args[0][0] == [
-        svn.name, 'export', '--username', 'username', '--password',
-        'password', 'http://svn.example.com/',
+        svn.name, 'export', '--non-interactive', '--username', 'username',
+        '--password', 'password', 'http://svn.example.com/',
         script.scratch_path / 'test',
     ]
