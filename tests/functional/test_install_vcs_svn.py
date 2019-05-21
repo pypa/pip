136145611358578155cfdcaf6d379c466e968f27
johnthagen <johnthagen@gmail.com>
2019-05-21 06:57:18 -0400

    Explain why use_interactive is set to False in unit tests

--- tests/functional/test_install_vcs_svn.py
+++ tests/functional/test_install_vcs_svn.py
@@ -8,6 +8,8 @@ from pip._internal.vcs.subversion import Subversion
 @pytest.mark.network
 def test_obtain_should_recognize_auth_info_url(call_subprocess_mock, script):
     url = 'svn+http://username:password@svn.example.com/'
+    # use_interactive is set to False to test that remote call options are
+    # properly added.
     svn = Subversion(use_interactive=False)
     svn.obtain(script.scratch_path / 'test', url=url)
     assert call_subprocess_mock.call_args[0][0] == [
@@ -21,6 +23,8 @@ def test_obtain_should_recognize_auth_info_url(call_subprocess_mock, script):
 @pytest.mark.network
 def test_export_should_recognize_auth_info_url(call_subprocess_mock, script):
     url = 'svn+http://username:password@svn.example.com/'
+    # use_interactive is set to False to test that remote call options are
+    # properly added.
     svn = Subversion(use_interactive=False)
     svn.export(script.scratch_path / 'test', url=url)
     assert call_subprocess_mock.call_args[0][0] == [
