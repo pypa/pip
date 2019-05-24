a4aff3b7ffa41b81c7cad564084d84fd9699c932
johnthagen <johnthagen@gmail.com>
2019-05-24 07:32:43 -0400

    Use a TestCase to remove some duplication across similar tests

--- tests/functional/test_install_vcs_svn.py
+++ tests/functional/test_install_vcs_svn.py
@@ -1,3 +1,5 @@
+from unittest import TestCase
+
 from mock import patch
 
 from pip._internal.vcs import RevOptions
@@ -32,46 +34,36 @@ def test_export_should_recognize_auth_info_url(call_subprocess_mock, script):
     ]
 
 
-@patch('pip._internal.vcs.call_subprocess')
-def test_fetch_new_should_include_remote_call_options(
-        call_subprocess_mock, script):
-    url = 'svn+http://username:password@svn.example.com/'
-    # use_interactive is set to False to test that remote call options are
-    # properly added.
-    svn = Subversion(use_interactive=False)
-    rev_options = RevOptions(Subversion)
-    dest = script.scratch_path / 'test'
-    svn.fetch_new(dest, url, rev_options)
-    assert call_subprocess_mock.call_args[0][0] == [
-        svn.name, 'checkout', '-q', '--non-interactive', url, dest,
-    ]
+class TestSubversionArgs(TestCase):
+    def setUp(self):
+        patcher = patch('pip._internal.vcs.call_subprocess')
+        self.addCleanup(patcher.stop)
+        self.call_subprocess_mock = patcher.start()
 
+        # Test Data.
+        self.url = 'svn+http://username:password@svn.example.com/'
+        # use_interactive is set to False to test that remote call options are
+        # properly added.
+        self.svn = Subversion(use_interactive=False)
+        self.rev_options = RevOptions(Subversion)
+        self.dest = '/tmp/test'
 
-@patch('pip._internal.vcs.call_subprocess')
-def test_switch_should_include_remote_call_options(
-        call_subprocess_mock, script):
-    url = 'svn+http://username:password@svn.example.com/'
-    # use_interactive is set to False to test that remote call options are
-    # properly added.
-    svn = Subversion(use_interactive=False)
-    rev_options = RevOptions(Subversion)
-    dest = script.scratch_path / 'test'
-    svn.switch(dest, url, rev_options)
-    assert call_subprocess_mock.call_args[0][0] == [
-        svn.name, 'switch', '--non-interactive', url, dest,
-    ]
+    def assert_call_args(self, args):
+        assert self.call_subprocess_mock.call_args[0][0] == args
 
+    def test_fetch_new_should_include_remote_call_options(self):
+        self.svn.fetch_new(self.dest, self.url, self.rev_options)
+        self.assert_call_args(
+            [self.svn.name, 'checkout', '-q', '--non-interactive', self.url,
+             self.dest])
 
-@patch('pip._internal.vcs.call_subprocess')
-def test_update_should_include_remote_call_options(
-        call_subprocess_mock, script):
-    url = 'svn+http://username:password@svn.example.com/'
-    # use_interactive is set to False to test that remote call options are
-    # properly added.
-    svn = Subversion(use_interactive=False)
-    rev_options = RevOptions(Subversion)
-    dest = script.scratch_path / 'test'
-    svn.update(dest, url, rev_options)
-    assert call_subprocess_mock.call_args[0][0] == [
-        svn.name, 'update', '--non-interactive', dest,
-    ]
+    def test_switch_should_include_remote_call_options(self):
+        self.svn.switch(self.dest, self.url, self.rev_options)
+        self.assert_call_args(
+            [self.svn.name, 'switch', '--non-interactive', self.url,
+             self.dest])
+
+    def test_update_should_include_remote_call_options(self):
+        self.svn.update(self.dest, self.url, self.rev_options)
+        self.assert_call_args(
+            [self.svn.name, 'update', '--non-interactive', self.dest])
