cd5bd2cd520c4f834a229d7b3125d4e179fcf806
Chris Jerdonek <chris.jerdonek@gmail.com>
2019-05-26 12:29:34 -0700

    Improve error message if METADATA or PKG-INFO metadata is None.

---
+++ news/5082.feature
@@ -0,0 +1,2 @@
+Improve the error message when ``METADATA`` or ``PKG-INFO`` is None when
+accessing metadata.
--- src/pip/_internal/utils/packaging.py
+++ src/pip/_internal/utils/packaging.py
@@ -6,6 +6,7 @@ from email.parser import FeedParser
 from pip._vendor import pkg_resources
 from pip._vendor.packaging import specifiers, version
 
+from pip._internal.exceptions import NoneMetadataError
 from pip._internal.utils.misc import display_path
 from pip._internal.utils.typing import MYPY_CHECK_RUNNING
 
@@ -43,16 +44,27 @@ def check_requires_python(requires_python, version_info):
 
 def get_metadata(dist):
     # type: (Distribution) -> Message
+    """
+    :raises NoneMetadataError: if the distribution reports `has_metadata()`
+        True but `get_metadata()` returns None.
+    """
+    metadata_name = 'METADATA'
     if (isinstance(dist, pkg_resources.DistInfoDistribution) and
-            dist.has_metadata('METADATA')):
-        metadata = dist.get_metadata('METADATA')
+            dist.has_metadata(metadata_name)):
+        metadata = dist.get_metadata(metadata_name)
     elif dist.has_metadata('PKG-INFO'):
-        metadata = dist.get_metadata('PKG-INFO')
+        metadata_name = 'PKG-INFO'
+        metadata = dist.get_metadata(metadata_name)
     else:
         logger.warning("No metadata found in %s", display_path(dist.location))
         metadata = ''
 
+    if metadata is None:
+        raise NoneMetadataError(dist, metadata_name)
+
     feed_parser = FeedParser()
+    # The following line errors out if with a "NoneType" TypeError if
+    # passed metadata=None.
     feed_parser.feed(metadata)
     return feed_parser.close()
 
