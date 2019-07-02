129c383330b651898ed9ff85a2f025f4412811fa
Brandt Bucher <brandtbucher@gmail.com>
2019-07-02 10:47:01 -0700

    Get glibc version from os instead of ctypes.

--- src/pip/_internal/utils/glibc.py
+++ src/pip/_internal/utils/glibc.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 
-import ctypes
+import os
 import re
 import warnings
 
@@ -14,26 +14,12 @@ def glibc_version_string():
     # type: () -> Optional[str]
     "Returns glibc version string, or None if not using glibc."
 
-    # ctypes.CDLL(None) internally calls dlopen(NULL), and as the dlopen
-    # manpage says, "If filename is NULL, then the returned handle is for the
-    # main program". This way we can let the linker do the work to figure out
-    # which libc our process is actually using.
-    process_namespace = ctypes.CDLL(None)
-    try:
-        gnu_get_libc_version = process_namespace.gnu_get_libc_version
-    except AttributeError:
-        # Symbol doesn't exist -> therefore, we are not linked to
-        # glibc.
-        return None
+    try:  # https://github.com/pypa/pip/issues/6675#issue-463147612
+        glibc_version = os.confstr('CS_GNU_LIBC_VERSION').split()
+    except (AttributeError, OSError, ValueError):
+        return None  # os.confstr() or CS_GNU_LIBC_VERSION not available...
 
-    # Call gnu_get_libc_version, which returns a string like "2.5"
-    gnu_get_libc_version.restype = ctypes.c_char_p
-    version_str = gnu_get_libc_version()
-    # py2 / py3 compatibility:
-    if not isinstance(version_str, str):
-        version_str = version_str.decode("ascii")
-
-    return version_str
+    return glibc_version[-1] if len(glibc_version) == 2 else None
 
 
 # Separated out from have_compatible_glibc for easier unit testing
