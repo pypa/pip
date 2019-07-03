d74da185a203f5ba4d129529b7151382b182c0a1
Brandt Bucher <brandtbucher@gmail.com>
2019-07-02 21:47:51 -0700

    Fall back on ctypes.

--- src/pip/_internal/utils/glibc.py
+++ src/pip/_internal/utils/glibc.py
@@ -15,11 +15,35 @@ def glibc_version_string():
     "Returns glibc version string, or None if not using glibc."
 
     try:  # https://github.com/pypa/pip/issues/6675#issue-463147612
-        glibc_version = os.confstr('CS_GNU_LIBC_VERSION').split()
+        glibc_version = os.confstr("CS_GNU_LIBC_VERSION").split()
     except (AttributeError, OSError, ValueError):
-        return None  # os.confstr() or CS_GNU_LIBC_VERSION not available...
+        pass  # os.confstr() or CS_GNU_LIBC_VERSION not available...
+    else:
+        return glibc_version[-1] if len(glibc_version) == 2 else None
 
-    return glibc_version[-1] if len(glibc_version) == 2 else None
+    try:
+        import ctypes
+    except ImportError:
+        return None
+
+    # ctypes.CDLL(None) internally calls dlopen(NULL), and as the dlopen
+    # manpage says, "If filename is NULL, then the returned handle is for the
+    # main program". This way we can let the linker do the work to figure out
+    # which libc our process is actually using.
+    process_namespace = ctypes.CDLL(None)
+    try:
+        gnu_get_libc_version = process_namespace.gnu_get_libc_version
+    except AttributeError:
+        # Symbol doesn't exist -> therefore, we are not linked to
+        # glibc.
+        return None
+    # Call gnu_get_libc_version, which returns a string like "2.5"
+    gnu_get_libc_version.restype = ctypes.c_char_p
+    version_str = gnu_get_libc_version()
+    # py2 / py3 compatibility:
+    if not isinstance(version_str, str):
+        version_str = version_str.decode("ascii")
+    return version_str
 
 
 # Separated out from have_compatible_glibc for easier unit testing
