190ea6a3d481f23f8031ac8c01b718da41aace5e
Chris Jerdonek <chris.jerdonek@gmail.com>
2019-07-14 23:03:40 -0700

    Remove strict_optional=False for models/link.py.

--- src/pip/_internal/models/link.py
+++ src/pip/_internal/models/link.py
@@ -206,5 +206,8 @@ class Link(KeyBasedCompareMixin):
         """
         if not self.has_hash:
             return False
+        # Assert non-None so mypy knows self.hash_name and self.hash are str.
+        assert self.hash_name is not None
+        assert self.hash is not None
 
         return hashes.is_hash_allowed(self.hash_name, hex_digest=self.hash)
