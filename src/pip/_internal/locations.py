4cf08e85c09ad2136ff7212719bc8d126490a3cd
Xavier Fernandez <xavier.fernandez@polyconseil.fr>
2019-07-18 22:50:05 +0200

    Isolate src_prefix computation in a function

--- src/pip/_internal/locations.py
+++ src/pip/_internal/locations.py
@@ -23,21 +23,23 @@ if MYPY_CHECK_RUNNING:
 USER_CACHE_DIR = appdirs.user_cache_dir("pip")
 
 
-if running_under_virtualenv():
-    src_prefix = os.path.join(sys.prefix, 'src')
-else:
-    # FIXME: keep src in cwd for now (it is not a temporary folder)
-    try:
-        src_prefix = os.path.join(os.getcwd(), 'src')
-    except OSError:
-        # In case the current working directory has been renamed or deleted
-        sys.exit(
-            "The folder you are executing pip from can no longer be found."
-        )
+def get_src_prefix():
+    if running_under_virtualenv():
+        src_prefix = os.path.join(sys.prefix, 'src')
+    else:
+        # FIXME: keep src in cwd for now (it is not a temporary folder)
+        try:
+            src_prefix = os.path.join(os.getcwd(), 'src')
+        except OSError:
+            # In case the current working directory has been renamed or deleted
+            sys.exit(
+                "The folder you are executing pip from can no longer be found."
+            )
 
-# under macOS + virtualenv sys.prefix is not properly resolved
-# it is something like /path/to/python/bin/..
-src_prefix = os.path.abspath(src_prefix)
+    # under macOS + virtualenv sys.prefix is not properly resolved
+    # it is something like /path/to/python/bin/..
+    return os.path.abspath(src_prefix)
+
 
 # FIXME doesn't account for venv linked to global site-packages
 
