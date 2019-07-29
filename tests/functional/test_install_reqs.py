145a753cd3edb9478ea0947b3be62f3470817672
Pradyun Gedam <pradyunsg@gmail.com>
2019-07-29 20:31:52 +0530

    Update tests for eagerly populated links

--- tests/functional/test_install_reqs.py
+++ tests/functional/test_install_reqs.py
@@ -162,7 +162,7 @@ def test_respect_order_in_requirements_file(script, data):
     )
 
     downloaded = [line for line in result.stdout.split('\n')
-                  if 'Collecting' in line]
+                  if 'Processing' in line]
 
     assert 'parent' in downloaded[0], (
         'First download should be "parent" but was "%s"' % downloaded[0]
