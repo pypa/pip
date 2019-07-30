f9ce7ca2aba1f206e1b4026740e96e3c8ec92bc0
Min ho Kim <minho42@gmail.com>
2019-07-30 14:40:40 +1000

    Revert "Add global options and no user config args to make_setuptools_shim_args (#6706)"
    This reverts commit f76014efac796c0656ad61bd2bbcc23b67d66495.

--- src/pip/_internal/wheel.py
+++ src/pip/_internal/wheel.py
@@ -934,11 +934,9 @@ class WheelBuilder(object):
         # isolating. Currently, it breaks Python in virtualenvs, because it
         # relies on site.py to find parts of the standard library outside the
         # virtualenv.
-        return make_setuptools_shim_args(
-            req.setup_py_path,
-            global_options=self.global_options,
-            unbuffered_output=True
-        )
+        base_cmd = make_setuptools_shim_args(req.setup_py_path,
+                                             unbuffered_output=True)
+        return base_cmd + list(self.global_options)
 
     def _build_one_pep517(self, req, tempd, python_tag=None):
         """Build one InstallRequirement using the PEP 517 build process.
