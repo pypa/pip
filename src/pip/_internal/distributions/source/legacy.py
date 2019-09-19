f051201d08218cec2948819b87c851d63e1240e5
Pradyun Gedam <pradyunsg@gmail.com>
2019-09-19 15:06:38 +0530

    Invoke method only when build isolation is needed

--- src/pip/_internal/distributions/source/legacy.py
+++ src/pip/_internal/distributions/source/legacy.py
@@ -29,12 +29,13 @@ class SourceDistribution(AbstractDistribution):
 
         self.req.load_pyproject_toml()
         should_isolate = self.req.use_pep517 and build_isolation
-        self._setup_isolation(should_isolate, finder)
+        if should_isolate:
+            self._setup_isolation(finder)
 
         self.req.prepare_metadata()
         self.req.assert_source_matches_version()
 
-    def _setup_isolation(self, should_isolate, finder):
+    def _setup_isolation(self, finder):
         def _raise_conflicts(conflicting_with, conflicting_reqs):
             raise InstallationError(
                 "Some build dependencies for %s conflict with %s: %s." % (
@@ -42,41 +43,40 @@ class SourceDistribution(AbstractDistribution):
                         '%s is incompatible with %s' % (installed, wanted)
                         for installed, wanted in sorted(conflicting))))
 
-        if should_isolate:
-            # Isolate in a BuildEnvironment and install the build-time
-            # requirements.
-            self.req.build_env = BuildEnvironment()
-            self.req.build_env.install_requirements(
-                finder, self.req.pyproject_requires, 'overlay',
-                "Installing build dependencies"
+        # Isolate in a BuildEnvironment and install the build-time
+        # requirements.
+        self.req.build_env = BuildEnvironment()
+        self.req.build_env.install_requirements(
+            finder, self.req.pyproject_requires, 'overlay',
+            "Installing build dependencies"
+        )
+        conflicting, missing = self.req.build_env.check_requirements(
+            self.req.requirements_to_check
+        )
+        if conflicting:
+            _raise_conflicts("PEP 517/518 supported requirements",
+                             conflicting)
+        if missing:
+            logger.warning(
+                "Missing build requirements in pyproject.toml for %s.",
+                self.req,
             )
-            conflicting, missing = self.req.build_env.check_requirements(
-                self.req.requirements_to_check
+            logger.warning(
+                "The project does not specify a build backend, and "
+                "pip cannot fall back to setuptools without %s.",
+                " and ".join(map(repr, sorted(missing)))
             )
-            if conflicting:
-                _raise_conflicts("PEP 517/518 supported requirements",
-                                 conflicting)
-            if missing:
-                logger.warning(
-                    "Missing build requirements in pyproject.toml for %s.",
-                    self.req,
-                )
-                logger.warning(
-                    "The project does not specify a build backend, and "
-                    "pip cannot fall back to setuptools without %s.",
-                    " and ".join(map(repr, sorted(missing)))
-                )
-            # Install any extra build dependencies that the backend requests.
-            # This must be done in a second pass, as the pyproject.toml
-            # dependencies must be installed before we can call the backend.
-            with self.req.build_env:
-                # We need to have the env active when calling the hook.
-                self.req.spin_message = "Getting requirements to build wheel"
-                reqs = self.req.pep517_backend.get_requires_for_build_wheel()
-            conflicting, missing = self.req.build_env.check_requirements(reqs)
-            if conflicting:
-                _raise_conflicts("the backend dependencies", conflicting)
-            self.req.build_env.install_requirements(
-                finder, missing, 'normal',
-                "Installing backend dependencies"
-            )
+        # Install any extra build dependencies that the backend requests.
+        # This must be done in a second pass, as the pyproject.toml
+        # dependencies must be installed before we can call the backend.
+        with self.req.build_env:
+            # We need to have the env active when calling the hook.
+            self.req.spin_message = "Getting requirements to build wheel"
+            reqs = self.req.pep517_backend.get_requires_for_build_wheel()
+        conflicting, missing = self.req.build_env.check_requirements(reqs)
+        if conflicting:
+            _raise_conflicts("the backend dependencies", conflicting)
+        self.req.build_env.install_requirements(
+            finder, missing, 'normal',
+            "Installing backend dependencies"
+        )
