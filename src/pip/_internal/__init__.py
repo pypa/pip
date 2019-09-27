9ba442362f05f7ec02d256b55580ab023a538135
NtaleGrey <Shadikntale@gmail.com>
2019-09-27 14:27:08 +0300

    remove DependencyWarning warning from pip._internal

--- src/pip/_internal/__init__.py
+++ src/pip/_internal/__init__.py
@@ -6,7 +6,6 @@ import warnings
 # We ignore certain warnings from urllib3, since they are not relevant to pip's
 # usecases.
 from pip._vendor.urllib3.exceptions import (
-    DependencyWarning,
     InsecureRequestWarning,
 )
 
@@ -14,7 +13,3 @@ import pip._internal.utils.inject_securetransport  # noqa
 
 # Raised when using --trusted-host.
 warnings.filterwarnings("ignore", category=InsecureRequestWarning)
-# Raised since socks support depends on PySocks, which may not be installed.
-#   Barry Warsaw noted (on 2016-06-17) that this should be done before
-#   importing pip.vcs, which has since moved to pip._internal.vcs.
-warnings.filterwarnings("ignore", category=DependencyWarning)
