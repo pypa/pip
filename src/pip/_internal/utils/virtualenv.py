1aee0eb5cbaa48b5a193b942e38a15c4cf376c05
Pradyun Gedam <pradyunsg@gmail.com>
2019-10-07 15:36:29 +0530

    Correctly ignore system site-packages for venv
    Why: PEP 405 virtual environments have a different mechanism for
    ignoring system site-packages in virtual environments.

--- src/pip/_internal/utils/virtualenv.py
+++ src/pip/_internal/utils/virtualenv.py
@@ -3,6 +3,11 @@ import os
 import site
 import sys
 
+from pip._internal.utils.typing import MYPY_CHECK_RUNNING
+
+if MYPY_CHECK_RUNNING:
+    from typing import Optional, List
+
 logger = logging.getLogger(__name__)
 
 
@@ -32,6 +37,46 @@ def running_under_virtualenv():
     return _running_under_venv() or _running_under_regular_virtualenv()
 
 
+def _get_pyvenv_cfg_lines():
+    # type: () -> Optional[List[str]]
+    """Reads {sys.prefix}/pyvenv.cfg and returns its contents as list of lines
+
+    Returns None, if it could not read/access the file.
+    """
+    pyvenv_cfg_file = os.path.join(sys.prefix, 'pyvenv.cfg')
+    try:
+        with open(pyvenv_cfg_file) as f:
+            return f.read().splitlines()  # avoids trailing newlines
+    except OSError:
+        return None
+
+
+def _no_global_under_venv():
+    # type: () -> bool
+    """Check `{sys.prefix}/pyvenv.cfg` for system site-packages inclusion
+
+    PEP 405 specifies that when system site-packages are not supposed to be
+    visible from a virtual environment, `pyvenv.cfg` must contain the following
+    line:
+
+        include-system-site-packages = false
+
+    Additionally, log a warning if accessing the file fails.
+    """
+    cfg_lines = _get_pyvenv_cfg_lines()
+    if cfg_lines is None:
+        # We're not in a "sane" venv, so assume there is no system
+        # site-packages access (since that's PEP 405's default state).
+        logger.warning(
+            "Could not access 'pyvenv.cfg' despite a virtual environment "
+            "being active. Assuming global site-packages is not accessible "
+            "in this environment."
+        )
+        return True
+
+    return "include-system-site-packages = false" in cfg_lines
+
+
 def _no_global_under_regular_virtualenv():
     # type: () -> bool
     """Check if "no-global-site-packages.txt" exists beside site.py
@@ -53,5 +98,8 @@ def virtualenv_no_global():
 
     if _running_under_regular_virtualenv():
         return _no_global_under_regular_virtualenv()
+
+    if _running_under_venv():
+        return _no_global_under_venv()
 
     return False
--- tests/unit/test_utils_virtualenv.py
+++ tests/unit/test_utils_virtualenv.py
@@ -1,3 +1,4 @@
+import logging
 import site
 import sys
 
@@ -46,6 +47,8 @@ def test_virtualenv_no_global_with_regular_virtualenv(
     no_global_file,
     expected,
 ):
+    monkeypatch.setattr(virtualenv, '_running_under_venv', lambda: False)
+
     monkeypatch.setattr(site, '__file__', tmpdir / 'site.py')
     monkeypatch.setattr(
         virtualenv, '_running_under_regular_virtualenv',
@@ -55,3 +58,78 @@ def test_virtualenv_no_global_with_regular_virtualenv(
         (tmpdir / 'no-global-site-packages.txt').touch()
 
     assert virtualenv.virtualenv_no_global() == expected
+
+
+@pytest.mark.parametrize(
+    "pyvenv_cfg_lines, under_venv, expected, expect_warning", [
+        (None, False, False, False),
+        (None, True, True, True),  # this has a warning.
+        (
+            [
+                "home = <we do not care>",
+                "include-system-site-packages = true",
+                "version = <we do not care>",
+            ],
+            True,
+            False,
+            False,
+        ),
+        (
+            [
+                "home = <we do not care>",
+                "include-system-site-packages = false",
+                "version = <we do not care>",
+            ],
+            True,
+            True,
+            False,
+        ),
+    ],
+)
+def test_virtualenv_no_global_with_pep_405_virtual_environment(
+    monkeypatch,
+    caplog,
+    pyvenv_cfg_lines,
+    under_venv,
+    expected,
+    expect_warning,
+):
+    monkeypatch.setattr(
+        virtualenv, '_running_under_regular_virtualenv', lambda: False
+    )
+    monkeypatch.setattr(
+        virtualenv, '_get_pyvenv_cfg_lines', lambda: pyvenv_cfg_lines
+    )
+    monkeypatch.setattr(virtualenv, '_running_under_venv', lambda: under_venv)
+
+    with caplog.at_level(logging.WARNING):
+        assert virtualenv.virtualenv_no_global() == expected
+
+    if expect_warning:
+        assert caplog.records
+
+        # Check for basic information
+        message = caplog.records[-1].getMessage().lower()
+        assert "could not access 'pyvenv.cfg'" in message
+        assert "assuming global site-packages is not accessible" in message
+
+
+@pytest.mark.parametrize(
+    "contents, expected", [
+        (None, None),
+        ("", []),
+        ("a = b\nc = d\n", ["a = b", "c = d"]),
+        ("a = b\nc = d", ["a = b", "c = d"]),  # no trailing newlines
+    ]
+)
+def test_get_pyvenv_cfg_lines_for_pep_405_virtual_environment(
+    monkeypatch,
+    tmpdir,
+    contents,
+    expected,
+):
+    monkeypatch.setattr(sys, 'prefix', str(tmpdir))
+    if contents is not None:
+        tmpdir.joinpath('pyvenv.cfg').write_text(contents)
+
+    assert virtualenv._get_pyvenv_cfg_lines() == expected
