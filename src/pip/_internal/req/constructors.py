caf1d659f54d42cc91f77361898e666643094721
lmarvaud <24732919+lmarvaud@users.noreply.github.com>
2019-10-10 15:06:18 +0200

    Allow extra in re-install PEP 508 dependencies when the URL changes

--- src/pip/_internal/req/constructors.py
+++ src/pip/_internal/req/constructors.py
@@ -437,9 +437,17 @@ def install_req_from_req_string(
     if req.url:
         # Create an InstallRequirement for a PEP 508 URL with the same behavior
         # as 'pip install req.url'
-        return install_req_from_line(
-            req.url, comes_from=comes_from, isolated=isolated,
-            wheel_cache=wheel_cache, use_pep517=use_pep517
+        parts = parse_req_from_line(req.url, None)
+        constraint = False
+        options = None
+
+        return InstallRequirement(
+            parts.requirement, comes_from, link=parts.link, markers=parts.markers,
+            use_pep517=use_pep517, isolated=isolated,
+            options=options if options else {},
+            wheel_cache=wheel_cache,
+            constraint=constraint,
+            extras=req.extras,
         )
     return InstallRequirement(
         req, comes_from, isolated=isolated, use_pep517=use_pep517
