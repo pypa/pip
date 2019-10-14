6278375f3b98f6301c552f68edc095fdd1a445a8
Prabhu Marappan <prabhum.794@gmail.com>
2019-10-14 17:31:05 +0530

    Remove format in log methods

--- src/pip/_internal/operations/generate_metadata.py
+++ src/pip/_internal/operations/generate_metadata.py
@@ -95,8 +95,8 @@ def _generate_metadata_legacy(install_req):
     # type: (InstallRequirement) -> str
     req_details_str = install_req.name or "from {}".format(install_req.link)
     logger.debug(
-        'Running setup.py (path:{}) egg_info for package {}'
-        .format(install_req.setup_py_path, req_details_str)
+        'Running setup.py (path:%s) egg_info for package %s',
+        install_req.setup_py_path, req_details_str
     )
 
     # Compose arguments for subprocess call
