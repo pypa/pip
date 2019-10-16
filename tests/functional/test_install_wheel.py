976fb039226cce08432df7d529f9aba9ac003fb2
julienmalard <julien.malard@mail.mcgill.ca>
2019-10-16 14:35:19 -0400

    Added unicode wheel test

--- tests/functional/test_install_wheel.py
+++ tests/functional/test_install_wheel.py
@@ -57,6 +57,21 @@ def test_basic_install_from_wheel(script, data):
     assert script_file in result.files_created
 
 
+def test_basic_install_from_unicode_wheel(script, data):
+    """
+    Test installing from a wheel (that has a script)
+    """
+    result = script.pip(
+        'install', 'test_unicode==1.0', '--no-index',
+        '--find-links=' + data.find_links,
+        expect_error=False,
+    )
+    dist_info_folder = script.site_packages / 'test_unicode-1.0.dist-info'
+    assert dist_info_folder in result.files_created, (dist_info_folder,
+                                                      result.files_created,
+                                                      result.stdout)
+
+
 def test_basic_install_from_wheel_with_extras(script, data):
     """
     Test installing from a wheel with extras.
