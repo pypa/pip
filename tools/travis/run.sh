a7edcc730e2df40cbe42627ff2622fbec253d5db
Chris Hunt <chrahunt@gmail.com>
2019-10-29 02:04:55 -0400

    Normalize parallelization parameter.

--- tools/travis/run.sh
+++ tools/travis/run.sh
@@ -41,12 +41,12 @@ echo "TOXENV=${TOXENV}"
 set -x
 if [[ "$GROUP" == "1" ]]; then
     # Unit tests
-    tox -- --use-venv -m unit
+    tox -- --use-venv -m unit -n auto
     # Integration tests (not the ones for 'pip install')
-    tox -- --use-venv -m integration -n 4 --duration=5 -k "not test_install"
+    tox -- --use-venv -m integration -n auto --duration=5 -k "not test_install"
 elif [[ "$GROUP" == "2" ]]; then
     # Separate Job for running integration tests for 'pip install'
-    tox -- --use-venv -m integration -n 4 --duration=5 -k "test_install"
+    tox -- --use-venv -m integration -n auto --duration=5 -k "test_install"
 else
     # Non-Testing Jobs should run once
     tox
