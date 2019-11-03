911722173ec3b7ed3b1c3ff5a2637a7340c904e4
Pradyun Gedam <pradyunsg@gmail.com>
2019-11-03 15:25:49 +0530

    Move distributions.{source.legacy -> source}
    Why: Based on some more experience from refactoring metadata generation,
    it became clear to me that the separation of legacy vs modern codepaths
    should happen at lower level than this abstraction.

--- src/pip/_internal/distributions/source.py
+++ src/pip/_internal/distributions/source.py
@@ -16,11 +16,6 @@ class SourceDistribution(AbstractDistribution):
 
     The preparation step for these needs metadata for the packages to be
     generated, either using PEP 517 or using the legacy `setup.py egg_info`.
-
-    NOTE from @pradyunsg (14 June 2019)
-    I expect SourceDistribution class will need to be split into
-    `legacy_source` (setup.py based) and `source` (PEP 517 based) when we start
-    bringing logic for preparation out of InstallRequirement into this class.
     """
 
     def get_pkg_resources_distribution(self):
