85d3193be77c2d1df12fd0efbbd4a6c0f7ebb77e
Michael Felt <michael@felt.demon.nl>
2019-11-20 20:34:22 +0000

    update per submission to pypa/packaging

--- src/pip/_internal/_AIX_platform.py
+++ src/pip/_internal/_AIX_platform.py
@@ -1,84 +1,82 @@
 """Shared AIX support functions."""
 
 import sys
-from subprocess import check_output
+import subprocess
 from sysconfig import get_config_var
 
-# from ._typing import MYPY_CHECK_RUNNING
+from ._typing import MYPY_CHECK_RUNNING
+
+if MYPY_CHECK_RUNNING:  # pragma: no cover
+    from typing import List
 
 """
-AIX filesets are identified by four decimal values aka VRMF.
-V (version) is the value returned by "uname -v"
-R (release) is the value returned by "uname -r"
-M and F values are not available via uname
-There is a fifth, lessor known value: builddate that
-is expressed as YYWW (Year WeekofYear)
+AIX filesets are identified by four decimal values: V.R.M.F.
+V (version) and R (release) can be retreived using ``uname``
+M(aintenance) and F(ix) values are not available via uname
+For determining ABI compatibility the values V.R.M
+are essential - suplemented by a final value known as
+`builddate` that is expressed as YYWW (Year WeekofYear)
 
-The fileset bos.mp64 contains the AIX kernel and it's
-VRMF and builddate are equivalent to installed
-levels of the runtime environment.
-The program lslpp is used to get these values.
-The pep425 platform tag for AIX becomes:
-AIX.VRTL.YYWW.SZ, e.g., AIX.6107.1415.32
+The values for V.R.M of the system Python was built on are available in
+the variable sysconfig.get_config_var("BUILD_GNU_TYPE"). The system builddate
+value may be available in sys.config.get_config_var("AIX_BUILDDATE")
+When all of these values on a running system are greater or equal to the
+values of the build system then the bdist modules are binary compatible
+with the AIX kernel.
 
-The routines here convert VRMF values to VRTL and
-adding BUILDDATE and bitness for comparision with
-other AIX platform tags
+For pep425 purposes the AIX platform tag becomes:
+"AIX.{}.{:04d}.{}".format(vmtl, builddate, bitsize)
+e.g., "AIX.6107.1415.32" for AIX 6.1 TL7 bd 1415, 32-bit
+and, "AIX.6107.1415.64" for AIX 6.1 TL7 bd 1415, 64-bit
 """
 
+# _bd - set to impossible setting year98-week98 if None
+_tmp = str(get_config_var("AIX_BUILDDATE"))
+_bd = 9898 if (_tmp == "None") else int(_tmp)
 _bgt = get_config_var("BUILD_GNU_TYPE")
-
 _is_32bit = sys.maxsize == 2147483647
 
 
 def _aix_tag(vrtl, bd):
     # type: (int, int) -> str
     sz = 32 if _is_32bit else 64
-    return "AIX.{:04d}.{}.{}".format(vrtl, bd, sz)
+    return "AIX.{}.{:04d}.{}".format(vrtl, bd, sz)
 
 
-# extract vrtl from the VRMF string
+# compute vrtl from the VRMF string
 def _aix_vrtl(vrmf):
     # type: (str) -> int
     v, r, tl = vrmf.split(".")[:3]
     return int("{}{}{:02d}".format(v[-1], r, int(tl)))
 
 
-# extract vrtl from the BUILD_GNU_TYPE as an int
-def _aix_bgt():
-    # type: () -> int
-    assert(_bgt)
-    return(_aix_vrtl(_bgt))
-
-
-# return the AIX_BUILDDATE, or 9898 if not defined
-def _aix_bd():
-    # type: () -> int
-    bd = get_config_var("AIX_BUILDDATE")
-    # if bd == "" or None set Builddate value to an impossible value
-    # year XX98-week98
-    if not bd:
-        return 9898
-    else:
-        return int(bd)
-
-
-def aix_buildtag():
-    # type: () -> str
-    return _aix_tag(_aix_bgt(), _aix_bd())
-
-
-def _aix_platform():
-    # type: () -> str
-    vrmf = ""
-    tmp = check_output(["/usr/bin/lslpp", "-Lqc", "bos.mp64"])
+def _aix_bosmp64():
+    # type: () -> List[str]
+    """
+    The fileset bos.mp64 is the AIX kernel. It's VRMF and builddate
+    reflect the current levels of the runtime environment.
+    """
+    tmp = subprocess.check_output(["/usr/bin/lslpp", "-Lqc", "bos.mp64"])
     tmp = tmp.decode("utf-8").strip().split(":")  # type: ignore
-    lpp, vrmf, bd = list(tmp[index] for index in [0, 2, -1])  # type: ignore
+    # lpp, vrmf, bd = list(tmp[index] for index in [0, 2, -1])  # type: ignore
+    # e.g., ['bos.mp64', '7.1.4.34', '1806']
+    return list(tmp[index] for index in [0, 2, -1])
+
+
+def aix_platform():
+    # type: () -> str
+    lpp, vrmf, bd = _aix_bosmp64()
     assert lpp == "bos.mp64", "%s != %s" % (lpp, "bos.mp64")
     return _aix_tag(_aix_vrtl(vrmf), int(bd))
 
 
-def get_platform():
+# extract vrtl from the BUILD_GNU_TYPE as an int
+def _aix_bgt():
+    # type: () -> int
+    assert _bgt
+    return _aix_vrtl(_bgt)
+
+
+def aix_buildtag():
     # type: () -> str
-    """Return AIX platform tag"""
-    return _aix_platform()
+    return _aix_tag(_aix_bgt(), _bd)
