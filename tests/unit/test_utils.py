7aaa705c15b16b13ac64807d1d23dcd797f5977d
Chris Hunt <chrahunt@gmail.com>
2019-11-23 18:02:58 -0500

    Remove unused glibc functions
    The remaining glibc-related functions are required to allow us to put
    the libc version in the pip user agent (for stats).

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -10,7 +10,6 @@ import shutil
 import stat
 import sys
 import time
-import warnings
 from io import BytesIO
 
 import pytest
@@ -24,7 +23,6 @@ from pip._internal.exceptions import (
 from pip._internal.utils.deprecation import PipDeprecationWarning, deprecated
 from pip._internal.utils.encoding import BOMS, auto_decode
 from pip._internal.utils.glibc import (
-    check_glibc_version,
     glibc_version_string,
     glibc_version_string_confstr,
     glibc_version_string_ctypes,
@@ -538,38 +536,6 @@ def raises(error):
 
 
 class TestGlibc(object):
-    def test_manylinux_check_glibc_version(self):
-        """
-        Test that the check_glibc_version function is robust against weird
-        glibc version strings.
-        """
-        for two_twenty in ["2.20",
-                           # used by "linaro glibc", see gh-3588
-                           "2.20-2014.11",
-                           # weird possibilities that I just made up
-                           "2.20+dev",
-                           "2.20-custom",
-                           "2.20.1",
-                           ]:
-            assert check_glibc_version(two_twenty, 2, 15)
-            assert check_glibc_version(two_twenty, 2, 20)
-            assert not check_glibc_version(two_twenty, 2, 21)
-            assert not check_glibc_version(two_twenty, 3, 15)
-            assert not check_glibc_version(two_twenty, 1, 15)
-
-        # For strings that we just can't parse at all, we should warn and
-        # return false
-        for bad_string in ["asdf", "", "foo.bar"]:
-            with warnings.catch_warnings(record=True) as ws:
-                warnings.filterwarnings("always")
-                assert not check_glibc_version(bad_string, 2, 5)
-                for w in ws:
-                    if "Expected glibc version with" in str(w.message):
-                        break
-                else:
-                    # Didn't find the warning we were expecting
-                    assert False
-
     @pytest.mark.skipif("sys.platform == 'win32'")
     def test_glibc_version_string(self, monkeypatch):
         monkeypatch.setattr(
