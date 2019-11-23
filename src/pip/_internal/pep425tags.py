1c8c481214f89df6ea50659646f537388c80ea2d
Chris Hunt <chrahunt@gmail.com>
2019-11-23 18:02:30 -0500

    Only calculate py-compatible tags in one place
    Since `_compatible_tags` is the function that will be responsible for
    generating the non-interpreter-specific tags, we remove the
    corresponding sections from `_cpython_tags` and `_generic_tags`. The
    resulting tags in `get_supported` are equivalent because these were
    the last tags to be computed in those functions, and `_compatible_tags`
    is executed after them (so any non-duplicate tags it produces will be
    last).
    
    To reinforce the reponsibility of `_compatible_tags` we also remove the
    abi-related tag generation, which is already handled in `_cpython_tags`
    and `_generic_tags`.

--- src/pip/_internal/pep425tags.py
+++ src/pip/_internal/pep425tags.py
@@ -343,19 +343,6 @@ def _cpython_tags(
             for arch in arches:
                 supported.append(("%s%s" % (impl, version), "abi3", arch))
 
-    # Has binaries, does not use the Python API:
-    for arch in arches:
-        supported.append(('py%s' % (current_version[0]), 'none', arch))
-
-    # No abi / arch, but requires our implementation:
-    supported.append(('%s%s' % (impl, current_version), 'none', 'any'))
-
-    # No abi / arch, generic Python
-    supported.append(('py%s' % (current_version,), 'none', 'any'))
-    supported.append(('py%s' % (current_version[0]), 'none', 'any'))
-    for version in other_versions:
-        supported.append(('py%s' % (version,), 'none', 'any'))
-
     return supported
 
 
@@ -408,19 +395,6 @@ def _generic_tags(
             for arch in arches:
                 supported.append(("%s%s" % (impl, version), "abi3", arch))
 
-    # Has binaries, does not use the Python API:
-    for arch in arches:
-        supported.append(('py%s' % (current_version[0]), 'none', arch))
-
-    # No abi / arch, but requires our implementation:
-    supported.append(('%s%s' % (impl, current_version), 'none', 'any'))
-
-    # No abi / arch, generic Python
-    supported.append(('py%s' % (current_version,), 'none', 'any'))
-    supported.append(('py%s' % (current_version[0]), 'none', 'any'))
-    for version in other_versions:
-        supported.append(('py%s' % (version,), 'none', 'any'))
-
     return supported
 
 
@@ -444,34 +418,7 @@ def _compatible_tags(
 
     impl = impl or interpreter_name()
 
-    abis = []  # type: List[str]
-
-    abi = abi or get_abi_tag()
-    if abi:
-        abis[0:0] = [abi]
-
-    supports_abi3 = not PY2 and impl == "cp"
-
-    if supports_abi3:
-        abis.append("abi3")
-
-    abis.append('none')
-
     arches = _get_custom_platforms(platform or get_platform(), platform)
-
-    # Current version, current API (built specifically for our Python):
-    for abi in abis:
-        for arch in arches:
-            supported.append(('%s%s' % (impl, current_version), abi, arch))
-
-    # abi3 modules compatible with older version of Python
-    if supports_abi3:
-        for version in other_versions:
-            # abi3 was introduced in Python 3.2
-            if version in {'31', '30'}:
-                break
-            for arch in arches:
-                supported.append(("%s%s" % (impl, version), "abi3", arch))
 
     # Has binaries, does not use the Python API:
     for arch in arches:
