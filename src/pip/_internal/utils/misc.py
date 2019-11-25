f856c6371e58fe14e04f7d35f47e904f418d9387
BorisZZZ <BorisZZZ@users.noreply.github.com>
2019-11-25 08:55:35 +0200

    Update misc.py

--- src/pip/_internal/utils/misc.py
+++ src/pip/_internal/utils/misc.py
@@ -98,6 +98,7 @@ def ensure_dir(path):
     try:
         os.makedirs(path)
     except OSError as e:
+        # Windows can raise spurious ENOTEMPTY errors. See #6426.
         if e.errno != errno.EEXIST and e.errno != errno.ENOTEMPTY:
             raise
 
