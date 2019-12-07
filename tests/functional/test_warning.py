b48b38e5eb04228ae76235167cbe4cf383570cfd
Victor V. Paulo <victorvpaulo@gmail.com>
2019-12-07 05:17:02 -0300

    Make tests not skip if implementation is not CPython

--- tests/functional/test_warning.py
+++ tests/functional/test_warning.py
@@ -1,6 +1,7 @@
 import textwrap
 
 import pytest
+import platform
 
 
 @pytest.fixture
@@ -30,38 +31,32 @@ def test_deprecation_warnings_can_be_silenced(script, warnings_demo):
     assert result.stderr == ''
 
 
-deprecation_message = "DEPRECATION: Python 2.7 will reach the" \
-                      " end of its life on January 1st, 2020." \
-                      " Please upgrade your Python as Python 2.7" \
-                      " won't be maintained after that date. " \
-                      "A future version of pip will drop support" \
-                      " for Python 2.7. More details about Python 2" \
-                      " support in pip, can be found at" \
-                      " https://pip.pypa.io/en/latest/development/release-process/#python-2-support"  # noqa
+DEPRECATION_TEXT = "drop support for Python 2.7"
+CPYTHON_DEPRECATION_TEXT = "January 1st, 2020"
 
 
-@pytest.mark.skipif(
-    "sys.version_info[:2] in [(2, 7)] "
-    "and platform.python_implementation() == 'CPython'")
+@pytest.mark.skipif("sys.version_info[:2] in [(2, 7)]")
 def test_version_warning_is_not_shown_if_python_version_is_not_27(script):
     result = script.pip("debug", allow_stderr_warning=True)
-    assert deprecation_message not in result.stderr, str(result)
+    assert DEPRECATION_TEXT not in result.stderr, str(result)
+    assert CPYTHON_DEPRECATION_TEXT not in result.stderr, str(result)
 
 
-@pytest.mark.skipif(
-    "not (sys.version_info[:2] in [(2, 7)] "
-    "and platform.python_implementation() == 'CPython')")
+@pytest.mark.skipif("not sys.version_info[:2] in [(2, 7)]")
 def test_version_warning_is_shown_if_python_version_is_27(script):
     result = script.pip("debug", allow_stderr_warning=True)
-    assert deprecation_message in result.stderr, str(result)
+    assert DEPRECATION_TEXT in result.stderr, str(result)
+    if platform.python_implementation() == 'CPython':
+        assert CPYTHON_DEPRECATION_TEXT in result.stderr, str(result)
+    else:
+        assert CPYTHON_DEPRECATION_TEXT not in result.stderr, str(result)
 
 
-@pytest.mark.skipif(
-    "not (sys.version_info[:2] in [(2, 7)] "
-    "and platform.python_implementation() == 'CPython')")
+@pytest.mark.skipif("not sys.version_info[:2] in [(2, 7)]")
 def test_version_warning_is_not_shown_when_flag_is_passed(script):
     result = script.pip(
         "debug", "--no-python-version-warning", allow_stderr_warning=True
     )
-    assert deprecation_message not in result.stderr, str(result)
+    assert DEPRECATION_TEXT not in result.stderr, str(result)
+    assert CPYTHON_DEPRECATION_TEXT not in result.stderr, str(result)
     assert "--no-python-version-warning" not in result.stderr
