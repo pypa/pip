9fe926f3f10da3bf4f9b2578a5c4cc60ff0dcd96
Victor V. Paulo <victorvpaulo@gmail.com>
2019-12-07 06:03:30 -0300

    Modify test names in test_warning to improve clarity

--- tests/functional/test_warning.py
+++ tests/functional/test_warning.py
@@ -38,14 +38,14 @@ CPYTHON_DEPRECATION_TEXT = "January 1st, 2020"
 
 
 @skip_if_python2
-def test_version_warning_is_not_shown_if_python_version_is_not_27(script):
+def test_version_warning_is_not_shown_if_python_version_is_not_2(script):
     result = script.pip("debug", allow_stderr_warning=True)
     assert DEPRECATION_TEXT not in result.stderr, str(result)
     assert CPYTHON_DEPRECATION_TEXT not in result.stderr, str(result)
 
 
 @skip_if_not_python2
-def test_version_warning_is_shown_if_python_version_is_27(script):
+def test_version_warning_is_shown_if_python_version_is_2(script):
     result = script.pip("debug", allow_stderr_warning=True)
     assert DEPRECATION_TEXT in result.stderr, str(result)
     if platform.python_implementation() == 'CPython':
