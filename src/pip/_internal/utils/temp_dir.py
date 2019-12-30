ae887ac51c38049c899161e1e5fd173fe40d40c9
Dan Davis <daniel.davis@nih.gov>
2019-12-30 18:21:40 -0500

    Addresses #7280 - More targeted fix to problem
    - Try to delete the temporary directory, as most of the time this works
    - If it fails on windows due to an Access error, it clearly is not an
      Access error because we just created the directory.
      Warn the user and continue.

--- src/pip/_internal/utils/temp_dir.py
+++ src/pip/_internal/utils/temp_dir.py
@@ -4,6 +4,7 @@ import errno
 import itertools
 import logging
 import os.path
+import sys
 import tempfile
 from contextlib import contextmanager
 
@@ -124,7 +125,21 @@ class TempDirectory(object):
         """
         self._deleted = True
         if os.path.exists(self._path):
-            rmtree(self._path)
+            try:
+                rmtree(self._path)
+            except OSError as e:
+                skip_error = (
+                    sys.platform == 'win32' and
+                    e.errno == errno.EACCES and
+                    getattr(e, 'winerror', 0) in {5, 32}
+                )
+                if skip_error:
+                    logger.warning(
+                        "%s (virus scanner may be holding it)."
+                        "cannot remove '%s'",
+                        e.strerror, e.filename)
+                else:
+                    raise
 
 
 class AdjacentTempDirectory(TempDirectory):
