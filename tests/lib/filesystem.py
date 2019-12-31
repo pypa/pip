942ab14f00c59d80d5536dae099afca07ac4499e
Dan Davis <daniel.davis@nih.gov>
2019-12-31 17:30:09 -0500

    Hardening test cases for #7280
    - Provide a class that manages a sub-process which can open a file and
      monkey with pip's abiliity to remove a file on windows.
    - This class can function as both a context manager and/or a pytest fixture.

--- tests/lib/filesystem.py
+++ tests/lib/filesystem.py
@@ -1,9 +1,11 @@
 """Helpers for filesystem-dependent tests.
 """
 import os
+import multiprocessing
 import socket
 import subprocess
 import sys
+import traceback
 from functools import partial
 from itertools import chain
 
@@ -32,6 +34,95 @@ def make_unreadable_file(path):
         # leave everything else.
         args = ["icacls", path, "/deny", username + ":(RD)"]
         subprocess.check_call(args)
+
+
+if sys.platform == 'win32':
+    def lock_action(f):
+        pass
+else:
+    def lock_action(f):
+        pass
+
+
+def external_file_opener(conn):
+    """
+    This external process is run with multiprocessing.
+    It waits for a path from the parent, opens it, and then wait for another message before closing it.
+
+    :param conn: bi-directional pipe
+    :return: nothing
+    """
+    f = None
+    try:
+        # Wait for parent to send path
+        msg = conn.recv()
+        if msg is True:
+            # Do nothing - we have been told to exit without a path or action
+            pass
+        else:
+            path, action = msg
+            # Open the file
+            try:
+                f = open(path, 'r')
+                if action == 'lock':
+                    lock_action(f)
+                elif action == 'noread':
+                    make_unreadable_file(path)
+            except OSError as e:
+                traceback.print_exc(None, sys.stderr)
+
+            # Indicate the file is opened
+            conn.send(True)
+            # Now path is open and we wait for signal to exit
+            conn.recv()
+    finally:
+        if f:
+            f.close()
+        conn.close()
+
+
+class FileOpener(object):
+    """
+    Test class acts as a context manager which can open a file from another process, and hold it open
+    to assure that this does not interfere with pip's operations.
+
+    If a path is passed to the FileOpener, it immediately sends a message to the other process to
+    open that path.  An action of "lock" or "noread" can also be sent to the subprocess, resulting
+    in various additional monkey wrenches now and in the future.
+
+    Opening the path and taking the action can be deferred however, so that the FileOpener may
+    function as a pytest fixture if so desired.
+    """
+    def __init__(self,
+                 path=None, # type: Optional(str)
+                 action=None,  # type: Optional(str)
+                 ):
+        self.path = None
+        self.conn, child_conn = multiprocessing.Pipe()
+        self.child = multiprocessing.Process(target=external_file_opener, daemon=True, args=(child_conn,))
+        self.child.start()
+        if path:
+            self.send(path, action)
+
+    def send(self, path, action=None):
+        if self.path is not None:
+            raise AttributeError('path may only be set once')
+        self.path = str(path)
+        self.conn.send( (str(path), action) )
+        return self.conn.recv()
+
+    def cleanup(self):
+        # send a message to the child to exit; it will exit whether the path was sent or not
+        if self.child:
+            self.conn.send(True)
+            self.child.join()
+        self.child = None
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.cleanup()
 
 
 def get_filelist(base):
