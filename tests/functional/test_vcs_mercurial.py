e5c43ed6afcd9f05ff0bc51f560b3f2c6dd34045
Tzu-ping Chung <uranusjr@gmail.com>
2020-01-14 16:45:28 +0800

    Add tests for get_repository_root

---
+++ tests/functional/test_vcs_mercurial.py
@@ -0,0 +1,14 @@
+from pip._internal.vcs.mercurial import Mercurial
+from tests.lib import _create_test_package
+
+
+def test_get_repository_root(script):
+    version_pkg_path = _create_test_package(script, vcs="hg")
+    tests_path = version_pkg_path.joinpath("tests")
+    tests_path.mkdir()
+
+    root1 = Mercurial.get_repository_root(version_pkg_path)
+    assert root1 == version_pkg_path
+
+    root2 = Mercurial.get_repository_root(version_pkg_path.joinpath("tests"))
+    assert root2 == version_pkg_path
