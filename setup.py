f29e421847d9bad3fb030339b85e706c2bec0179
KOLANICH <kolan_n@mail.ru>
2020-02-13 13:10:34 +0300

    Moved the metadata into `setup.cfg`.

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ import codecs
 import os
 import sys
 
-from setuptools import find_packages, setup
+from setuptools import setup
+
+__requires__ = ("setuptools >= 30.3.0", )
 
 
 def read(rel_path):
@@ -26,52 +28,8 @@ def get_version(rel_path):
         raise RuntimeError("Unable to find version string.")
 
 
-long_description = read('README.rst')
-
 setup(
-    name="pip",
     version=get_version("src/pip/__init__.py"),
-    description="The PyPA recommended tool for installing Python packages.",
-    long_description=long_description,
-
-    license='MIT',
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Topic :: Software Development :: Build Tools",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
-    ],
-    url='https://pip.pypa.io/',
-    keywords='distutils easy_install egg setuptools wheel virtualenv',
-    project_urls={
-        "Documentation": "https://pip.pypa.io",
-        "Source": "https://github.com/pypa/pip",
-    },
-
-    author='The pip developers',
-    author_email='pypa-dev@groups.google.com',
-
-    package_dir={"": "src"},
-    packages=find_packages(
-        where="src",
-        exclude=["contrib", "docs", "tests*", "tasks"],
-    ),
-    package_data={
-        "pip._vendor.certifi": ["*.pem"],
-        "pip._vendor.requests": ["*.pem"],
-        "pip._vendor.distlib._backport": ["sysconfig.cfg"],
-        "pip._vendor.distlib": ["t32.exe", "t64.exe", "w32.exe", "w64.exe"],
-    },
     entry_points={
         "console_scripts": [
             "pip=pip._internal.cli.main:main",
@@ -81,7 +39,4 @@ setup(
             ),
         ],
     },
-
-    zip_safe=False,
-    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
 )
