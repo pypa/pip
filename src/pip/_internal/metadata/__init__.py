aa75a1d6b4a955386ca46fca2a4b1924ba3e00c8
Tzu-ping Chung <uranusjr@gmail.com>
2020-02-26 02:01:48 +0800

    Fix conditional Mypy type

--- src/pip/_internal/metadata/__init__.py
+++ src/pip/_internal/metadata/__init__.py
@@ -6,24 +6,22 @@ The pkg_resources implementation is used on Python 2, otherwise we use
 importlib_metadata.
 """
 
-__all__ = [
-    "Distribution",
-    "get_file_lines",
-    "get_metadata",
-]
-
 import sys
 
 from pip._internal.utils.typing import MYPY_CHECK_RUNNING
 
-if sys.version_info < (3, 5):
-    from pip._internal.metadata import _pkg_resources as impl
-else:
+if sys.version_info >= (3, 5):
     from pip._internal.metadata import _importlib_metadata as impl
-
+else:
+    from pip._internal.metadata import _pkg_resources as impl
 
 if MYPY_CHECK_RUNNING:
-    Distribution = impl.Distribution
+    from typing import Union
+    from pip._internal.metadata import _importlib_metadata as _i
+    from pip._internal.metadata import _pkg_resources as _p
+
+    Distribution = Union[_i.Distribution, _p.Distribution]
+
 
 get_file_lines = impl.get_file_lines
 
