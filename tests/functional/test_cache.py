88582379038dbb91c13d5bc1b78ed5585e994cfb
Ellen Marie Dash <me@duckie.co>
2020-03-10 15:08:18 -0400

    [tests/functional/test_cache] Refactor list_matches_wheel() and remove_matches_wheel().

--- tests/functional/test_cache.py
+++ tests/functional/test_cache.py
@@ -60,26 +60,29 @@ def empty_wheel_cache(wheel_cache_dir):
         shutil.rmtree(wheel_cache_dir)
 
 
-def list_matches_wheel(wheel_name, lines):
-    """Returns True if any line in `lines`, which should be the output of
+def list_matches_wheel(wheel_name, result):
+    """Returns True if any line in `result`, which should be the output of
     a `pip cache list` call, matches `wheel_name`.
 
     E.g., If wheel_name is `foo-1.2.3` it searches for a line starting with
           `- foo-1.2.3-py3-none-any.whl `."""
+    lines = result.stdout.splitlines()
     expected = ' - {}-py3-none-any.whl '.format(wheel_name)
     return any(map(lambda l: l.startswith(expected), lines))
 
 
 @pytest.fixture
 def remove_matches_wheel(wheel_cache_dir):
-    """Returns True if any line in `lines`, which should be the output of
+    """Returns True if any line in `result`, which should be the output of
     a `pip cache remove`/`pip cache purge` call, matches `wheel_name`.
 
     E.g., If wheel_name is `foo-1.2.3`, it searches for a line equal to
     `Removed <wheel cache dir>/arbitrary/pathname/foo-1.2.3-py3-none-any.whl`.
     """
 
-    def _remove_matches_wheel(wheel_name, lines):
+    def _remove_matches_wheel(wheel_name, result):
+        lines = result.stdout.splitlines()
+
         wheel_filename = '{}-py3-none-any.whl'.format(wheel_name)
 
         # The "/arbitrary/pathname/" bit is an implementation detail of how
@@ -106,11 +109,11 @@ def test_cache_list(script):
     """Running `pip cache list` should return exactly what the
     populate_wheel_cache fixture adds."""
     result = script.pip('cache', 'list')
-    lines = result.stdout.splitlines()
-    assert list_matches_wheel('yyy-1.2.3', lines)
-    assert list_matches_wheel('zzz-4.5.6', lines)
-    assert list_matches_wheel('zzz-4.5.7', lines)
-    assert list_matches_wheel('zzz-7.8.9', lines)
+
+    assert list_matches_wheel('yyy-1.2.3', result)
+    assert list_matches_wheel('zzz-4.5.6', result)
+    assert list_matches_wheel('zzz-4.5.7', result)
+    assert list_matches_wheel('zzz-7.8.9', result)
 
 
 @pytest.mark.usefixtures("empty_wheel_cache")
@@ -132,12 +135,11 @@ def test_cache_list_name_match(script):
     """Running `pip cache list zzz` should list zzz-4.5.6, zzz-4.5.7,
     zzz-7.8.9, but nothing else."""
     result = script.pip('cache', 'list', 'zzz', '--verbose')
-    lines = result.stdout.splitlines()
 
-    assert not list_matches_wheel('yyy-1.2.3', lines)
-    assert list_matches_wheel('zzz-4.5.6', lines)
-    assert list_matches_wheel('zzz-4.5.7', lines)
-    assert list_matches_wheel('zzz-7.8.9', lines)
+    assert not list_matches_wheel('yyy-1.2.3', result)
+    assert list_matches_wheel('zzz-4.5.6', result)
+    assert list_matches_wheel('zzz-4.5.7', result)
+    assert list_matches_wheel('zzz-7.8.9', result)
 
 
 @pytest.mark.usefixtures("populate_wheel_cache")
@@ -145,12 +147,11 @@ def test_cache_list_name_and_version_match(script):
     """Running `pip cache list zzz-4.5.6` should list zzz-4.5.6, but
     nothing else."""
     result = script.pip('cache', 'list', 'zzz-4.5.6', '--verbose')
-    lines = result.stdout.splitlines()
 
-    assert not list_matches_wheel('yyy-1.2.3', lines)
-    assert list_matches_wheel('zzz-4.5.6', lines)
-    assert not list_matches_wheel('zzz-4.5.7', lines)
-    assert not list_matches_wheel('zzz-7.8.9', lines)
+    assert not list_matches_wheel('yyy-1.2.3', result)
+    assert list_matches_wheel('zzz-4.5.6', result)
+    assert not list_matches_wheel('zzz-4.5.7', result)
+    assert not list_matches_wheel('zzz-7.8.9', result)
 
 
 @pytest.mark.usefixture("populate_wheel_cache")
@@ -170,12 +171,11 @@ def test_cache_remove_name_match(script, remove_matches_wheel):
     """Running `pip cache remove zzz` should remove zzz-4.5.6 and zzz-7.8.9,
     but nothing else."""
     result = script.pip('cache', 'remove', 'zzz', '--verbose')
-    lines = result.stdout.splitlines()
 
-    assert not remove_matches_wheel('yyy-1.2.3', lines)
-    assert remove_matches_wheel('zzz-4.5.6', lines)
-    assert remove_matches_wheel('zzz-4.5.7', lines)
-    assert remove_matches_wheel('zzz-7.8.9', lines)
+    assert not remove_matches_wheel('yyy-1.2.3', result)
+    assert remove_matches_wheel('zzz-4.5.6', result)
+    assert remove_matches_wheel('zzz-4.5.7', result)
+    assert remove_matches_wheel('zzz-7.8.9', result)
 
 
 @pytest.mark.usefixtures("populate_wheel_cache")
@@ -183,27 +183,28 @@ def test_cache_remove_name_and_version_match(script, remove_matches_wheel):
     """Running `pip cache remove zzz-4.5.6` should remove zzz-4.5.6, but
     nothing else."""
     result = script.pip('cache', 'remove', 'zzz-4.5.6', '--verbose')
-    lines = result.stdout.splitlines()
 
-    assert not remove_matches_wheel('yyy-1.2.3', lines)
-    assert remove_matches_wheel('zzz-4.5.6', lines)
-    assert not remove_matches_wheel('zzz-4.5.7', lines)
-    assert not remove_matches_wheel('zzz-7.8.9', lines)
+    assert not remove_matches_wheel('yyy-1.2.3', result)
+    assert remove_matches_wheel('zzz-4.5.6', result)
+    assert not remove_matches_wheel('zzz-4.5.7', result)
+    assert not remove_matches_wheel('zzz-7.8.9', result)
 
 
 @pytest.mark.usefixtures("populate_wheel_cache")
 def test_cache_purge(script, remove_matches_wheel):
+    """Running `pip cache purge` should remove all cached wheels."""
     result = script.pip('cache', 'purge', '--verbose')
-    lines = result.stdout.splitlines()
 
-    assert remove_matches_wheel('yyy-1.2.3', lines)
-    assert remove_matches_wheel('zzz-4.5.6', lines)
-    assert remove_matches_wheel('zzz-4.5.7', lines)
-    assert remove_matches_wheel('zzz-7.8.9', lines)
+    assert remove_matches_wheel('yyy-1.2.3', result)
+    assert remove_matches_wheel('zzz-4.5.6', result)
+    assert remove_matches_wheel('zzz-4.5.7', result)
+    assert remove_matches_wheel('zzz-7.8.9', result)
 
 
 @pytest.mark.usefixtures("populate_wheel_cache")
 def test_cache_purge_too_many_args(script, wheel_cache_files):
+    """Running `pip cache purge aaa` should raise an error and remove no
+    cached wheels."""
     result = script.pip('cache', 'purge', 'aaa', '--verbose',
                         expect_error=True)
     assert result.stdout == ''
