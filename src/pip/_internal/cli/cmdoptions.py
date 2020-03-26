afb4e6f5ba84a4baf6484a622deaa8955da23bfb
Noah Gorny <noah.bar.ilan@gmail.com>
2020-03-26 21:01:41 +0200

    cli: cmdoptions: Mark always_unzip as depreceated

--- src/pip/_internal/cli/cmdoptions.py
+++ src/pip/_internal/cli/cmdoptions.py
@@ -832,6 +832,10 @@ always_unzip = partial(
     action='store_true',
     help=SUPPRESS_HELP,
 )  # type: Callable[..., Option]
+# TODO: Move into a class that inherits from partial, currently does not
+# work as mypy complains functools.partial is a generic class.
+# This way we know we can ignore this option in docs auto generation
+setattr(always_unzip, 'deprecated', True)
 
 
 def _handle_merge_hash(option, opt_str, value, parser):
