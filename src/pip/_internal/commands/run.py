276bd846c1f2ed273d13cdb5282afd9b2ed4c9ef
Jason R. Coombs <jaraco@jaraco.com>
2020-04-10 11:05:41 -0400

    Mark select functions as provisional or conditionally provisional.

--- src/pip/_internal/commands/run.py
+++ src/pip/_internal/commands/run.py
@@ -3,9 +3,41 @@
 
 from __future__ import absolute_import
 
+import functools
+import logging
+
 from pip._vendor import pip_run
 
 from pip._internal.cli.base_command import SUCCESS, Command
+
+logger = logging.getLogger(__name__)
+
+
+def provisional(msg):
+    """
+    Unconditionally warn when func is invoked.
+    """
+    return provisional_eval(msg, cond=lambda result: True)
+
+
+def provisional_eval(msg, cond=bool):
+    """
+    Warn if cond(func result) evaluates True.
+    """
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+            if cond(result):
+                logger.warning(msg)
+            return result
+        return wrapper
+    return decorator
+
+
+pip_run.scripts.DepsReader.try_read = classmethod(provisional_eval(
+    msg="Support for reading requirements from a script is provisional.")(
+    vars(pip_run.scripts.DepsReader)['try_read'].__func__))
 
 
 class RunCommand(Command):
