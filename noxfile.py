e8613ffcaa1cb9fd615bae26a333205c1249baed
Paul Moore <p.f.moore@gmail.com>
2020-04-14 11:03:48 +0100

    Remove pip version requirement for vendoring

--- noxfile.py
+++ noxfile.py
@@ -152,9 +152,6 @@ def lint(session):
 
 @nox.session
 def vendoring(session):
-    # Required, otherwise we interpret --no-binary :all: as
-    # "do not build wheels", which fails for PEP 517 requirements
-    session.install("-U", "pip>=19.3.1")
     session.install("vendoring")
 
     session.run("vendoring", "sync", ".", "-v")
