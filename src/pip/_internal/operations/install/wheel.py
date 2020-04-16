a9c8dc5468a6dd39e20fa12ee21718b673189c38
cjc7373 <niuchangcun@163.com>
2020-04-16 20:59:03 +0800

    fix normalize path for Windows

--- src/pip/_internal/operations/install/wheel.py
+++ src/pip/_internal/operations/install/wheel.py
@@ -262,7 +262,11 @@ def _record_to_fs_path(record_path):
 def _fs_to_record_path(path, relative_to=None):
     # type: (text_type, Optional[text_type]) -> RecordPath
     if relative_to is not None:
-        path = os.path.relpath(path, relative_to)
+        # On Windows, do not handle relative paths if they belong to different
+        # logical disks
+        if os.path.splitdrive(path)[0].lower() == \
+                os.path.splitdrive(relative_to)[0].lower():
+            path = os.path.relpath(path, relative_to)
     path = path.replace(os.path.sep, '/')
     return cast('RecordPath', path)
 
