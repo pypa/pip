90eef6dfa81e00a1f64f425df368c6e6c52798d2
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-04-25 13:35:04 +0200

    Fix E501 (line too long)

--- tests/functional/test_color.py
+++ tests/functional/test_color.py
@@ -5,7 +5,9 @@ colorama = pytest.importorskip("pip._vendor.colorama")
 
 def test_color_always(script):
     """It uses colored output when passed --color=always."""
-    ret = script.pip("uninstall", "--color=always", "foo", allow_stderr_warning=True)
+    ret = script.pip(
+        "uninstall", "--color=always", "foo", allow_stderr_warning=True
+    )
     assert colorama.Fore.YELLOW in ret.stderr, "Expected color in output"
 
 
@@ -14,11 +16,15 @@ def test_color_auto(script):
 
     Don't expect color because ``script`` redirects output to a pipe.
     """
-    ret = script.pip("uninstall", "--color=auto", "foo", allow_stderr_warning=True)
+    ret = script.pip(
+        "uninstall", "--color=auto", "foo", allow_stderr_warning=True
+    )
     assert colorama.Fore.YELLOW not in ret.stderr, "Expected no color in output"
 
 
 def test_color_never(script):
     """It does not use colored output when passed --color=never."""
-    ret = script.pip("uninstall", "--color=never", "foo", allow_stderr_warning=True)
+    ret = script.pip(
+        "uninstall", "--color=never", "foo", allow_stderr_warning=True
+    )
     assert colorama.Fore.YELLOW not in ret.stderr, "Expected no color in output"
