0ce2c0ddb0434e854f7a59cf0b89ba898e92d8b6
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-04-25 12:54:13 +0200

    Add test cases for --color option

---
+++ tests/functional/test_color.py
@@ -0,0 +1,24 @@
+import pytest
+
+colorama = pytest.importorskip("pip._vendor.colorama")
+
+
+def test_color_always(script):
+    """It uses colored output when passed --color=always."""
+    ret = script.pip("uninstall", "--color=always", "foo", allow_stderr_warning=True)
+    assert colorama.Fore.YELLOW in ret.stderr, "Expected color in output"
+
+
+def test_color_auto(script):
+    """It does not use colored output when passed --color=auto.
+
+    Don't expect color because ``script`` redirects output to a pipe.
+    """
+    ret = script.pip("uninstall", "--color=auto", "foo", allow_stderr_warning=True)
+    assert colorama.Fore.YELLOW not in ret.stderr, "Expected no color in output"
+
+
+def test_color_never(script):
+    """It does not use colored output when passed --color=never."""
+    ret = script.pip("uninstall", "--color=never", "foo", allow_stderr_warning=True)
+    assert colorama.Fore.YELLOW not in ret.stderr, "Expected no color in output"
