d2f61ecf275f4a85aa69f1b754a369a73cdaf1b5
bmartinn <>
2020-05-10 00:03:38 +0300

    Add parallel (multi-process) install, if multiprocessing Pool is not available fall-back to serial installation

--- src/pip/_internal/req/__init__.py
+++ src/pip/_internal/req/__init__.py
@@ -3,10 +3,16 @@ import logging
 from typing import Iterator, List, Optional, Sequence, Tuple
 
 from pip._internal.utils.logging import indent_log
+from pip._internal.utils.typing import MYPY_CHECK_RUNNING
 
 from .req_file import parse_requirements
 from .req_install import InstallRequirement
 from .req_set import RequirementSet
+
+try:
+    from multiprocessing.pool import Pool
+except ImportError:  # Platform-specific: No multiprocessing available
+    Pool = None
 
 __all__ = [
     "RequirementSet", "InstallRequirement",
@@ -60,39 +66,66 @@ def install_given_reqs(
             ', '.join(to_install.keys()),
         )
 
-    installed = []
+    # pre allocate installed package names
+    installed = [None] * len(to_install)
+    install_args = [install_options, global_options, dict(
+    	root=root, home=home, prefix=prefix, warn_script_location=warn_script_location,
+    	use_user_site=use_user_site, pycompile=pycompile)]
+
+    if Pool is not None:
+        # first let's try to install in parallel, if we fail we do it by order.
+        pool = Pool()
+        try:
+            pool_result = pool.starmap_async(__single_install, [(install_args, r) for r in to_install])
+            # python 2.7 timeout=None will not catch KeyboardInterrupt
+            installed = pool_result.get(timeout=999999)
+        except (KeyboardInterrupt, SystemExit):
+            pool.terminate()
+            raise
+        except Exception:
+            # we will reinstall sequentially
+            pass
+        pool.close()
+        pool.join()
 
     with indent_log():
-        for req_name, requirement in to_install.items():
-            if requirement.should_reinstall:
-                logger.info('Attempting uninstall: %s', req_name)
-                with indent_log():
-                    uninstalled_pathset = requirement.uninstall(
-                        auto_confirm=True
-                    )
-            else:
-                uninstalled_pathset = None
+        for i, requirement in enumerate(to_install):
+            if installed[i] is None:
+                installed[i] = __single_install(install_args, requirement, allow_raise=True)
 
-            try:
-                requirement.install(
-                    install_options,
-                    global_options,
-                    root=root,
-                    home=home,
-                    prefix=prefix,
-                    warn_script_location=warn_script_location,
-                    use_user_site=use_user_site,
-                    pycompile=pycompile,
-                )
-            except Exception:
-                # if install did not succeed, rollback previous uninstall
-                if uninstalled_pathset and not requirement.install_succeeded:
-                    uninstalled_pathset.rollback()
-                raise
-            else:
-                if uninstalled_pathset and requirement.install_succeeded:
-                    uninstalled_pathset.commit()
+    return [i for i in installed if i is not None]
 
-            installed.append(InstallationResult(req_name))
 
-    return installed
+def __single_install(args, a_requirement, allow_raise=False):
+    if a_requirement.should_reinstall:
+        logger.info('Attempting uninstall: %s', a_requirement.name)
+        with indent_log():
+            uninstalled_pathset = a_requirement.uninstall(
+                auto_confirm=True
+            )
+    try:
+        a_requirement.install(
+            args[0],   # install_options,
+            args[1],   # global_options,
+            **args[2]  # **kwargs
+        )
+    except Exception:
+        should_rollback = (
+                a_requirement.should_reinstall and
+                not a_requirement.install_succeeded
+        )
+        # if install did not succeed, rollback previous uninstall
+        if should_rollback:
+            uninstalled_pathset.rollback()
+        if allow_raise:
+            raise
+    else:
+        should_commit = (
+                a_requirement.should_reinstall and
+                a_requirement.install_succeeded
+        )
+        if should_commit:
+            uninstalled_pathset.commit()
+        return InstallationResult(a_requirement.name)
+
+    return None
