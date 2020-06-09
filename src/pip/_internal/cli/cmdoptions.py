8f1d808deb6e6ce06942160c9eef36821da9d59c
Devesh Kumar Singh <deveshkusingh@gmail.com>
2020-06-10 00:39:50 +0530

    Add noqa B010 for setattr

--- src/pip/_internal/cli/cmdoptions.py
+++ src/pip/_internal/cli/cmdoptions.py
@@ -835,7 +835,7 @@ always_unzip = partial(
 # TODO: Move into a class that inherits from partial, currently does not
 # work as mypy complains functools.partial is a generic class.
 # This way we know we can ignore this option in docs auto generation
-setattr(always_unzip, 'deprecated', True)
+setattr(always_unzip, 'deprecated', True)  # noqa: B010
 
 
 def _handle_merge_hash(option, opt_str, value, parser):
