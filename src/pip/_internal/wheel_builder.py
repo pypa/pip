9a8bcf3f343fe692c540808b451113b147b4082a
Devesh Kumar Singh <deveshkusingh@gmail.com>
2020-06-25 23:27:50 +0530

    Add type: ignore to requirements loop

--- src/pip/_internal/wheel_builder.py
+++ src/pip/_internal/wheel_builder.py
@@ -274,7 +274,7 @@ def build(
     # Build the wheels.
     logger.info(
         'Building wheels for collected packages: %s',
-        ', '.join(req.name for req in requirements if req.name),
+        ', '.join(req.name for req in requirements),  # type: ignore
     )
 
     with indent_log():
@@ -297,12 +297,12 @@ def build(
     if build_successes:
         logger.info(
             'Successfully built %s',
-            ' '.join([req.name for req in build_successes if req.name]),
+            ' '.join([req.name for req in build_successes]),  # type: ignore
         )
     if build_failures:
         logger.info(
             'Failed to build %s',
-            ' '.join([req.name for req in build_failures if req.name]),
+            ' '.join([req.name for req in build_failures]),  # type: ignore
         )
     # Return a list of requirements that failed to build
     return build_successes, build_failures
