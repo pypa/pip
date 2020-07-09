6c090785648644fe9b92641f54d8525a0e5d49f7
Stéphane Bidoul <stephane.bidoul@gmail.com>
2020-07-09 12:43:15 +0200

    Patch distlib to accept colons in script names

--- src/pip/_vendor/distlib/util.py
+++ src/pip/_vendor/distlib/util.py
@@ -701,7 +701,7 @@ class ExportEntry(object):
     __hash__ = object.__hash__
 
 
-ENTRY_RE = re.compile(r'''(?P<name>(\w|[-.+])+)
+ENTRY_RE = re.compile(r'''(?P<name>(\w|[-.+:])+)
                       \s*=\s*(?P<callable>(\w+)([:\.]\w+)*)
                       \s*(\[\s*(?P<flags>[\w-]+(=\w+)?(,\s*\w+(=\w+)?)*)\s*\])?
                       ''', re.VERBOSE)
