7abbdcf7aef2b89f4cb155b30dcdd8f546db300c
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2020-09-23 22:35:52 +0530

    Blacken src/pip/_internal/models/selection_prefs.py

--- src/pip/_internal/models/selection_prefs.py
+++ src/pip/_internal/models/selection_prefs.py
@@ -12,8 +12,13 @@ class SelectionPreferences(object):
     and installing files.
     """
 
-    __slots__ = ['allow_yanked', 'allow_all_prereleases', 'format_control',
-                 'prefer_binary', 'ignore_requires_python']
+    __slots__ = [
+        "allow_yanked",
+        "allow_all_prereleases",
+        "format_control",
+        "prefer_binary",
+        "ignore_requires_python",
+    ]
 
     # Don't include an allow_yanked default value to make sure each call
     # site considers whether yanked releases are allowed. This also causes
@@ -23,8 +28,8 @@ class SelectionPreferences(object):
         self,
         allow_yanked,  # type: bool
         allow_all_prereleases=False,  # type: bool
-        format_control=None,          # type: Optional[FormatControl]
-        prefer_binary=False,          # type: bool
+        format_control=None,  # type: Optional[FormatControl]
+        prefer_binary=False,  # type: bool
         ignore_requires_python=None,  # type: Optional[bool]
     ):
         # type: (...) -> None
