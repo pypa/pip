01e0a3e1c2fb94d09f09ab21bb262163922b3fcf
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2020-09-23 22:36:06 +0530

    Blacken tests/data/

--- tests/data/src/TopoRequires2/setup.py
+++ tests/data/src/TopoRequires2/setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(
-    name='TopoRequires2',
-    version='0.0.1',
-    packages=['toporequires2'],
-    install_requires=['TopoRequires'],
+    name="TopoRequires2",
+    version="0.0.1",
+    packages=["toporequires2"],
+    install_requires=["TopoRequires"],
 )
