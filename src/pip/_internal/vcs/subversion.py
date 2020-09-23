38e03eecea71412122a18125c23e9fe2efa996af
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2020-09-23 22:36:05 +0530

    Blacken src/pip/_internal/vcs/subversion.py

--- src/pip/_internal/vcs/subversion.py
+++ src/pip/_internal/vcs/subversion.py
@@ -21,7 +21,7 @@ from pip._internal.vcs.versioncontrol import VersionControl, vcs
 _svn_xml_url_re = re.compile('url="([^"]+)"')
 _svn_rev_re = re.compile(r'committed-rev="(\d+)"')
 _svn_info_xml_rev_re = re.compile(r'\s*revision="(\d+)"')
-_svn_info_xml_url_re = re.compile(r'<url>(.*)</url>')
+_svn_info_xml_url_re = re.compile(r"<url>(.*)</url>")
 
 
 if MYPY_CHECK_RUNNING:
@@ -36,10 +36,10 @@ logger = logging.getLogger(__name__)
 
 
 class Subversion(VersionControl):
-    name = 'svn'
-    dirname = '.svn'
-    repo_name = 'checkout'
-    schemes = ('svn', 'svn+ssh', 'svn+http', 'svn+https', 'svn+svn')
+    name = "svn"
+    dirname = ".svn"
+    repo_name = "checkout"
+    schemes = ("svn", "svn+ssh", "svn+http", "svn+https", "svn+svn")
 
     @classmethod
     def should_add_vcs_url_prefix(cls, remote_url):
@@ -47,7 +47,7 @@ class Subversion(VersionControl):
 
     @staticmethod
     def get_base_rev_args(rev):
-        return ['-r', rev]
+        return ["-r", rev]
 
     @classmethod
     def get_revision(cls, location):
@@ -60,9 +60,9 @@ class Subversion(VersionControl):
         for base, dirs, _ in os.walk(location):
             if cls.dirname not in dirs:
                 dirs[:] = []
-                continue    # no sense walking uncontrolled subdirs
+                continue  # no sense walking uncontrolled subdirs
             dirs.remove(cls.dirname)
-            entries_fn = os.path.join(base, cls.dirname, 'entries')
+            entries_fn = os.path.join(base, cls.dirname, "entries")
             if not os.path.exists(entries_fn):
                 # FIXME: should we warn?
                 continue
@@ -70,10 +70,10 @@ class Subversion(VersionControl):
             dirurl, localrev = cls._get_svn_url_rev(base)
 
             if base == location:
-                base = dirurl + '/'   # save the root url
+                base = dirurl + "/"  # save the root url
             elif not dirurl or not dirurl.startswith(base):
                 dirs[:] = []
-                continue    # not part of the same svn tree, skip it
+                continue  # not part of the same svn tree, skip it
             revision = max(revision, localrev)
         return revision
 
@@ -83,7 +83,7 @@ class Subversion(VersionControl):
         This override allows the auth information to be passed to svn via the
         --username and --password options instead of via the URL.
         """
-        if scheme == 'ssh':
+        if scheme == "ssh":
             # The --username and --password options can't be used for
             # svn+ssh URLs, so keep the auth information in the URL.
             return super(Subversion, cls).get_netloc_and_auth(netloc, scheme)
@@ -95,8 +95,8 @@ class Subversion(VersionControl):
         # type: (str) -> Tuple[str, Optional[str], AuthInfo]
         # hotfix the URL scheme after removing svn+ from svn+ssh:// readd it
         url, rev, user_pass = super(Subversion, cls).get_url_rev_and_auth(url)
-        if url.startswith('ssh://'):
-            url = 'svn+' + url
+        if url.startswith("ssh://"):
+            url = "svn+" + url
         return url, rev, user_pass
 
     @staticmethod
@@ -104,9 +104,9 @@ class Subversion(VersionControl):
         # type: (Optional[str], Optional[HiddenText]) -> CommandArgs
         extra_args = []  # type: CommandArgs
         if username:
-            extra_args += ['--username', username]
+            extra_args += ["--username", username]
         if password:
-            extra_args += ['--password', password]
+            extra_args += ["--password", password]
 
         return extra_args
 
@@ -116,7 +116,7 @@ class Subversion(VersionControl):
         # setup.py we have to look up in the location until we find a real
         # setup.py
         orig_location = location
-        while not os.path.exists(os.path.join(location, 'setup.py')):
+        while not os.path.exists(os.path.join(location, "setup.py")):
             last_location = location
             location = os.path.dirname(location)
             if location == last_location:
@@ -135,26 +135,23 @@ class Subversion(VersionControl):
     def _get_svn_url_rev(cls, location):
         from pip._internal.exceptions import SubProcessError
 
-        entries_path = os.path.join(location, cls.dirname, 'entries')
+        entries_path = os.path.join(location, cls.dirname, "entries")
         if os.path.exists(entries_path):
             with open(entries_path) as f:
                 data = f.read()
         else:  # subversion >= 1.7 does not have the 'entries' file
-            data = ''
+            data = ""
 
-        if (data.startswith('8') or
-                data.startswith('9') or
-                data.startswith('10')):
-            data = list(map(str.splitlines, data.split('\n\x0c\n')))
+        if data.startswith("8") or data.startswith("9") or data.startswith("10"):
+            data = list(map(str.splitlines, data.split("\n\x0c\n")))
             del data[0][0]  # get rid of the '8'
             url = data[0][3]
             revs = [int(d[9]) for d in data if len(d) > 9 and d[9]] + [0]
-        elif data.startswith('<?xml'):
+        elif data.startswith("<?xml"):
             match = _svn_xml_url_re.search(data)
             if not match:
-                raise ValueError(
-                    'Badly formatted data: {data!r}'.format(**locals()))
-            url = match.group(1)    # get repository URL
+                raise ValueError("Badly formatted data: {data!r}".format(**locals()))
+            url = match.group(1)  # get repository URL
             revs = [int(m.group(1)) for m in _svn_rev_re.finditer(data)] + [0]
         else:
             try:
@@ -165,12 +162,10 @@ class Subversion(VersionControl):
                 # is being used to prompt for passwords, because passwords
                 # are only potentially needed for remote server requests.
                 xml = cls.run_command(
-                    ['info', '--xml', location],
+                    ["info", "--xml", location],
                 )
                 url = _svn_info_xml_url_re.search(xml).group(1)
-                revs = [
-                    int(m.group(1)) for m in _svn_info_xml_rev_re.finditer(xml)
-                ]
+                revs = [int(m.group(1)) for m in _svn_info_xml_rev_re.finditer(xml)]
             except SubProcessError:
                 url, revs = None, []
 
@@ -216,14 +211,14 @@ class Subversion(VersionControl):
         #      compiled Mar 28 2018, 08:49:13 on x86_64-pc-linux-gnu
         #   svn, version 1.12.0-SlikSvn (SlikSvn/1.12.0)
         #      compiled May 28 2019, 13:44:56 on x86_64-microsoft-windows6.2
-        version_prefix = 'svn, version '
-        version = self.run_command(['--version'])
+        version_prefix = "svn, version "
+        version = self.run_command(["--version"])
 
         if not version.startswith(version_prefix):
             return ()
 
-        version = version[len(version_prefix):].split()[0]
-        version_list = version.partition('-')[0].split('.')
+        version = version[len(version_prefix) :].split()[0]
+        version_list = version.partition("-")[0].split(".")
         try:
             parsed_version = tuple(map(int, version_list))
         except ValueError:
@@ -269,7 +264,7 @@ class Subversion(VersionControl):
         if not self.use_interactive:
             # --non-interactive switch is available since Subversion 0.14.4.
             # Subversion < 1.8 runs in interactive mode by default.
-            return ['--non-interactive']
+            return ["--non-interactive"]
 
         svn_version = self.get_vcs_version()
         # By default, Subversion >= 1.8 runs in non-interactive mode if
@@ -281,7 +276,7 @@ class Subversion(VersionControl):
         # SVN 1.7, pip should continue to support SVN 1.7. Therefore, pip
         # can't safely add the option if the SVN version is < 1.8 (or unknown).
         if svn_version >= (1, 8):
-            return ['--force-interactive']
+            return ["--force-interactive"]
 
         return []
 
@@ -290,15 +285,18 @@ class Subversion(VersionControl):
         """Export the svn repository at the url to the destination location"""
         url, rev_options = self.get_url_rev_options(url)
 
-        logger.info('Exporting svn repository %s to %s', url, location)
+        logger.info("Exporting svn repository %s to %s", url, location)
         with indent_log():
             if os.path.exists(location):
                 # Subversion doesn't like to check out over an existing
                 # directory --force fixes this, but was only added in svn 1.5
                 rmtree(location)
             cmd_args = make_command(
-                'export', self.get_remote_call_options(),
-                rev_options.to_args(), url, location,
+                "export",
+                self.get_remote_call_options(),
+                rev_options.to_args(),
+                url,
+                location,
             )
             self.run_command(cmd_args)
 
@@ -306,29 +304,38 @@ class Subversion(VersionControl):
         # type: (str, HiddenText, RevOptions) -> None
         rev_display = rev_options.to_display()
         logger.info(
-            'Checking out %s%s to %s',
+            "Checking out %s%s to %s",
             url,
             rev_display,
             display_path(dest),
         )
         cmd_args = make_command(
-            'checkout', '-q', self.get_remote_call_options(),
-            rev_options.to_args(), url, dest,
+            "checkout",
+            "-q",
+            self.get_remote_call_options(),
+            rev_options.to_args(),
+            url,
+            dest,
         )
         self.run_command(cmd_args)
 
     def switch(self, dest, url, rev_options):
         # type: (str, HiddenText, RevOptions) -> None
         cmd_args = make_command(
-            'switch', self.get_remote_call_options(), rev_options.to_args(),
-            url, dest,
+            "switch",
+            self.get_remote_call_options(),
+            rev_options.to_args(),
+            url,
+            dest,
         )
         self.run_command(cmd_args)
 
     def update(self, dest, url, rev_options):
         # type: (str, HiddenText, RevOptions) -> None
         cmd_args = make_command(
-            'update', self.get_remote_call_options(), rev_options.to_args(),
+            "update",
+            self.get_remote_call_options(),
+            rev_options.to_args(),
             dest,
         )
         self.run_command(cmd_args)
