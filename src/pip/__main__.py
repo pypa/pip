e59ba23468390217479465019f8d78e724a23550
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2020-09-23 22:35:44 +0530

    Blacken src/pip/__main__.py

--- src/pip/__main__.py
+++ src/pip/__main__.py
@@ -7,12 +7,12 @@ import sys
 # of sys.path, if present to avoid using current directory
 # in pip commands check, freeze, install, list and show,
 # when invoked as python -m pip <command>
-if sys.path[0] in ('', os.getcwd()):
+if sys.path[0] in ("", os.getcwd()):
     sys.path.pop(0)
 
 # If we are running from a wheel, add the wheel to sys.path
 # This allows the usage python pip-*.whl/pip install pip-*.whl
-if __package__ == '':
+if __package__ == "":
     # __file__ is pip-*.whl/pip/__main__.py
     # first dirname call strips of '/__main__.py', second strips off '/pip'
     # Resulting path is the name of the wheel itself
@@ -22,5 +22,5 @@ if __package__ == '':
 
 from pip._internal.cli.main import main as _main  # isort:skip # noqa
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(_main())
