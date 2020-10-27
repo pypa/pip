ddb7ba24da7074456d14d5c150891460b44f6b93
Tzu-ping Chung <uranusjr@gmail.com>
2020-10-27 18:32:12 +0800

    Add failing test case for specifier

--- tests/functional/test_new_resolver_hashes.py
+++ tests/functional/test_new_resolver_hashes.py
@@ -4,34 +4,41 @@ import hashlib
 import pytest
 
 from pip._internal.utils.urls import path_to_url
-from tests.lib import create_basic_sdist_for_package, create_basic_wheel_for_package
+from tests.lib import (
+    create_basic_sdist_for_package,
+    create_basic_wheel_for_package,
+)
+
+_AddedLink = collections.namedtuple(
+    "_AddedLink", "url hash stem"
+)
 
 _FindLinks = collections.namedtuple(
     "_FindLinks", "index_html sdist_hash wheel_hash",
 )
 
 
+def _add_link_to_find(html_path, archive_path):
+    archive_url = path_to_url(archive_path)
+    archive_hash = hashlib.sha256(archive_path.read_bytes()).hexdigest()
+    line = "<a href='{url}#sha256={hash}'>{stem}</a>\n".format(
+        url=archive_url,
+        hash=archive_hash,
+        stem=archive_path.stem,
+    )
+    with open(html_path, "a") as f:
+        f.write(line)
+
+    return archive_hash
+
+
 def _create_find_links(script):
     sdist_path = create_basic_sdist_for_package(script, "base", "0.1.0")
     wheel_path = create_basic_wheel_for_package(script, "base", "0.1.0")
 
-    sdist_hash = hashlib.sha256(sdist_path.read_bytes()).hexdigest()
-    wheel_hash = hashlib.sha256(wheel_path.read_bytes()).hexdigest()
-
     index_html = script.scratch_path / "index.html"
-    index_html.write_text(
-        """
-        <a href="{sdist_url}#sha256={sdist_hash}">{sdist_path.stem}</a>
-        <a href="{wheel_url}#sha256={wheel_hash}">{wheel_path.stem}</a>
-        """.format(
-            sdist_url=path_to_url(sdist_path),
-            sdist_hash=sdist_hash,
-            sdist_path=sdist_path,
-            wheel_url=path_to_url(wheel_path),
-            wheel_hash=wheel_hash,
-            wheel_path=wheel_path,
-        )
-    )
+    sdist_hash = _add_link_to_find(index_html, sdist_path)
+    wheel_hash = _add_link_to_find(index_html, wheel_path)
 
     return _FindLinks(index_html, sdist_hash, wheel_hash)
 
@@ -208,3 +215,38 @@ def test_new_resolver_hash_intersect_empty_from_constraint(script):
         "from some requirements."
     )
     assert message in result.stderr, str(result)
+
+
+def test_new_resolver_use_specifier_from_constraints(script):
+    find_links = _create_find_links(script)
+
+    child_wheel = create_basic_wheel_for_package(
+        script,
+        "child",
+        "1.0",
+        depends=["base"],
+    )
+    child_hash = _add_link_to_find(find_links.index_html, child_wheel)
+
+    constraints_txt = script.scratch_path / "constraints.txt"
+    constraints_txt.write_text(
+        "child==1.0 --hash=sha256:{child_hash}".format(child_hash=child_hash),
+    )
+
+    requirements_txt = script.scratch_path / "requirements.txt"
+    requirements_txt.write_text(
+        """
+        -c {constraints_txt}
+        base==0.1.0 --hash=sha256:{h}
+        """.format(constraints_txt=constraints_txt, h=find_links.wheel_hash),
+    )
+
+    script.pip(
+        "install",
+        "--use-feature=2020-resolver",
+        "--no-cache-dir",
+        "--no-deps",
+        "--no-index",
+        "--find-links", find_links.index_html,
+        "--requirement", requirements_txt,
+    )
