1a2795e42f2df4c3151d4b25505bb5f3920cb02e
Tzu-ping Chung <uranusjr@gmail.com>
2020-11-04 21:18:51 +0800

    fork sync.

--- noxfile.py
+++ noxfile.py
@@ -170,6 +170,9 @@ def lint(session: nox.Session) -> None:
         args = ["--all-files", "--show-diff-on-failure"]
 
     session.run("pre-commit", "run", *args)
+    session.run(
+        "pre-commit", "run", "-c", ".pre-commit-config-slow.yaml", *args
+    )
 
 
 @nox.session
