09e8fa96c7bf821916e9a21135359d1507fa5b68
Jon Dufresne <jon.dufresne@gmail.com>
2020-12-14 14:24:42 -0800

    Distribute and install py.typed to provide type information
    Complies with PEP 561:
    https://www.python.org/dev/peps/pep-0561/#packaging-type-information
    
    By distributing and installing the py.typed file, mypy will use pip's
    type information when imported into other projects. For example, the
    pip-tools project can use pip's types and mypy to help verify
    correctness.
    
    mypy docs:
    https://mypy.readthedocs.io/en/stable/installed_packages.html#making-pep-561-compatible-packages

--- setup.py
+++ setup.py
@@ -68,6 +68,7 @@ setup(
         exclude=["contrib", "docs", "tests*", "tasks"],
     ),
     package_data={
+        "pip": ["py.typed"],
         "pip._vendor": ["vendor.txt"],
         "pip._vendor.certifi": ["*.pem"],
         "pip._vendor.requests": ["*.pem"],
