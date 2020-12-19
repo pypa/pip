872fd658c4d8f4676d491d7986ba1a78a201f701
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2020-12-19 16:09:55 +0000

    Add `Python 3 :: Only` classifier

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(
         "Topic :: Software Development :: Build Tools",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only"
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
