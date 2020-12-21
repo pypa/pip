49d34ceb63fa9f201acbfd19eda6e5998f5500b1
Stéphane Bidoul <stephane.bidoul@gmail.com>
2020-12-21 15:07:48 +0100

    Revert "Bubble up SubProcessError to basecommand._main"
    This reverts commit e9f738a3daec91b131ae985e16809d47b1cfdaff.

--- src/pip/_internal/cli/base_command.py
+++ src/pip/_internal/cli/base_command.py
@@ -27,7 +27,6 @@ from pip._internal.exceptions import (
     InstallationError,
     NetworkConnectionError,
     PreviousBuildDirError,
-    SubProcessError,
     UninstallationError,
 )
 from pip._internal.utils.deprecation import deprecated
@@ -230,7 +229,7 @@ class Command(CommandContextMixIn):
 
             return PREVIOUS_BUILD_DIR_ERROR
         except (InstallationError, UninstallationError, BadCommand,
-                SubProcessError, NetworkConnectionError) as exc:
+                NetworkConnectionError) as exc:
             logger.critical(str(exc))
             logger.debug('Exception information:', exc_info=True)
 
