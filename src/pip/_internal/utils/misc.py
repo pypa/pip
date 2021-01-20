6739f56351a88add7a9e09e4eec25f691f79ec79
Paul Moore <p.f.moore@gmail.com>
2021-01-20 15:42:29 +0000

    Use our own copy of strtobool, rather than the one from distutils

--- src/pip/_internal/utils/misc.py
+++ src/pip/_internal/utils/misc.py
@@ -244,6 +244,23 @@ def ask_password(message):
     return getpass.getpass(message)
 
 
+def strtobool(val):
+    # type: (str) -> int
+    """Convert a string representation of truth to true (1) or false (0).
+
+    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
+    are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
+    'val' is anything else.
+    """
+    val = val.lower()
+    if val in ('y', 'yes', 't', 'true', 'on', '1'):
+        return 1
+    elif val in ('n', 'no', 'f', 'false', 'off', '0'):
+        return 0
+    else:
+        raise ValueError("invalid truth value %r" % (val,))
+
+
 def format_size(bytes):
     # type: (float) -> str
     if bytes > 1000 * 1000:
