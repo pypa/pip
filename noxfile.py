fe3aec0f5e4a15c4110eb1974497371f26c4fec3
Stéphane Bidoul <stephane.bidoul@gmail.com>
2021-01-24 15:39:51 +0100

    Better way to run slow linters in CI only
    Taking advantage of pre-commit's manual stage.

--- noxfile.py
+++ noxfile.py
@@ -147,11 +147,9 @@ def lint(session):
         args = session.posargs + ["--all-files"]
     else:
         args = ["--all-files", "--show-diff-on-failure"]
+    args.append("--hook-stage=manual")
 
     session.run("pre-commit", "run", *args)
-    session.run(
-        "pre-commit", "run", "-c", ".pre-commit-config-slow.yaml", *args
-    )
 
 
 @nox.session
