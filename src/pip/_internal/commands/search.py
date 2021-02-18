6045ade9a919bb6ab6b12eadc5bb8254ecbc00ba
Andrey Bienkowski <hexagonrecursion@gmail.com>
2021-02-18 11:32:41 +0300

    Apply review suggestions

--- src/pip/_internal/commands/search.py
+++ src/pip/_internal/commands/search.py
@@ -140,7 +140,8 @@ def print_results(hits, name_column_width=None, terminal_width=None):
                 summary = ('\n' + ' ' * (name_column_width + 3)).join(
                     summary_lines)
 
-        line = f'{{{name} ({latest}):{name_column_width}}} - {summary}'
+        name_latest=f'{name} ({latest})'
+        line = f'{name_latest:{name_column_width}} - {summary}'
         try:
             write_output(line)
             dist = env.get_distribution(name)
