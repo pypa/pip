7a95720e796a5e56481c1cc20b6ce6249c50f357
KOLANICH <kolan_n@mail.ru>
2021-02-24 11:50:16 +0300

    Fixed --editable install for setuptools projects without setup.py.
    Co-Authored-By: Tzu-ping Chung <uranusjr@gmail.com>
    Co-Authored-By: Pradyun Gedam <3275593+pradyunsg@users.noreply.github.com>

--- src/pip/_internal/utils/setuptools_build.py
+++ src/pip/_internal/utils/setuptools_build.py
@@ -8,9 +8,11 @@ from typing import List, Optional, Sequence
 # invoking via the shim.  This avoids e.g. the following manifest_maker
 # warning: "warning: manifest_maker: standard file '-c' not found".
 _SETUPTOOLS_SHIM = (
-    "import sys, setuptools, tokenize; sys.argv[0] = {0!r}; __file__={0!r};"
-    "f=getattr(tokenize, 'open', open)(__file__);"
-    "code=f.read().replace('\\r\\n', '\\n');"
+    "import io, os, sys, setuptools, tokenize; sys.argv[0] = {0!r}; __file__={0!r};"
+    "f = getattr(tokenize, 'open', open)(__file__) "
+    "if os.path.exists(__file__) "
+    "else io.StringIO('from setuptools import setup; setup()');"
+    "code = f.read().replace('\\r\\n', '\\n');"
     "f.close();"
     "exec(compile(code, __file__, 'exec'))"
 )
