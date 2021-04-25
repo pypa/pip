15012723079664f61f3b13674a664af1f6cdb1e3
Tzu-ping Chung <uranusjr@gmail.com>
2021-04-25 16:24:42 +0800

    Use osx_framework_user for Mac framework build

--- src/pip/_internal/locations/_sysconfig.py
+++ src/pip/_internal/locations/_sysconfig.py
@@ -52,7 +52,10 @@ def _infer_prefix() -> str:
 
 def _infer_user() -> str:
     """Try to find a user scheme for the current platform."""
-    suffixed = f"{os.name}_user"
+    if sysconfig.get_config_var("PYTHONFRAMEWORK"):  # Mac framework build.
+        suffixed = "osx_framework_user"
+    else:
+        suffixed = f"{os.name}_user"
     if suffixed in _AVAILABLE_SCHEMES:
         return suffixed
     if "posix_user" not in _AVAILABLE_SCHEMES:  # User scheme unavailable.
