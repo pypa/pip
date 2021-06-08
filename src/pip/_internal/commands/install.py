5c735941532a224b9cac4c8d3d36b7f3c367ab7f
OBITORASU <suprememightyzeus@gmail.com>
2021-06-08 21:40:45 +0530

    change message to error

--- src/pip/_internal/commands/install.py
+++ src/pip/_internal/commands/install.py
@@ -441,7 +441,7 @@ class InstallCommand(RequirementCommand):
             message = create_os_error_message(
                 error, show_traceback, options.use_user_site,
             )
-            if WINDOWS and len(message) >= 280:
+            if WINDOWS and len(error) >= 280:
                 logger.warning(
                     'The following error can potentially be caused '
                     'because Long Paths is disabled on your system. '
