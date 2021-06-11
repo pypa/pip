25ffadf98063238245f7c9f6b9e8a03b3b1c72a6
Noah Gorny <noah@gittabags.com>
2021-06-11 04:23:47 +0300

    tests: unit: Fix test_command.py after new command addition

--- tests/unit/test_commands.py
+++ tests/unit/test_commands.py
@@ -11,7 +11,8 @@ from pip._internal.commands import commands_dict, create_command
 
 # These are the expected names of the commands whose classes inherit from
 # IndexGroupCommand.
-EXPECTED_INDEX_GROUP_COMMANDS = ['download', 'install', 'list', 'wheel']
+EXPECTED_INDEX_GROUP_COMMANDS = [
+    'download', 'index', 'install', 'list', 'wheel']
 
 
 def check_commands(pred, expected):
@@ -49,7 +50,9 @@ def test_session_commands():
     def is_session_command(command):
         return isinstance(command, SessionCommandMixin)
 
-    expected = ['download', 'install', 'list', 'search', 'uninstall', 'wheel']
+    expected = [
+        'download', 'index', 'install', 'list', 'search', 'uninstall', 'wheel'
+    ]
     check_commands(is_session_command, expected)
 
 
