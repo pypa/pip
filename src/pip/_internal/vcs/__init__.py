4b1e5d1d5521505e2ea3e7bd0a484d6507c3d150
Pradyun Gedam <pradyunsg@gmail.com>
2021-07-02 11:10:19 +0000

    Reorder vcs imports, to try more popular VCS systems sooner
    This should help shave off a few 100ms in `pip freeze` runs when
    the user has a VCS editable install, in the common cases.

--- src/pip/_internal/vcs/__init__.py
+++ src/pip/_internal/vcs/__init__.py
@@ -1,11 +1,17 @@
+# Import all vcs modules to register each VCS in the VcsSupport object.
+#
+# These imports are ordered based on rough "popularity" as judged by
+# @pradyunsg in July 2021. This ordering determines the order that
+# directories are checked for "is this in a vcs backend" and thus
+# ordering by priority here makes things a few ms faster.
+import pip._internal.vcs.git  # isort: skip
+import pip._internal.vcs.subversion  # isort: skip
+import pip._internal.vcs.mercurial  # isort: skip
+import pip._internal.vcs.bazaar  # isort: skip  # noqa: F401
+
 # Expose a limited set of classes and functions so callers outside of
 # the vcs package don't need to import deeper than `pip._internal.vcs`.
 # (The test directory may still need to import from a vcs sub-package.)
-# Import all vcs modules to register each VCS in the VcsSupport object.
-import pip._internal.vcs.bazaar
-import pip._internal.vcs.git
-import pip._internal.vcs.mercurial
-import pip._internal.vcs.subversion  # noqa: F401
 from pip._internal.vcs.versioncontrol import (  # noqa: F401
     RemoteNotFoundError,
     RemoteNotValidError,
