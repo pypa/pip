f6b184ccaf464021e7633d15fa719dd35e164fdd
Tzu-ping Chung <uranusjr@gmail.com>
2021-07-22 15:28:43 +0800

    Strip slash before Windows drive letter in path (#10116)
    Functions like urllib.parse.urlsplit() parses a file:// URL created from
    a non-UNC Windows absolute path with a leading slash in the path
    component:
    
    >>> from pathlib import Path
    >>> from urllib.parse import urlsplit
    >>> path = Path("C:/Users/VssAdministrator")
    >>> parsed = urlsplit(path.as_posix())
    >>> parsed.path
    '/C:/Users/VssAdministrator'
    
    This value unfortunately does not play well with path functions like
    open(), so we performs some additional cleanup to strip that leading
    slash.
    
    This commit also contains some minor cleanup to unify how Windows is
    detected, and how a file:// URL is fetched.

--- src/pip/_internal/utils/urls.py
+++ src/pip/_internal/utils/urls.py
@@ -1,8 +1,10 @@
 import os
-import sys
+import string
 import urllib.parse
 import urllib.request
 from typing import Optional
+
+from .compat import WINDOWS
 
 
 def get_url_scheme(url):
@@ -37,7 +39,7 @@ def url_to_path(url):
     if not netloc or netloc == "localhost":
         # According to RFC 8089, same as empty authority.
         netloc = ""
-    elif sys.platform == "win32":
+    elif WINDOWS:
         # If we have a UNC path, prepend UNC share notation.
         netloc = "\\\\" + netloc
     else:
@@ -46,4 +48,18 @@ def url_to_path(url):
         )
 
     path = urllib.request.url2pathname(netloc + path)
+
+    # On Windows, urlsplit parses the path as something like "/C:/Users/foo".
+    # This creates issues for path-related functions like io.open(), so we try
+    # to detect and strip the leading slash.
+    if (
+        WINDOWS
+        and not netloc  # Not UNC.
+        and len(path) >= 3
+        and path[0] == "/"  # Leading slash to strip.
+        and path[1] in string.ascii_letters  # Drive letter.
+        and path[2:4] in (":", ":/")  # Colon + end of string, or colon + absolute path.
+    ):
+        path = path[1:]
+
     return path
