22f7153f5ba55834944fc509bf0681ac9e9428e3
Tzu-ping Chung <uranusjr@gmail.com>
2021-07-23 11:05:58 +0800

    Fix linter errors

--- src/pip/_internal/commands/uninstall.py
+++ src/pip/_internal/commands/uninstall.py
@@ -5,7 +5,7 @@ from pip._vendor.packaging.utils import canonicalize_name
 
 from pip._internal.cli.base_command import Command
 from pip._internal.cli.req_command import SessionCommandMixin, warn_if_run_as_root
-from pip._internal.cli.status_codes import SUCCESS, ERROR
+from pip._internal.cli.status_codes import ERROR, SUCCESS
 from pip._internal.exceptions import InstallationError
 from pip._internal.req import parse_requirements
 from pip._internal.req.constructors import (
