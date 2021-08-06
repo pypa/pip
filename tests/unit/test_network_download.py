2aa4a9c95d729ed07f7bb2c0d2632cd7511b2b7e
Andrew Meleka <andrew.albert@live.com>
2021-08-06 13:18:51 +0200

    Modify the log line and fix tests

--- tests/unit/test_network_download.py
+++ tests/unit/test_network_download.py
@@ -14,17 +14,17 @@ from tests.lib.requests_mocks import MockResponse
 
 @pytest.mark.parametrize("url, headers, from_cache, expected", [
     ('http://example.com/foo.tgz', {}, False,
-        "Downloading http://example.com/foo.tgz"),
+        "Downloading foo.tgz"),
     ('http://example.com/foo.tgz', {'content-length': 2}, False,
-        "Downloading http://example.com/foo.tgz (2 bytes)"),
+        "Downloading foo.tgz (2 bytes)"),
     ('http://example.com/foo.tgz', {'content-length': 2}, True,
-        "Using cached http://example.com/foo.tgz (2 bytes)"),
+        "Using cached foo.tgz (2 bytes)"),
     ('https://files.pythonhosted.org/foo.tgz', {}, False,
         "Downloading foo.tgz"),
     ('https://files.pythonhosted.org/foo.tgz', {'content-length': 2}, False,
         "Downloading foo.tgz (2 bytes)"),
     ('https://files.pythonhosted.org/foo.tgz', {'content-length': 2}, True,
-        "Using cached foo.tgz"),
+        "Using cached foo.tgz (2 bytes)"),
 ])
 def test_prepare_download__log(caplog, url, headers, from_cache, expected):
     caplog.set_level(logging.INFO)
