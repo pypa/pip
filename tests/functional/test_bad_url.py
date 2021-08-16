1a0fb4239c0ec9f11fe20697482f6783b3d88250
Diego Ramirez <dr01191115@gmail.com>
2021-08-16 09:11:38 -0500

    Create test_bad_url.py
    A test for this new error message.

---
+++ tests/functional/test_bad_url.py
@@ -0,0 +1,28 @@
+import subprocess
+import random
+from typing import List, Tuple
+
+def get_url_error_message(cmd: List[str]) -> Tuple[str, str]:
+    # this makes pip to react using
+    # subprocess. It must fail, so then
+    # we can test the error message.
+    proc = subprocess.run(cmd, capture_output=True, text=True)
+    expected_message = "ERROR: 404 Client Error: FileNotFoundError for url: "
+    return proc.stderr, expected_message
+
+def get_random_pathname() -> str:
+    "create a random, impossible pathname."
+    base = "random_impossible_pathname_"
+    alphabet = "abcdefghijklmnopqrstuvwxyz0123456789"
+    name = base + "".join(random.choice(alphabet)) for _ in range(10)
+    return name
+
+def test_bad_url_error_message() -> None:
+    """
+    Test the error message returned when using a bad "file:" URL.
+    """
+    file = get_random_pathname()
+    command = ["pip", "install", "file:%s"%file]
+    msg, expected = get_url_error_message(command)
+    # assert that "msg" starts with "expected"
+    assert msg.startswith(expected)
