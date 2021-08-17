b7bce7c4d31e4742f03c605b0bd6ef5730cc1d90
Diego Ramirez <dr01191115@gmail.com>
2021-08-17 09:01:50 -0500

    Update test_bad_url.py
    Modify the way to test the requirements file, to avoid an AssertionError.

--- tests/functional/test_bad_url.py
+++ tests/functional/test_bad_url.py
@@ -8,7 +8,7 @@ def get_url_error_message(script: Any, fake_file: str) -> Tuple[str, str, int]:
     # this makes pip to react using
     # subprocess. It must fail, so then
     # we can test the error message.
-    proc = script.pip("install", "-r", fake_file, expect_error=True)
+    proc = script.pip("install", "-r", f"file:{fake_file}", expect_error=True)
     expected_message = "ERROR: 404 Client Error: FileNotFoundError for url: "
     return proc.stderr, expected_message, proc.returncode
 
@@ -16,7 +16,7 @@ def get_url_error_message(script: Any, fake_file: str) -> Tuple[str, str, int]:
 def test_filenotfound_error_message(script: Any) -> None:
     # Test the error message returned when using a bad 'file:' URL.
     # make pip to fail and get an error message
-    # by running "pip install -r nonexistent_file"
+    # by running "pip install -r file:nonexistent_file"
     msg, expected, code = get_url_error_message(script, "nonexistent_file")
     # assert that "msg" starts with "expected"
     assert code == 1
