9faf241097abfaac600de1a70c600e94d4472299
Diego Ramirez <dr01191115@gmail.com>
2021-08-17 09:31:58 -0500

    Update test_bad_url.py
    Squash all the test into a single function.

--- tests/functional/test_bad_url.py
+++ tests/functional/test_bad_url.py
@@ -4,20 +4,13 @@
 from typing import Any, Tuple
 
 
-def get_url_error_message(script: Any, fake_file: str) -> Tuple[str, str, int]:
-    # this makes pip to react using
-    # subprocess. It must fail, so then
-    # we can test the error message.
-    proc = script.pip("install", "-r", f"file:{fake_file}", expect_error=True)
-    expected_message = "ERROR: 404 Client Error: FileNotFoundError for url: "
-    return proc.stderr, expected_message, proc.returncode
-
-
 def test_filenotfound_error_message(script: Any) -> None:
     # Test the error message returned when using a bad 'file:' URL.
     # make pip to fail and get an error message
     # by running "pip install -r file:nonexistent_file"
-    msg, expected, code = get_url_error_message(script, "nonexistent_file")
+    proc = script.pip("install", "-r", "file:unexistent_file", expect_error=True)
+    expected = "ERROR: 404 Client Error: FileNotFoundError for url: "
+    msg, code = proc.stderr, proc.returncode
     # assert that "msg" starts with "expected"
     assert code == 1
     assert msg.startswith(expected)
