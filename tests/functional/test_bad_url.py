6f0369c8d67698317cbe0176e97541907c903464
Diego Ramirez <dr01191115@gmail.com>
2021-08-19 07:55:07 -0500

    Update test_bad_url.py
    Add some indentations to the string formatting.

--- tests/functional/test_bad_url.py
+++ tests/functional/test_bad_url.py
@@ -11,7 +11,7 @@ def test_filenotfound_error_message(script: Any) -> None:
     proc = script.pip("install", "-r", "file:unexistent_file", expect_error=True)
     assert proc.returncode == 1
     expect = (
-    "ERROR: 404 Client Error: FileNotFoundError for url: "
-    "file:///unexistent_file"
+        "ERROR: 404 Client Error: FileNotFoundError for url: "
+        "file:///unexistent_file"
     )
     assert proc.stderr.rstrip() == expect
