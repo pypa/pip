f1f44c9995fdb83974692fd29df722809f2b97da
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-08-22 11:11:41 +0530

    comment about why the if `var` is `None` check is needed.

--- tests/lib/__init__.py
+++ tests/lib/__init__.py
@@ -1082,6 +1082,9 @@ def create_basic_wheel_for_package(
     requires_python=None,
     extra_files=None,
 ):
+
+    # these can't be made default args because some tests/functions pass None as an arg.
+    # for e.g create_basic_wheel_for_package("scriptname", "name", "v1.0.0", None)
     if depends is None:
         depends = []
     if extras is None:
