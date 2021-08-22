0ab2bbdaec589f51062e47dec50499b1e94d4005
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-08-22 12:47:00 +0530

    imported Dict type annotation

--- tests/lib/__init__.py
+++ tests/lib/__init__.py
@@ -11,7 +11,7 @@ from contextlib import contextmanager
 from hashlib import sha256
 from io import BytesIO
 from textwrap import dedent
-from typing import List, Optional
+from typing import List, Optional, Dict
 from zipfile import ZipFile
 
 import pytest
