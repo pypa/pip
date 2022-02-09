9dd53c12a6802ede42a37b4da06566b3f497bb2d
q0w <43147888+q0w@users.noreply.github.com>
2022-02-09 20:02:44 +0300

    Support PEP600

--- src/pip/_internal/utils/compatibility_tags.py
+++ src/pip/_internal/utils/compatibility_tags.py
@@ -7,6 +7,7 @@ from typing import List, Optional, Tuple
 from pip._vendor.packaging.tags import (
     PythonVersion,
     Tag,
+    _linux_platforms,
     compatible_tags,
     cpython_tags,
     generic_tags,
@@ -43,33 +44,12 @@ def _mac_platforms(arch: str) -> List[str]:
     return arches
 
 
-def _custom_manylinux_platforms(arch: str) -> List[str]:
-    arches = [arch]
-    arch_prefix, arch_sep, arch_suffix = arch.partition("_")
-    if arch_prefix == "manylinux2014":
-        # manylinux1/manylinux2010 wheels run on most manylinux2014 systems
-        # with the exception of wheels depending on ncurses. PEP 599 states
-        # manylinux1/manylinux2010 wheels should be considered
-        # manylinux2014 wheels:
-        # https://www.python.org/dev/peps/pep-0599/#backwards-compatibility-with-manylinux2010-wheels
-        if arch_suffix in {"i686", "x86_64"}:
-            arches.append("manylinux2010" + arch_sep + arch_suffix)
-            arches.append("manylinux1" + arch_sep + arch_suffix)
-    elif arch_prefix == "manylinux2010":
-        # manylinux1 wheels run on most manylinux2010 systems with the
-        # exception of wheels depending on ncurses. PEP 571 states
-        # manylinux1 wheels should be considered manylinux2010 wheels:
-        # https://www.python.org/dev/peps/pep-0571/#backwards-compatibility-with-manylinux1-wheels
-        arches.append("manylinux1" + arch_sep + arch_suffix)
-    return arches
-
-
 def _get_custom_platforms(arch: str) -> List[str]:
     arch_prefix, arch_sep, arch_suffix = arch.partition("_")
     if arch.startswith("macosx"):
         arches = _mac_platforms(arch)
-    elif arch_prefix in ["manylinux2014", "manylinux2010"]:
-        arches = _custom_manylinux_platforms(arch)
+    elif "linux" in arch_prefix:
+        arches = list(_linux_platforms())
     else:
         arches = [arch]
     return arches
