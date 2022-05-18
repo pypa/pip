e91171bfe4bd7a7e549af93c2aa23c78201b3ab6
Jonathan Helmus <jhelmus@twitter.com>
2022-05-18 17:52:57 -0500

    add environment markers option to resolve command

--- src/pip/_internal/commands/resolve.py
+++ src/pip/_internal/commands/resolve.py
@@ -94,6 +94,8 @@ class ResolveCommand(RequirementCommand):
             py_version_info=options.python_version,
         )
 
+        self._patch_environment_markers(options.environment_markers)
+
         self.trace_basic_info(finder)
 
         requirement_set = resolver.resolve(reqs, check_supported_wheels=True)
