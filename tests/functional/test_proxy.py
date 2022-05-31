759c4770ca3a842c56c05c5f559c3e66c2f99a39
q0w <43147888+q0w@users.noreply.github.com>
2022-05-31 11:32:44 +0300

    Test against netrc env

--- tests/functional/test_proxy.py
+++ tests/functional/test_proxy.py
@@ -1,11 +1,19 @@
+import ssl
+from pathlib import Path
 from typing import Any, Dict, Optional
 
 import proxy
 import pytest
 from proxy.http.proxy import HttpProxyBasePlugin
 
-from tests.lib import PipTestEnvironment
-from tests.lib.path import Path
+from tests.conftest import CertFactory
+from tests.lib import PipTestEnvironment, TestData
+from tests.lib.server import (
+    authorization_response,
+    make_mock_server,
+    package_page,
+    server_running,
+)
 
 
 class AccessLogPlugin(HttpProxyBasePlugin):
@@ -15,14 +23,12 @@ class AccessLogPlugin(HttpProxyBasePlugin):
 
 
 @pytest.mark.network
-def test_proxy_overrides_env(
-    script: PipTestEnvironment, monkeypatch: pytest.MonkeyPatch
-) -> None:
-    monkeypatch.setenv("http_proxy", "127:0.0.1:8888")
-    monkeypatch.setenv("https_proxy", "127:0.0.1:8888")
+def test_proxy_overrides_env(script: PipTestEnvironment) -> None:
     with proxy.Proxy(
         port=8899,
     ), proxy.Proxy(plugins=[AccessLogPlugin], port=8888):
+        script.environ["http_proxy"] = "127:0.0.1:8888"
+        script.environ["https_proxy"] = "127:0.0.1:8888"
         result = script.pip(
             "download",
             "--proxy",
@@ -35,3 +41,49 @@ def test_proxy_overrides_env(
         )
         result.did_create(Path("scratch") / "pip_downloads" / "INITools-0.1.tar.gz")
         assert "CONNECT" not in result.stdout
+
+
+def test_proxy_does_not_override_netrc(
+    script: PipTestEnvironment,
+    data: TestData,
+    cert_factory: CertFactory,
+) -> None:
+    cert_path = cert_factory()
+    ctx = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+    ctx.load_cert_chain(cert_path, cert_path)
+    ctx.load_verify_locations(cafile=cert_path)
+    ctx.verify_mode = ssl.CERT_REQUIRED
+
+    server = make_mock_server(ssl_context=ctx)
+    server.mock.side_effect = [
+        package_page(
+            {
+                "simple-3.0.tar.gz": "/files/simple-3.0.tar.gz",
+            }
+        ),
+        authorization_response(data.packages / "simple-3.0.tar.gz"),
+        authorization_response(data.packages / "simple-3.0.tar.gz"),
+    ]
+
+    url = f"https://{server.host}:{server.port}/simple"
+
+    netrc = script.scratch_path / ".netrc"
+    netrc.write_text(f"machine {server.host} login USERNAME password PASSWORD")
+    with proxy.Proxy(port=8888), server_running(server):
+        script.environ["NETRC"] = netrc
+        script.pip(
+            "install",
+            "--proxy",
+            "http://127.0.0.1:8888",
+            "--trusted-host",
+            "127.0.0.1",
+            "--no-cache-dir",
+            "--index-url",
+            url,
+            "--cert",
+            cert_path,
+            "--client-cert",
+            cert_path,
+            "simple",
+        )
+        script.assert_installed(simple="3.0")
