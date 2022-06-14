88f86ab5f778713a4b7aa6b56ad805839e7fd6e8
aws-taylor <57725958+aws-taylor@users.noreply.github.com>
2022-06-14 16:12:46 -0700

    Forward 'message' argument of BackendInvalid to Exception
    The 'BackendInvalid' exception does not include traceback information like the other backend exceptions, but instead exposes a 'message'.  Since the intention seems to have been that this field would be some piece of useful customer-facing information, it makes sense to forward this up to the Exception base class so that it's rendered on the console. Without this, customers see an opaque `pip._vendor.pep517.wrappers.BackendInvalid` whereas with this customers will see `pip._vendor.pep517.wrappers.BackendInvalid: message` if thrown by the backend.

--- src/pip/_vendor/pep517/wrappers.py
+++ src/pip/_vendor/pep517/wrappers.py
@@ -39,9 +39,9 @@ class BackendUnavailable(Exception):
 class BackendInvalid(Exception):
     """Will be raised if the backend is invalid."""
     def __init__(self, backend_name, backend_path, message):
+        super(BackendInvalid, self).__init__(message)
         self.backend_name = backend_name
         self.backend_path = backend_path
-        self.message = message
 
 
 class HookMissing(Exception):
