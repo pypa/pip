c7d94b982b58727711ea5c532d763cf83d10c0d1
Robert O'Shea <robertoshea2k11@gmail.com>
2022-08-01 20:08:38 +0100

    Removed unused import

--- src/pip/_internal/operations/install/wheel.py
+++ src/pip/_internal/operations/install/wheel.py
@@ -41,7 +41,7 @@ from pip._vendor.distlib.util import get_export_entry
 from pip._vendor.packaging.utils import canonicalize_name
 
 from pip._internal.exceptions import InstallationError
-from pip._internal.locations import get_major_minor_version, get_scheme
+from pip._internal.locations import get_major_minor_version
 from pip._internal.metadata import (
     BaseDistribution,
     FilesystemWheel,
