4d13842ec6e246e90718c371b74503cee8d061ad
hauntsaninja <hauntsaninja@gmail.com>
2022-08-12 18:48:15 -0700

    fixups

--- src/pip/_internal/utils/hashes.py
+++ src/pip/_internal/utils/hashes.py
@@ -1,5 +1,5 @@
 import hashlib
-from typing import TYPE_CHECKING, BinaryIO, Dict, Iterable, List
+from typing import TYPE_CHECKING, BinaryIO, Dict, Iterable, List, Optional
 
 from pip._internal.exceptions import HashMismatch, HashMissing, InstallationError
 from pip._internal.utils.misc import read_chunks
