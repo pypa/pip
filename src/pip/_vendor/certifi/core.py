128ee65bc7bf4799f8b7c81c01d1445eddf9eb77
@hadesragnarokclassic <115194255+hadesragnarokclassic@users.noreply.github.com>
2022-10-26 05:25:37 +0700

    Revert "Update certifi and replace complex certifi patch with a more targetted requests patch"

--- src/pip/_vendor/certifi/core.py
+++ src/pip/_vendor/certifi/core.py
@@ -4,12 +4,26 @@ certifi.py
 
 This module returns the installation location of cacert.pem or its contents.
 """
-import sys
+import os
+import types
+from typing import Union
 
 
-if sys.version_info >= (3, 11):
+class _PipPatchedCertificate(Exception):
+    pass
 
-    from importlib.resources import as_file, files
+
+try:
+    # Return a certificate file on disk for a standalone pip zipapp running in
+    # an isolated build environment to use. Passing --cert to the standalone
+    # pip does not work since requests calls where() unconditionally on import.
+    _PIP_STANDALONE_CERT = os.environ.get("_PIP_STANDALONE_CERT")
+    if _PIP_STANDALONE_CERT:
+        def where():
+            return _PIP_STANDALONE_CERT
+        raise _PipPatchedCertificate()
+
+    from importlib.resources import path as get_path, read_text
 
     _CACERT_CTX = None
     _CACERT_PATH = None
@@ -33,54 +47,15 @@ if sys.version_info >= (3, 11):
             # We also have to hold onto the actual context manager, because
             # it will do the cleanup whenever it gets garbage collected, so
             # we will also store that at the global level as well.
-            _CACERT_CTX = as_file(files("pip._vendor.certifi").joinpath("cacert.pem"))
-            _CACERT_PATH = str(_CACERT_CTX.__enter__())
-
-        return _CACERT_PATH
-
-    def contents() -> str:
-        return files("pip._vendor.certifi").joinpath("cacert.pem").read_text(encoding="ascii")
-
-elif sys.version_info >= (3, 7):
-
-    from importlib.resources import path as get_path, read_text
-
-    _CACERT_CTX = None
-    _CACERT_PATH = None
-
-    def where() -> str:
-        # This is slightly terrible, but we want to delay extracting the
-        # file in cases where we're inside of a zipimport situation until
-        # someone actually calls where(), but we don't want to re-extract
-        # the file on every call of where(), so we'll do it once then store
-        # it in a global variable.
-        global _CACERT_CTX
-        global _CACERT_PATH
-        if _CACERT_PATH is None:
-            # This is slightly janky, the importlib.resources API wants you
-            # to manage the cleanup of this file, so it doesn't actually
-            # return a path, it returns a context manager that will give
-            # you the path when you enter it and will do any cleanup when
-            # you leave it. In the common case of not needing a temporary
-            # file, it will just return the file system location and the
-            # __exit__() is a no-op.
-            #
-            # We also have to hold onto the actual context manager, because
-            # it will do the cleanup whenever it gets garbage collected, so
-            # we will also store that at the global level as well.
             _CACERT_CTX = get_path("pip._vendor.certifi", "cacert.pem")
             _CACERT_PATH = str(_CACERT_CTX.__enter__())
 
         return _CACERT_PATH
 
-    def contents() -> str:
-        return read_text("pip._vendor.certifi", "cacert.pem", encoding="ascii")
+except _PipPatchedCertificate:
+    pass
 
-else:
-    import os
-    import types
-    from typing import Union
-
+except ImportError:
     Package = Union[types.ModuleType, str]
     Resource = Union[str, "os.PathLike"]
 
@@ -104,5 +79,6 @@ else:
 
         return os.path.join(f, "cacert.pem")
 
-    def contents() -> str:
-        return read_text("pip._vendor.certifi", "cacert.pem", encoding="ascii")
+
+def contents() -> str:
+    return read_text("certifi", "cacert.pem", encoding="ascii")
