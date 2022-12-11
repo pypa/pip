cb6884ce8e6bbe85e29fda6271bfdb21c2b01483
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2022-12-11 20:08:27 +0000

    Pacify flake8 by re-exporting the variables under the same name

--- src/pip/_internal/build_env/__init__.py
+++ src/pip/_internal/build_env/__init__.py
@@ -8,9 +8,12 @@ if sys.version_info >= (3, 8):
 else:
     from pip._vendor.typing_extensions import Literal
 
-from pip._internal.build_env._base import BuildEnvironment, get_runnable_pip
-from pip._internal.build_env._custom import CustomBuildEnvironment
-from pip._internal.build_env._noop import NoOpBuildEnvironment
-from pip._internal.build_env._venv import VenvBuildEnvironment
+from pip._internal.build_env._base import BuildEnvironment as BuildEnvironment
+from pip._internal.build_env._base import get_runnable_pip as get_runnable_pip
+from pip._internal.build_env._custom import (
+    CustomBuildEnvironment as CustomBuildEnvironment,
+)
+from pip._internal.build_env._noop import NoOpBuildEnvironment as NoOpBuildEnvironment
+from pip._internal.build_env._venv import VenvBuildEnvironment as VenvBuildEnvironment
 
 BuildIsolationMode = Literal["noop", "custom", "venv"]
