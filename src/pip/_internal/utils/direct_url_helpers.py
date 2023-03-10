c29b3ecd791cdf0eeb58cb57296738ffa5b386d2
Fridolin Pokorny <fridolin.pokorny@datadoghq.com>
2023-03-10 16:30:18 +0100

    Remove unused import
    Signed-off-by: Fridolin Pokorny <fridolin.pokorny@datadoghq.com>

--- src/pip/_internal/utils/direct_url_helpers.py
+++ src/pip/_internal/utils/direct_url_helpers.py
@@ -2,7 +2,6 @@ from typing import Optional
 
 from pip._internal.models.direct_url import ArchiveInfo, DirectUrl, DirInfo, VcsInfo
 from pip._internal.models.link import Link
-from pip._internal.utils.misc import hash_file
 from pip._internal.utils.urls import path_to_url
 from pip._internal.vcs import vcs
 
