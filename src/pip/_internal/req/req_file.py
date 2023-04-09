2f1d4a0218f9daf013292be9656c362cbc1760e1
Stéphane Bidoul <stephane.bidoul@gmail.com>
2023-04-09 11:03:49 +0200

    Warn if --hash is used on a line without requirement
    in a requirements file

--- src/pip/_internal/req/req_file.py
+++ src/pip/_internal/req/req_file.py
@@ -2,6 +2,7 @@
 Requirements file parsing
 """
 
+import logging
 import optparse
 import os
 import re
@@ -75,6 +76,8 @@ SUPPORTED_OPTIONS_REQ: List[Callable[..., optparse.Option]] = [
 
 # the 'dest' string values
 SUPPORTED_OPTIONS_REQ_DEST = [str(o().dest) for o in SUPPORTED_OPTIONS_REQ]
+
+logger = logging.getLogger(__name__)
 
 
 class ParsedRequirement:
@@ -209,6 +212,13 @@ def handle_option_line(
     options: Optional[optparse.Values] = None,
     session: Optional[PipSession] = None,
 ) -> None:
+    if opts.hashes:
+        logger.warning(
+            "%s line %s has --hash but no requirement, and will be ignored.",
+            filename,
+            lineno,
+        )
+
     if options:
         # percolate options upward
         if opts.require_hashes:
