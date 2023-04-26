81eb6c6d1350a1122144d6906f871f8b9f7348d6
Darshan <Darshancoding@gmail.com>
2023-04-26 20:51:40 +0530

    testing linters 2

--- src/pip/_internal/commands/uninstall.py
+++ src/pip/_internal/commands/uninstall.py
@@ -7,7 +7,7 @@ from pip._vendor.packaging.utils import canonicalize_name
 from pip._internal.cli import cmdoptions
 from pip._internal.cli.base_command import Command
 from pip._internal.cli.req_command import SessionCommandMixin, warn_if_run_as_root
-from pip._internal.cli.status_codes import ERROR, SUCCESS
+from pip._internal.cli.status_codes import SUCCESS
 from pip._internal.exceptions import InstallationError
 from pip._internal.req import parse_requirements
 from pip._internal.req.constructors import (
