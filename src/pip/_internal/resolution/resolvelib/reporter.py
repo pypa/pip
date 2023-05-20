1552934c088269437ca183a18a571833e94c8c05
stonebig <stonebig34@gmail.com>
2023-05-20 17:12:03 +0200

    Update reporter.py

--- src/pip/_internal/resolution/resolvelib/reporter.py
+++ src/pip/_internal/resolution/resolvelib/reporter.py
@@ -20,9 +20,9 @@ class PipReporter(BaseReporter):
                 "requirements. This could take a while."
             ),
             8: (
-                "pip is looking at multiple versions of {package_name} to "
+                "pip is still looking at multiple versions of {package_name} to "
                 "determine which version is compatible with other "
-                "requirements. This could take quite a while."
+                "requirements. This could take a while."
             ),
             13: (
                 "This is taking longer than usual. You might need to provide "
