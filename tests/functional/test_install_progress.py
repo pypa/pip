5f7782a4ae81991b9b5a0766538ebe5e0d1c2766
Joey Ballentine <joeyjballentine@gmail.com>
2023-06-13 23:09:33 -0400

    maybe this time

--- tests/functional/test_install_progress.py
+++ tests/functional/test_install_progress.py
@@ -1,19 +1,22 @@
 from tests.lib import (
     PipTestEnvironment,
+    TestData,
 )
+import subprocess
 
 
-def test_install_with_json_progress(script: PipTestEnvironment) -> None:
+def test_install_with_json_progress_cli(
+    script: PipTestEnvironment, data: TestData
+) -> None:
     """
     Test installing a package using pip install --progress-bar=json
     but not as a subprocess
     """
     result = script.pip(
         "install",
-        "simple==1.0",
-        "--no-index",
-        "--find-links",
-        script.scratch_path,
+        "dinner",
+        "--index-url",
+        data.find_links3,
         "--progress-bar=json",
         expect_error=True,
     )
@@ -21,3 +24,25 @@ def test_install_with_json_progress(script: PipTestEnvironment) -> None:
         'The "json" progress_bar type should only be used inside subprocesses.'
         in result.stderr
     )
+
+
+def test_install_with_json_progress_subproc(
+    _script: PipTestEnvironment, data: TestData
+) -> None:
+    """
+    Test installing a package using pip install --progress-bar=json
+    but not as a subprocess
+    """
+    result = subprocess.check_output(
+        [
+            "python",
+            "-m",
+            "pip",
+            "install",
+            "dinner",
+            "--index-url",
+            data.find_links3,
+            "--progress-bar=json",
+        ]
+    )
+    assert "PROGRESS:" in result.decode("utf-8")
