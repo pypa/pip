e862bc9c4e19447215946269739e40621cef271a
Matt Davis <matteius@gmail.com>
2023-06-26 20:25:28 -0400

    Correction to account for vcs only (no index urls)

--- tests/unit/test_search_scope.py
+++ tests/unit/test_search_scope.py
@@ -25,8 +25,8 @@ class TestSearchScope:
         assert "links-user:****@page.domain.com" in result
         assert "links-pass" not in result
 
-    def test_get_index_urls_locations(self) -> None:
-        """Check that the canonical name is on all indexes"""
+    def test_get_index_urls_location_default(self) -> None:
+        """Check that the default index url is used when no index is specified."""
         search_scope = SearchScope(
             find_links=[],
             index_urls=["file://index1/", "file://index2"],
@@ -35,7 +35,17 @@ class TestSearchScope:
         req = install_req_from_line("Complex_Name")
         assert req.name is not None
         actual = search_scope.get_index_urls_locations(req.name)
-        assert actual == [
-            "file://index1/complex-name/",
-            "file://index2/complex-name/",
-        ]
+        assert actual == ["file://index1/complex-name/"]
+
+    def test_get_index_urls_location_specified(self) -> None:
+        """Check that the specified index url is used."""
+        search_scope = SearchScope(
+            find_links=[],
+            index_urls=["file://index1/", "file://index2"],
+            no_index=False,
+            index_lookup={"complex-name": "file://index2"},
+        )
+        req = install_req_from_line("Complex_Name")
+        assert req.name is not None
+        actual = search_scope.get_index_urls_locations(req.name)
+        assert actual == ["file://index2/complex-name/"]
