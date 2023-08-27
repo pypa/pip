306343a7e9bf0826bed6e478b9509034692bf8cd
Mido <akeelmido@gmail.com>
2023-08-26 20:04:42 -0400

    Apply suggestions from code review
    fail fast simplifications, legibility, some performance improvement
    
    Co-authored-by: Christian Clauss <cclauss@me.com>

--- src/pip/_internal/cli/cmdoptions.py
+++ src/pip/_internal/cli/cmdoptions.py
@@ -112,18 +112,15 @@ def validate_platform_options(platform_options: List[str]) -> None:
     def is_macos_arch(suffix: str) -> bool:
         osx_re = re.compile(r"(?P<major>\d+)_(?P<minor>\d+)_(?P<arch>.+)")
         match = osx_re.fullmatch(suffix)
-        if match:
-            major, minor, arch = (
-                int(match.group("major")),
-                int(match.group("minor")),
-                match.group("arch"),
-            )
-
-            if (major, minor) < (10, 0) or arch not in ["arm64", "intel", "x86_64"]:
-                return False
-        else:
+        if not match:
             return False
-        return True
+
+        major, minor, arch = (
+            int(match.group("major")),
+            int(match.group("minor")),
+            match.group("arch")
+        )
+        return (major, minor) >= (10, 0) or arch in {"arm64", "intel", "x86_64"}:
 
     def is_linux_arch(prefix: str, suffix: str) -> bool:
         """
@@ -134,41 +131,36 @@ def validate_platform_options(platform_options: List[str]) -> None:
         # by _<architecture>
         linux_re = re.compile(r"(?P<arch>.+)")
         match = linux_re.fullmatch(suffix)
-        if match:
-            arch = match.group("arch")
-            if prefix in ["manylinux1", "manylinux2010"]:
-                if arch not in ["x86_64", "i686"]:
-                    return False
-            elif prefix == "manylinux2014":
-                if arch not in [
-                    "x86_64",
-                    "i686",
-                    "aarch64",
-                    "armv71",
-                    "ppc64",
-                    "ppc64le",
-                    "s390x",
-                ]:
-                    return False
-            elif prefix == "linux":
-                if arch not in ["x86_64", "i386"]:
-                    return False
-        else:
+        if not match:
             return False
-        return True
+
+        arch = match.group("arch")
+        if prefix == "linux":
+            return arch in {"i386", "x86_64"}
+        if prefix in {"manylinux1", "manylinux2010"}:
+            return arch in {"i686", "x86_64"}
+        if prefix == "manylinux2014":
+            return arch in {
+                "aarch64",
+                "armv71",
+                "i686",
+                "ppc64",
+                "ppc64le",
+                "s390x",
+                "x86_64",
+             }
+        return False
 
     def is_glibc_linux_arch(suffix: str) -> bool:
         # manylinux alone should be followed by a _<major>_<minor>_<arch>
         glibc_manylinux_re = re.compile(r"(?P<major>\d+)_(?P<minor>\d+)_(?P<arch>.+)")
         glx = glibc_manylinux_re.fullmatch(suffix)
-        if not glx:
-            # PEP 600 recommends any tag of the form "manylinux_[0-9]+_[0-9]+_(.*)"
-            # be accepted by package indexes. Therefore, as long as the glibc
-            # major and minor versions are digits and there exists a value for
-            # the architecture, we should accept it. If they do not exist
-            # return False as improper input
-            return False
-        return True
+        # PEP 600 recommends any tag of the form "manylinux_[0-9]+_[0-9]+_(.*)"
+        # be accepted by package indexes. Therefore, as long as the glibc
+        # major and minor versions are digits and there exists a value for
+        # the architecture, we should accept it. If they do not exist
+        # return False as improper input
+        return bool(glx)
 
     def is_win_arch(suffix: str) -> bool:
         # we don't need to check for a win32 given the calling function
@@ -176,12 +168,7 @@ def validate_platform_options(platform_options: List[str]) -> None:
         # to check if the architecture matches amd64.
         win_re = re.compile(r"(?P<arch>)")
         match = win_re.fullmatch(suffix)
-        if match:
-            if match.group("arch") != "amd64":
-                return False
-        else:
-            return False
-        return True
+        return match.group("arch") == "amd64" if match else False
 
     if not platform_options:
         return
@@ -194,12 +181,12 @@ def validate_platform_options(platform_options: List[str]) -> None:
         if platform_prefix == "macosx":
             if not is_macos_arch(platform_suffix):
                 invalid_platforms.append(platform_tag)
-        elif platform_prefix in [
-            "manylinux2014",
-            "manylinux2010",
-            "manylinux1",
+        elif platform_prefix in {
             "linux",
-        ]:
+            "manylinux1",
+            "manylinux2010",
+            "manylinux2014",
+        }
             if not is_linux_arch(platform_prefix, platform_suffix):
                 invalid_platforms.append(platform_tag)
         elif platform_prefix == "manylinux":
