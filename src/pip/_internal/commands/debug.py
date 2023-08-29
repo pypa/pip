0c0099b23b1109cd26c66def926ffab4c99f73cd
Christian Clauss <cclauss@me.com>
2023-08-29 11:31:42 +0200

    Ruff misidentifies config as a dict
    Co-authored-by: Tzu-ping Chung <uranusjr@gmail.com>

--- src/pip/_internal/commands/debug.py
+++ src/pip/_internal/commands/debug.py
@@ -134,6 +134,8 @@ def show_tags(options: Values) -> None:
 
 
 def ca_bundle_info(config: Configuration) -> str:
+    # Ruff misidentifies config as a dict.
+    # Configuration does not have support the mapping interface.
     levels = {key.split(".")[0] for key, _ in config.items()}  # noqa: PERF102
     if not levels:
         return "Not specified"
