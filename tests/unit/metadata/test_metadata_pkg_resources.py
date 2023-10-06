908e9130205ef7027dd6a095718ce7bfee58b461
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2023-10-06 19:59:17 +0100

    Do not normalise `iter_provided_extras` from pkg_resources
    This is explicitly checked for in the test suite and it isn't strictly
    necessary to ensure the correct behaviour. This does expose that the
    test mock objects are incomplete so those get a missing `extra`
    attribute added.

--- tests/unit/metadata/test_metadata_pkg_resources.py
+++ tests/unit/metadata/test_metadata_pkg_resources.py
@@ -39,17 +39,17 @@ class _MockWorkingSet(List[mock.Mock]):
 
 workingset = _MockWorkingSet(
     (
-        mock.Mock(test_name="global", project_name="global"),
-        mock.Mock(test_name="editable", project_name="editable"),
-        mock.Mock(test_name="normal", project_name="normal"),
-        mock.Mock(test_name="user", project_name="user"),
+        mock.Mock(test_name="global", project_name="global", extras=[]),
+        mock.Mock(test_name="editable", project_name="editable", extras=[]),
+        mock.Mock(test_name="normal", project_name="normal", extras=[]),
+        mock.Mock(test_name="user", project_name="user", extras=[]),
     )
 )
 
 workingset_stdlib = _MockWorkingSet(
     (
-        mock.Mock(test_name="normal", project_name="argparse"),
-        mock.Mock(test_name="normal", project_name="wsgiref"),
+        mock.Mock(test_name="normal", project_name="argparse", extras=[]),
+        mock.Mock(test_name="normal", project_name="wsgiref", extras=[]),
     )
 )
 
