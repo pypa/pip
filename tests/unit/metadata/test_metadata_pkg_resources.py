e38c2b95d3a6cac8f51cd666c6b08e87b71233b1
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2023-10-06 19:58:09 +0100

    Compare `Requirement` objects rather than requirement strings
    This ensures that the comparision does not fail due to mismatching extra
    normalisation.

--- tests/unit/metadata/test_metadata_pkg_resources.py
+++ tests/unit/metadata/test_metadata_pkg_resources.py
@@ -4,6 +4,7 @@ from typing import List, cast
 from unittest import mock
 
 import pytest
+from pip._vendor.packaging.requirements import Requirement
 from pip._vendor.packaging.specifiers import SpecifierSet
 from pip._vendor.packaging.version import parse as parse_version
 
@@ -82,7 +83,7 @@ def test_wheel_metadata_works() -> None:
     name = "simple"
     version = "0.1.0"
     require_a = "a==1.0"
-    require_b = 'b==1.1; extra == "also-b"'
+    require_b = 'b==1.1; extra == "also_b"'
     requires = [require_a, require_b, 'c==1.2; extra == "also_c"']
     extras = ["also_b", "also_c"]
     requires_python = ">=3"
@@ -108,8 +109,8 @@ def test_wheel_metadata_works() -> None:
     assert parse_version(version) == dist.version
     assert set(extras) == set(dist.iter_provided_extras())
     assert [require_a] == [str(r) for r in dist.iter_dependencies()]
-    assert [require_a, require_b] == [
-        str(r) for r in dist.iter_dependencies(["also_b"])
+    assert [Requirement(require_a), Requirement(require_b)] == [
+        Requirement(str(r)) for r in dist.iter_dependencies(["also_b"])
     ]
     assert metadata.as_string() == dist.metadata.as_string()
     assert SpecifierSet(requires_python) == dist.requires_python
