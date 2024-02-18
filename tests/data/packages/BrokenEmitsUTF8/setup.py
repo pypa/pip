9df756dcfc733950a17aad8c38151498cf06a44c
Hugo van Kemenade <1324225+hugovk@users.noreply.github.com>
2024-02-18 17:56:21 +0200

    Update for consistency
    Co-authored-by: Paul Moore <p.f.moore@gmail.com>

--- tests/data/packages/BrokenEmitsUTF8/setup.py
+++ tests/data/packages/BrokenEmitsUTF8/setup.py
@@ -8,7 +8,7 @@ class FakeError(Exception):
 
 if sys.argv[1] in ("install", "bdist_wheel"):
     if hasattr(sys.stdout, "buffer"):
-        sys.stdout.buffer.write(b"\nThis package prints out UTF-8 stuff like:\n")
+        sys.stdout.buffer.write("\nThis package prints out UTF-8 stuff like:\n".encode())
         sys.stdout.buffer.write("* return type of ‘main’ is not ‘int’\n".encode())
         sys.stdout.buffer.write(
             "* Björk Guðmundsdóttir [ˈpjœr̥k ˈkvʏðmʏntsˌtoʊhtɪr]".encode()
