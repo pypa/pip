ac5ed744a8a6e7c07d02514be0b6015574c9355f
Richard Si <sichard26@gmail.com>
2024-03-12 17:02:06 -0400

    Remove PyPI package direct URL dependency check

--- src/pip/_internal/req/constructors.py
+++ src/pip/_internal/req/constructors.py
@@ -19,7 +19,6 @@ from pip._vendor.packaging.requirements import InvalidRequirement, Requirement
 from pip._vendor.packaging.specifiers import Specifier
 
 from pip._internal.exceptions import InstallationError
-from pip._internal.models.index import PyPI, TestPyPI
 from pip._internal.models.link import Link
 from pip._internal.models.wheel import Wheel
 from pip._internal.req.req_file import ParsedRequirement
@@ -447,23 +446,6 @@ def install_req_from_req_string(
         req = get_requirement(req_string)
     except InvalidRequirement:
         raise InstallationError(f"Invalid requirement: '{req_string}'")
-
-    domains_not_allowed = [
-        PyPI.file_storage_domain,
-        TestPyPI.file_storage_domain,
-    ]
-    if (
-        req.url
-        and comes_from
-        and comes_from.link
-        and comes_from.link.netloc in domains_not_allowed
-    ):
-        # Explicitly disallow pypi packages that depend on external urls
-        raise InstallationError(
-            "Packages installed from PyPI cannot depend on packages "
-            "which are not also hosted on PyPI.\n"
-            f"{comes_from.name} depends on {req} "
-        )
 
     return InstallRequirement(
         req,
