07b9cedcca10afe93ceca9ed2ab9f44e07c32e3c
Jeremy Fleischman <jeremyfleischman@gmail.com>
2024-06-05 09:14:57 -0700

    Add support for pulling username from keyring subprocess provider
    This fixes https://github.com/pypa/pip/issues/12543.
    
    Somewhat confusingly, when using using `PIP_KEYRING_PROVIDER=import`,
    pip was able to fetch both a username and a password from keyring, but
    when using `PIP_KEYRING_PROVIDER=subprocess`, it needed the username.
    
    I had to rework the existing tests quite a bit to fit with this new
    behavior, as it's now OK to ask for a username/password for an index
    even if you don't have a username. This is why `KeyringSubprocessResult`
    now subclasses `KeyringModuleV2`. While I was in here, I opted to remove
    the "fixtures" values from `KeyringModuleV2` by introducing this new
    `add_credential` contextmanager. IMO, they were hurting the readability
    of our tests: you had to jump around quite a bit to see what the
    contents of the keyring would be during our tests. It also forced all
    our tests to play nicely with the same fixtures values, which IMO was an
    unnecessary constraint.
    
    Note: per the discussion
    [here](https://github.com/pypa/pip/pull/12748#issuecomment-2294948227),
    I've opted not to implement any "feature detection" to see if the
    `keyring` subprocess supports `--mode=creds`. For the record, this
    feature was added in
    https://github.com/jaraco/keyring/commit/7830a6418a77edfe28d4c4ba3142a2a6f8a70de6,
    which landed in keyring v25.2.0.

--- src/pip/_internal/network/auth.py
+++ src/pip/_internal/network/auth.py
@@ -6,6 +6,7 @@ providing credentials in the context of network requests.
 
 from __future__ import annotations
 
+import json
 import logging
 import os
 import shutil
@@ -118,23 +119,20 @@ class KeyRingCliProvider(KeyRingBaseProvider):
         self.keyring = cmd
 
     def get_auth_info(self, url: str, username: str | None) -> AuthInfo | None:
-        # This is the default implementation of keyring.get_credential
-        # https://github.com/jaraco/keyring/blob/97689324abcf01bd1793d49063e7ca01e03d7d07/keyring/backend.py#L134-L139
-        if username is not None:
-            password = self._get_password(url, username)
-            if password is not None:
-                return username, password
-        return None
+        return self._get_creds(url, username)
 
     def save_auth_info(self, url: str, username: str, password: str) -> None:
         return self._set_password(url, username, password)
 
-    def _get_password(self, service_name: str, username: str) -> str | None:
-        """Mirror the implementation of keyring.get_password using cli"""
+    def _get_creds(self, service_name: str, username: str | None) -> AuthInfo | None:
+        """Mirror the implementation of keyring.get_credential using cli"""
         if self.keyring is None:
             return None
 
-        cmd = [self.keyring, "get", service_name, username]
+        cmd = [self.keyring, "--mode=creds", "--output=json", "get", service_name]
+        if username is not None:
+            cmd.append(username)
+
         env = os.environ.copy()
         env["PYTHONIOENCODING"] = "utf-8"
         res = subprocess.run(
@@ -145,7 +143,9 @@ class KeyRingCliProvider(KeyRingBaseProvider):
         )
         if res.returncode:
             return None
-        return res.stdout.decode("utf-8").strip(os.linesep)
+
+        data = json.loads(res.stdout.decode("utf-8"))
+        return (data["username"], data["password"])
 
     def _set_password(self, service_name: str, username: str, password: str) -> None:
         """Mirror the implementation of keyring.set_password using cli"""
