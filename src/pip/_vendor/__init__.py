a866308712f53780095f27d6d02fe4f25571b759
Michał Górny <mgorny@gentoo.org>
2024-06-26 02:52:32 +0200

    Remove `pep517` from DEBUNDLED preload list

--- src/pip/_vendor/__init__.py
+++ src/pip/_vendor/__init__.py
@@ -65,7 +65,6 @@ if DEBUNDLED:
     vendored("packaging")
     vendored("packaging.version")
     vendored("packaging.specifiers")
-    vendored("pep517")
     vendored("pkg_resources")
     vendored("platformdirs")
     vendored("progress")
