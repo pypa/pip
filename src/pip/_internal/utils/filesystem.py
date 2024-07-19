303f62889244f0c658b3d8b64dba594858fe6e8e
rmorotti <romain.morotti@man.com>
2024-07-19 13:37:34 +0100

    PERF: remove fsync call on temporary file, it's not necessary

--- src/pip/_internal/utils/filesystem.py
+++ src/pip/_internal/utils/filesystem.py
@@ -61,18 +61,7 @@ def adjacent_tmp_file(path: str, **kwargs: Any) -> Generator[BinaryIO, None, Non
         try:
             yield result
         finally:
-            import time
-            start = time.perf_counter()
             result.flush()
-            middle = time.perf_counter()
-            os.fsync(result.fileno())
-            end = time.perf_counter()
-            global total_flush
-            global total_fsync
-            total_flush += middle - start
-            total_fsync += end - start
-            print("adjacent_tmp_file({}) flush {:.3f} (cumulative {:.3f})".format(f, middle-start, total_flush))
-            print("adjacent_tmp_file({}) fsync {:.3f} (cumulative {:.3f})".format(f, end-middle, total_fsync))
 
 
 replace = retry(stop_after_delay=1, wait=0.25)(os.replace)
