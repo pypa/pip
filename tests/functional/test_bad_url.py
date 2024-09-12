5eaccb83008da9b2e0999642d4841b7816475394
Tzu-ping Chung <uranusjr@gmail.com>
2024-09-13 01:47:29 +0800

    Also change the comment

--- tests/functional/test_bad_url.py
+++ tests/functional/test_bad_url.py
@@ -7,7 +7,7 @@ from typing import Any
 def test_filenotfound_error_message(script: Any) -> None:
     # Test the error message returned when using a bad 'file:' URL.
     # make pip to fail and get an error message
-    # by running "pip install -r file:nonexistent_file"
+    # by running "pip install -r file:///nonexistent_file"
     proc = script.pip("install", "-r", "file:///unexistent_file", expect_error=True)
     assert proc.returncode == 1
     expect = (
