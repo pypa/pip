db91fcb8d5d9e4a6d567cbdd5c3c2c47e5769887
Facundo Tuesca <facundo.tuesca@trailofbits.com>
2024-10-01 21:41:30 +0200

    Load plugins during init rather than on import

--- src/pip/_internal/utils/plugins.py
+++ src/pip/_internal/utils/plugins.py
@@ -7,7 +7,6 @@ from typing import Iterator, List
 from pip._internal.models.plugin import DistInspectorPlugin, Plugin, plugin_from_module
 
 logger = logging.getLogger(__name__)
-
 _loaded_plugins: List[Plugin] = []
 
 
@@ -24,15 +23,16 @@ def iter_entry_points(group_name: str) -> EntryPoints:
         return groups.get(group_name, [])
 
 
-for entrypoint in iter_entry_points(group_name="pip.plugins"):
-    try:
-        module = entrypoint.load()
-    except ModuleNotFoundError:
-        logger.warning("Tried to load plugin %s but failed", entrypoint.name)
-        continue
-    plugin = plugin_from_module(entrypoint.name, module)
-    if plugin is not None:
-        _loaded_plugins.append(plugin)
+def load_plugins() -> None:
+    for entrypoint in iter_entry_points(group_name="pip.plugins"):
+        try:
+            module = entrypoint.load()
+        except ModuleNotFoundError:
+            logger.warning("Tried to load plugin %s but failed", entrypoint.name)
+            continue
+        plugin = plugin_from_module(entrypoint.name, module)
+        if plugin is not None:
+            _loaded_plugins.append(plugin)
 
 
 @contextlib.contextmanager
