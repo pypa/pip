399f4ea139345a21a2e93b3b4a90b437ace91f94
dkjsone <221672629+dkjsone@users.noreply.github.com>
2025-09-03 23:24:51 +0800

    Update test cases with clearer exception assertions; Update news entry

--- tests/unit/test_utils_unpacking.py
+++ tests/unit/test_utils_unpacking.py
@@ -336,8 +336,8 @@ class TestUnpackArchives:
         with pytest.raises(InstallationError) as e:
             untar_file(tar_filepath, extract_path)
 
-        assert "trying to install outside target directory" in str(e.value)
-        assert "import_file" in str(e.value)
+        msg = "The tar file ({}) has a file ({}) trying to install outside target directory ({})"
+        assert msg.format(tar_filepath, "evil_symlink", import_filepath) in str(e.value)
 
         assert not os.path.exists(os.path.join(extract_path, "evil_symlink"))
 
@@ -359,18 +359,19 @@ class TestUnpackArchives:
 
         extract_path = os.path.join(self.tempdir, "extract_path")
 
+        link_path = ".." + os.sep + import_filename
+
         with tarfile.open(tar_filepath, "w") as tar:
             info = tarfile.TarInfo("evil_symlink")
             info.type = tarfile.SYMTYPE
-            info.linkpath = ".." + os.sep + import_filename
+            info.linkpath = link_path
             tar.addfile(info)
 
         with pytest.raises(InstallationError) as e:
             untar_file(tar_filepath, extract_path)
 
-        assert "trying to install outside target directory" in str(e.value)
-        assert ".." in str(e.value)
-        assert import_filename in str(e.value)
+        msg = "The tar file ({}) has a file ({}) trying to install outside target directory ({})"
+        assert msg.format(tar_filepath, "evil_symlink", link_path) in str(e.value)
 
         assert not os.path.exists(os.path.join(extract_path, "evil_symlink"))
 
