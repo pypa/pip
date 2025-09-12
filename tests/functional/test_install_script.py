0958182a7d8fd2d3607fafd13350eb40a5d80b59
James Gerity <snoopjedi@gmail.com>
2025-09-12 03:40:21 -0400

    Add extra layer of backslash escaping to match test stderr on Windows

--- tests/functional/test_install_script.py
+++ tests/functional/test_install_script.py
@@ -91,8 +91,17 @@ def test_script_file_python_version(script: PipTestEnvironment) -> None:
         expect_stderr=True,
         expect_error=True,
     )
+
+    if sys.platform == "win32":
+        # Special case: result.stderr contains an extra layer of backslash
+        # escaping, transform our path to match
+        script_path_str = str(script_path).replace("\\", "\\\\")
+    else:
+        script_path_str = str(script_path)
+
     assert (
-        f"ERROR: Script '{script_path}' requires a different Python" in result.stderr
+        f"ERROR: Script '{script_path_str}' requires a different Python"
+        in result.stderr
     ), (
         "Script with incompatible requires-python did not fail as expected -- "
         + result.stderr
