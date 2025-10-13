f07e1965bf7b8a5bad3f7e27739d30446f080ed3
Damian Shaw <damian.peter.shaw@gmail.com>
2025-10-12 22:13:34 -0400

    Fix test

--- tests/functional/test_new_resolver_errors.py
+++ tests/functional/test_new_resolver_errors.py
@@ -187,7 +187,7 @@ def test_new_resolver_no_versions_available_hint(script: PipTestEnvironment) -> 
 
     # Check that the new hint message is present
     assert (
-        "Additionally, some projects in these conflicts have no "
+        "Additionally, some packages in these conflicts have no "
         "matching distributions available for your environment:\n"
         "    incompatible-dep\n" in result.stdout
     ), str(result)
