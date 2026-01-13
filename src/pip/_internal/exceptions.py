d1011798f04ca86a928b7697afb268f7c5b0fa7e
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2026-01-13 18:11:09 +0000

    Revert "Avoid importing packaging.requirements module during tab autocompletion"
    This reverts commit f88e8666be39187520fe29607cd605c17a533bd9.

--- src/pip/_internal/exceptions.py
+++ src/pip/_internal/exceptions.py
@@ -19,6 +19,7 @@ from collections.abc import Iterable, Iterator
 from itertools import chain, groupby, repeat
 from typing import TYPE_CHECKING, Literal
 
+from pip._vendor.packaging.requirements import InvalidRequirement
 from pip._vendor.packaging.version import InvalidVersion
 from pip._vendor.rich.console import Console, ConsoleOptions, RenderResult
 from pip._vendor.rich.markup import escape
@@ -27,7 +28,6 @@ from pip._vendor.rich.text import Text
 if TYPE_CHECKING:
     from hashlib import _Hash
 
-    from pip._vendor.packaging.requirements import InvalidRequirement
     from pip._vendor.requests.models import PreparedRequest, Request, Response
 
     from pip._internal.metadata import BaseDistribution
@@ -810,9 +810,6 @@ class InvalidInstalledPackage(DiagnosticPipError):
         dist: BaseDistribution,
         invalid_exc: InvalidRequirement | InvalidVersion,
     ) -> None:
-        # packaging.requirements module is slow to import
-        from pip._vendor.packaging.requirements import InvalidRequirement
-
         installed_location = dist.installed_location
 
         if isinstance(invalid_exc, InvalidRequirement):
