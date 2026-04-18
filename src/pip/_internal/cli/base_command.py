3e2d852723a34ed5d113d67260ed8bc0931437d2
Damian Shaw <damian.peter.shaw@gmail.com>
2026-04-17 23:00:50 -0400

    Fix hang in broken-stdout handler when stderr pipe is full

--- src/pip/_internal/cli/base_command.py
+++ src/pip/_internal/cli/base_command.py
@@ -132,11 +132,15 @@ class Command(CommandContextMixIn):
 
             return ERROR
         except BrokenStdoutLoggingError:
-            # Bypass our logger and write any remaining messages to
-            # stderr because stdout no longer works.
-            print("ERROR: Pipe to stdout was broken", file=sys.stderr)
-            if level_number <= logging.DEBUG:
-                traceback.print_exc(file=sys.stderr)
+            # stdout is broken; write to stderr directly. Use os.write, not
+            # sys.stderr.write, so a full pipe buffer returns EPIPE instead
+            # of deadlocking (Windows anonymous pipes are ~4KB).
+            try:
+                os.write(2, b"ERROR: Pipe to stdout was broken\n")
+                if level_number <= logging.DEBUG:
+                    os.write(2, traceback.format_exc().encode("utf-8", "replace"))
+            except OSError:
+                pass
 
             return ERROR
         except KeyboardInterrupt:
