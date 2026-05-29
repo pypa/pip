bc4461cd078289dc8479353ccc3dae31cb139c31
sepehrrasooli <sepehrrasouli06@gmail.com>
2026-05-29 07:10:51 +0330

    Uset set instead of list and rename variable

--- src/pip/_internal/cli/parser.py
+++ src/pip/_internal/cli/parser.py
@@ -212,18 +212,18 @@ class ConfigOptionParser(CustomOptionParser):
     ) -> None:
         self.name = name
         self.config = Configuration(isolated)
-        self.invalid_variables: list[Any] = []
+        self._seen_invalid_options: set[tuple[str, str]] = set()
         assert self.name
         super().__init__(*args, **kwargs)
 
-    def check_default(self, option: optparse.Option, key: str, val: Any) -> Any:
-        if val in self.invalid_variables:
+    def check_default(self, option: optparse.Option, key: str, val: str) -> Any:
+        if (key, val) in self._seen_invalid_options:
             return None
         try:
             return option.check_value(key, val)
         except optparse.OptionValueError as exc:
             logger.warning("Ignoring configuration key %s: %s", key, exc)
-            self.invalid_variables.append(val)
+            self._seen_invalid_options.add((key, val))
             return None
 
     def _get_ordered_configuration_items(
