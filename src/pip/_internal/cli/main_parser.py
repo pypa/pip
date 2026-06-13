a5718f516817c1cedcc9f753d6b79a61e63c666d
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2026-06-13 22:11:58 +0100

    perf: Avoid importing build_env and`metadata modules during autocompletion (#13735)
    - Moving `get_runnable_pip` to `utils.misc` module to avoid importing
      `build_env` module.
    
    - Lazy importing `metadata` module (which transitively imports `email` and
      `urllib.request` which are slow)

--- src/pip/_internal/cli/main_parser.py
+++ src/pip/_internal/cli/main_parser.py
@@ -8,12 +8,11 @@ import sys
 
 from pip._vendor.rich.markup import escape
 
-from pip._internal.build_env import get_runnable_pip
 from pip._internal.cli import cmdoptions
 from pip._internal.cli.parser import ConfigOptionParser, UpdatingDefaultsHelpFormatter
 from pip._internal.commands import commands_dict, get_similar_commands
 from pip._internal.exceptions import CommandError
-from pip._internal.utils.misc import get_pip_version, get_prog
+from pip._internal.utils.misc import get_pip_version, get_prog, get_runnable_pip
 
 __all__ = ["create_main_parser", "parse_command"]
 
