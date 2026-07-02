67835178f9af0603391e9a3e2664b2c837b37f73
lets-build-an-ocean <sahebesabr11228@gmail.com>
2026-07-02 22:17:43 +0330

    Remove stdlib_pkgs from utils.compat

--- src/pip/_internal/utils/compat.py
+++ src/pip/_internal/utils/compat.py
@@ -87,13 +87,5 @@ else:
     from pip._vendor import tomli as tomllib
 
 
-# packages in the stdlib that may have installation metadata, but should not be
-# considered 'installed'.  this theoretically could be determined based on
-# dist.location (py27:`sysconfig.get_paths()['stdlib']`,
-# py26:sysconfig.get_config_vars('LIBDEST')), but fear platform variation may
-# make this ineffective, so hard-coding
-stdlib_pkgs = {"python", "wsgiref", "argparse"}
-
-
 # windows detection, covers cpython and ironpython
 WINDOWS = sys.platform.startswith("win") or (sys.platform == "cli" and os.name == "nt")
