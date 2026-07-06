e3659c1b3748522ff8150633a3a18d7826c7a885
Richard Si <sichard26@gmail.com>
2026-07-06 00:07:08 -0400

    Use locale encoding explicitly for subprocesses
    This is needed to suppress the no encoding specified warning.

--- src/pip/_internal/utils/subprocess.py
+++ src/pip/_internal/utils/subprocess.py
@@ -134,6 +134,7 @@ def call_subprocess(
             stderr=subprocess.STDOUT if not stdout_only else subprocess.PIPE,
             cwd=cwd,
             env=env,
+            encoding="locale",
             errors="backslashreplace",
         )
     except Exception as exc:
