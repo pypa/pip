e12ddd958df6878f38f2a6f777e4653613eb4821
Stéphane Bidoul <stephane.bidoul@gmail.com>
2026-07-12 13:20:53 +0200

    Add support for --uploaded-prior-to with -r pylock.toml

--- src/pip/_internal/req/constructors.py
+++ src/pip/_internal/req/constructors.py
@@ -657,5 +657,6 @@ def install_req_from_pylock_package(
         locked_link = Link(
             requirement_url,
             comes_from=pylock_path_or_url,
+            upload_time=package_dist.upload_time,
         )
         return ireq, locked_link
