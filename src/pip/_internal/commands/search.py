f938fcce162241cf260a4cc4f58e8190d9cba2df
ferkans-amir <amir.rezaei@tu-berlin.de>
2026-07-28 10:23:35 +0200

    fix(search): compare string representation of dist.version with latest in print_dist_installation_info

--- src/pip/_internal/commands/search.py
+++ src/pip/_internal/commands/search.py
@@ -116,7 +116,7 @@ def transform_hits(hits: list[dict[str, str]]) -> list[TransformedHit]:
 def print_dist_installation_info(latest: str, dist: BaseDistribution | None) -> None:
     if dist is not None:
         with indent_log():
-            if dist.version == latest:
+            if str(dist.version) == str(latest):
                 write_output("INSTALLED: %s (latest)", dist.version)
             else:
                 write_output("INSTALLED: %s", dist.version)
